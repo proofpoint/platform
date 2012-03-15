4fcf0526f7b4216786a72198dd5f1ea07a9775cf
Martin Traverso <mtraverso@acm.org>
2012-03-14 21:35:43 -0700

    set name of http threads

--- http-server/src/main/java/com/proofpoint/http/server/HttpServer.java
+++ http-server/src/main/java/com/proofpoint/http/server/HttpServer.java
@@ -142,6 +142,7 @@ public class HttpServer
             adminConnector.setHost(nodeInfo.getBindIp().getHostAddress());
 
             QueuedThreadPool adminThreadPool = new QueuedThreadPool(config.getAdminMaxThreads());
+            adminThreadPool.setName("http-admin-worker");
             adminThreadPool.setMinThreads(config.getAdminMinThreads());
             adminThreadPool.setMaxIdleTimeMs((int) config.getThreadMaxIdleTime().convertTo(TimeUnit.MILLISECONDS));
             adminConnector.setThreadPool(adminThreadPool);
@@ -153,6 +154,7 @@ public class HttpServer
         QueuedThreadPool threadPool = new QueuedThreadPool(config.getMaxThreads());
         threadPool.setMinThreads(config.getMinThreads());
         threadPool.setMaxIdleTimeMs((int) config.getThreadMaxIdleTime().convertTo(TimeUnit.MILLISECONDS));
+        threadPool.setName("http-worker");
         server.setThreadPool(threadPool);
 
         /**
