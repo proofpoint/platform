airlift/airlift

Lock pid file to indicate Running state

launcher/src/main/java/io/airlift/launcher/PidFile.java
@@ -1,0 +1,100 @@
+/*
+ * Copyright 2010 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.airlift.launcher;
+
+import com.google.common.base.Preconditions;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+@SuppressWarnings("FieldCanBeLocal")
+class PidFile
+{
+    private final String pid_file_path;
+    private FileChannel pidChannel = null;
+    private FileChannel lockChannel = null;
+    private FileLock startLock = null;
+
+    PidFile(String pid_file_path)
+    {
+        Preconditions.checkNotNull(pid_file_path, "pid_file_path is null");
+        this.pid_file_path = pid_file_path;
+    }
+
+    int starting()
+    {
+        //noinspection ResultOfMethodCallIgnored
+        new File(pid_file_path).getParentFile().mkdirs();
+        try {
+            pidChannel = new RandomAccessFile(pid_file_path, "rw").getChannel();
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException("Cannot open pid file: " + e);
+        }
+
+        if (System.getProperty("os.name").startsWith("Windows")) {
+            // Windows file locks are mandatory: use a separate lock file so that
+            // the pid file can be read by other processes
+            try {
+                lockChannel = new RandomAccessFile(pid_file_path + ".lck", "rw").getChannel();
+            }
+            catch (FileNotFoundException e) {
+                throw new RuntimeException("Cannot open pid lockfile: " + e);
+            }
+        }
+        else {
+            lockChannel = pidChannel;
+        }
+
+        try {
+            startLock = lockChannel.tryLock(0, 1, false);
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Cannot lock pid file: " + e);
+        }
+
+        if (startLock != null) {
+            // There is a race here between the time we lock the file and the
+            // time we write our pid into it. This could be fixed by readers using
+            // the pid returned by fcntl(F_GETLK) instead of the file contents, but
+            // have not yet found a way to call fcntl(F_GETLK) from Java.
+            try {
+                pidChannel.truncate(0);
+                pidChannel.write(ByteBuffer.wrap((Integer.toString(Porting.getpid()) + "\n").getBytes()));
+            }
+            catch (IOException e) {
+                throw new RuntimeException("Cannot write to pid file: " + e);
+            }
+        }
+        else {
+            try {
+                String line = new BufferedReader(Channels.newReader(pidChannel, "us-ascii")).readLine();
+                return Integer.decode(line.trim());
+            }
+            catch (IOException e) {
+                throw new RuntimeException("Cannot read pid file: " + e);
+            }
+        }
+        return 0;
+    }
+}

launcher/src/main/java/io/airlift/launcher/Porting.java
@@ -1,0 +1,119 @@
+/*
+ * Copyright 2010 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.airlift.launcher;
+
+import jnr.posix.POSIX;
+import jnr.posix.POSIXFactory;
+import jnr.posix.POSIXHandler;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.IllegalFormatException;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Porting
+{
+    private static final POSIX posix = POSIXFactory.getPOSIX(new OurPOSIXHandler(), true);
+
+    private Porting()
+    {
+    }
+
+    static int getpid()
+    {
+        return posix.getpid();
+    }
+
+    private static final class OurPOSIXHandler implements POSIXHandler
+    {
+        @Override
+        public void error(jnr.constants.platform.Errno error, String extraData)
+        {
+            throw new RuntimeException("native error " + error.description() + " " + extraData);
+        }
+
+        @Override
+        public void unimplementedError(String methodName)
+        {
+            throw new IllegalStateException(methodName + " is not implemented in jnr-posix");
+        }
+
+        @Override
+        public void warn(WARNING_ID id, String message, Object... data)
+        {
+            String msg;
+            try {
+                msg = String.format(message, data);
+            }
+            catch (IllegalFormatException e) {
+                msg = message + " " + Arrays.toString(data);
+            }
+            Logger.getLogger("jnr-posix").log(Level.WARNING, msg);
+        }
+
+        @Override
+        public boolean isVerbose()
+        {
+            return false;
+        }
+
+        @Override
+        public File getCurrentWorkingDirectory()
+        {
+            return new File(".");
+        }
+
+        @Override
+        public String[] getEnv()
+        {
+            String[] envp = new String[System.getenv().size()];
+            int i = 0;
+            for (Map.Entry<String, String> pair : System.getenv().entrySet()) {
+                envp[i++] = pair.getKey() + "=" + pair.getValue();
+            }
+            return envp;
+
+        }
+
+        @Override
+        public InputStream getInputStream()
+        {
+            return System.in;
+        }
+
+        @Override
+        public PrintStream getOutputStream()
+        {
+            return System.out;
+        }
+
+        @Override
+        public int getPID()
+        {
+            throw new IllegalStateException("getPID is not implemented in jnr-posix");
+        }
+
+        @Override
+        public PrintStream getErrorStream()
+        {
+            return System.err;
+        }
+    }
+}

proofpoint/platform

launcher/src/main/java/com/proofpoint/launcher/PidFile.java
@@ -1,0 +1,100 @@
+/*
+ * Copyright 2012 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.launcher;
+
+import com.google.common.base.Preconditions;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+
+@SuppressWarnings("FieldCanBeLocal")
+class PidFile
+{
+    private final String pid_file_path;
+    private FileChannel pidChannel = null;
+    private FileChannel lockChannel = null;
+    private FileLock startLock = null;
+
+    PidFile(String pid_file_path)
+    {
+        Preconditions.checkNotNull(pid_file_path, "pid_file_path is null");
+        this.pid_file_path = pid_file_path;
+    }
+
+    int starting()
+    {
+        //noinspection ResultOfMethodCallIgnored
+        new File(pid_file_path).getParentFile().mkdirs();
+        try {
+            pidChannel = new RandomAccessFile(pid_file_path, "rw").getChannel();
+        }
+        catch (FileNotFoundException e) {
+            throw new RuntimeException("Cannot open pid file: " + e);
+        }
+
+        if (System.getProperty("os.name").startsWith("Windows")) {
+            // Windows file locks are mandatory: use a separate lock file so that
+            // the pid file can be read by other processes
+            try {
+                lockChannel = new RandomAccessFile(pid_file_path + ".lck", "rw").getChannel();
+            }
+            catch (FileNotFoundException e) {
+                throw new RuntimeException("Cannot open pid lockfile: " + e);
+            }
+        }
+        else {
+            lockChannel = pidChannel;
+        }
+
+        try {
+            startLock = lockChannel.tryLock(0, 1, false);
+        }
+        catch (IOException e) {
+            throw new RuntimeException("Cannot lock pid file: " + e);
+        }
+
+        if (startLock != null) {
+            // There is a race here between the time we lock the file and the
+            // time we write our pid into it. This could be fixed by readers using
+            // the pid returned by fcntl(F_GETLK) instead of the file contents, but
+            // have not yet found a way to call fcntl(F_GETLK) from Java.
+            try {
+                pidChannel.truncate(0);
+                pidChannel.write(ByteBuffer.wrap((Integer.toString(Porting.getpid()) + "\n").getBytes()));
+            }
+            catch (IOException e) {
+                throw new RuntimeException("Cannot write to pid file: " + e);
+            }
+        }
+        else {
+            try {
+                String line = new BufferedReader(Channels.newReader(pidChannel, "us-ascii")).readLine();
+                return Integer.decode(line.trim());
+            }
+            catch (IOException e) {
+                throw new RuntimeException("Cannot read pid file: " + e);
+            }
+        }
+        return 0;
+    }
+}

launcher/src/main/java/com/proofpoint/launcher/Porting.java
@@ -1,0 +1,119 @@
+/*
+ * Copyright 2012 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.launcher;
+
+import jnr.posix.POSIX;
+import jnr.posix.POSIXFactory;
+import jnr.posix.POSIXHandler;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.Arrays;
+import java.util.IllegalFormatException;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Porting
+{
+    private static final POSIX posix = POSIXFactory.getPOSIX(new OurPOSIXHandler(), true);
+
+    private Porting()
+    {
+    }
+
+    static int getpid()
+    {
+        return posix.getpid();
+    }
+
+    private static final class OurPOSIXHandler implements POSIXHandler
+    {
+        @Override
+        public void error(jnr.constants.platform.Errno error, String extraData)
+        {
+            throw new RuntimeException("native error " + error.description() + " " + extraData);
+        }
+
+        @Override
+        public void unimplementedError(String methodName)
+        {
+            throw new IllegalStateException(methodName + " is not implemented in jnr-posix");
+        }
+
+        @Override
+        public void warn(WARNING_ID id, String message, Object... data)
+        {
+            String msg;
+            try {
+                msg = String.format(message, data);
+            }
+            catch (IllegalFormatException e) {
+                msg = message + " " + Arrays.toString(data);
+            }
+            Logger.getLogger("jnr-posix").log(Level.WARNING, msg);
+        }
+
+        @Override
+        public boolean isVerbose()
+        {
+            return false;
+        }
+
+        @Override
+        public File getCurrentWorkingDirectory()
+        {
+            return new File(".");
+        }
+
+        @Override
+        public String[] getEnv()
+        {
+            String[] envp = new String[System.getenv().size()];
+            int i = 0;
+            for (Map.Entry<String, String> pair : System.getenv().entrySet()) {
+                envp[i++] = pair.getKey() + "=" + pair.getValue();
+            }
+            return envp;
+
+        }
+
+        @Override
+        public InputStream getInputStream()
+        {
+            return System.in;
+        }
+
+        @Override
+        public PrintStream getOutputStream()
+        {
+            return System.out;
+        }
+
+        @Override
+        public int getPID()
+        {
+            throw new IllegalStateException("getPID is not implemented in jnr-posix");
+        }
+
+        @Override
+        public PrintStream getErrorStream()
+        {
+            return System.err;
+        }
+    }
+}