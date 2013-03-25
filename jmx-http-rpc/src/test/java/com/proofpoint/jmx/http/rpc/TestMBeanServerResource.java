airlift/airlift

Add Bootstrap methods for testing
Add method to disable initialization of logging framework
Add methods to set config properties directly instead of laoding from a file

jmx-http-rpc/src/test/java/io/airlift/jmx/http/rpc/TestMBeanServerResource.java
@@ -18,12 +18,11 @@ package io.airlift.jmx.http.rpc;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Binder;
-import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.Scopes;
-import io.airlift.configuration.ConfigurationFactory;
-import io.airlift.configuration.ConfigurationModule;
+import io.airlift.bootstrap.Bootstrap;
+import io.airlift.bootstrap.LifeCycleManager;
 import io.airlift.http.server.TheServlet;
 import io.airlift.http.server.testing.TestingHttpServer;
 import io.airlift.http.server.testing.TestingHttpServerModule;
@@ -45,7 +44,6 @@ import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.lang.management.ManagementFactory;
-import java.util.Collections;
 import java.util.UUID;
 
 import static org.testng.Assert.assertEquals;
@@ -55,6 +53,7 @@ import static org.weakref.jmx.ObjectNames.generatedNameOf;
 
 public class TestMBeanServerResource
 {
+    private LifeCycleManager lifeCycleManager;
     private TestingHttpServer server;
     private MBeanServerConnection mbeanServerConnection;
     private MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
@@ -65,12 +64,11 @@ public class TestMBeanServerResource
     public void setup()
             throws Exception
     {
-        Injector injector = Guice.createInjector(
+        Bootstrap app = new Bootstrap(
                 new TestingNodeModule(),
                 new TestingHttpServerModule(),
                 new JsonModule(),
                 new JmxHttpRpcModule(TheServlet.class),
-                new ConfigurationModule(new ConfigurationFactory(Collections.<String, String>emptyMap())),
                 new Module()
                 {
                     @Override
@@ -81,8 +79,13 @@ public class TestMBeanServerResource
                     }
                 });
 
+        Injector injector = app
+                .strictConfig()
+                .doNotInitializeLogging()
+                .initialize();
+
+        lifeCycleManager = injector.getInstance(LifeCycleManager.class);
         server = injector.getInstance(TestingHttpServer.class);
-        server.start();
 
         testMBean = injector.getInstance(TestMBean.class);
         testMBeanName = new ObjectName(generatedNameOf(TestMBean.class));
@@ -93,15 +96,14 @@ public class TestMBeanServerResource
                 new JMXServiceURL("service:jmx:" + server.getBaseUrl()),
                 ImmutableMap.of(JMXConnector.CREDENTIALS, new String[] {"foo", "bar"}));
         mbeanServerConnection = connect.getMBeanServerConnection();
-
     }
 
     @AfterMethod
     public void teardown()
             throws Exception
     {
-        if (server != null) {
-            server.stop();
+        if (lifeCycleManager != null) {
+            lifeCycleManager.stop();
             platformMBeanServer.unregisterMBean(testMBeanName);
         }
     }

rack/src/test/java/io/airlift/rack/TestRackAnnounce.java
@@ -15,13 +15,11 @@
  */
 package io.airlift.rack;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import com.google.common.io.Resources;
-import com.google.inject.Guice;
 import com.google.inject.Injector;
-import io.airlift.configuration.ConfigurationFactory;
-import io.airlift.configuration.ConfigurationModule;
+import io.airlift.bootstrap.Bootstrap;
+import io.airlift.bootstrap.LifeCycleManager;
 import io.airlift.discovery.client.AnnouncementHttpServerInfo;
 import io.airlift.discovery.client.Announcer;
 import io.airlift.discovery.client.DiscoveryLookupClient;
@@ -29,7 +27,6 @@ import io.airlift.discovery.client.ServiceAnnouncement;
 import io.airlift.discovery.client.ServiceDescriptor;
 import io.airlift.discovery.client.testing.InMemoryDiscoveryClient;
 import io.airlift.discovery.client.testing.TestingDiscoveryModule;
-import io.airlift.http.server.testing.TestingHttpServer;
 import io.airlift.http.server.testing.TestingHttpServerModule;
 import io.airlift.node.testing.TestingNodeModule;
 import org.testng.annotations.AfterMethod;
@@ -42,14 +39,14 @@ import static org.testng.Assert.assertEquals;
 
 public class TestRackAnnounce
 {
-    private TestingHttpServer server;
+    private LifeCycleManager lifeCycleManager;
 
     @AfterMethod
     public void tearDown()
             throws Exception
     {
-        if (server != null) {
-            server.stop();
+        if (lifeCycleManager != null) {
+            lifeCycleManager.stop();
         }
     }
 
@@ -57,20 +54,20 @@ public class TestRackAnnounce
     public void testAnnouncement()
             throws Exception
     {
-        Injector injector = Guice.createInjector(
+        Bootstrap app = new Bootstrap(
                 new TestingHttpServerModule(),
                 new RackModule(),
                 new TestingNodeModule(),
-                new TestingDiscoveryModule(),
-                new ConfigurationModule(new ConfigurationFactory(
-                        ImmutableMap.<String, String>builder()
-                                .put("rackserver.rack-config-path", Resources.getResource("test/raw/config.ru").getFile())
-                                .put("rackserver.announcement", "racktest")
-                                .build()
-                )));
+                new TestingDiscoveryModule());
 
-        server = injector.getInstance(TestingHttpServer.class);
-        server.start();
+        Injector injector = app
+                .strictConfig()
+                .doNotInitializeLogging()
+                .setRequiredConfigurationProperty("rackserver.rack-config-path", Resources.getResource("test/raw/config.ru").getFile())
+                .setRequiredConfigurationProperty("rackserver.announcement", "racktest")
+                .initialize();
+
+        lifeCycleManager = injector.getInstance(LifeCycleManager.class);
 
         injector.getInstance(Announcer.class).start();
 

rack/src/test/java/io/airlift/rack/TestRackModuleRaw.java
@@ -15,12 +15,10 @@
  */
 package io.airlift.rack;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Resources;
-import com.google.inject.Guice;
 import com.google.inject.Injector;
-import io.airlift.configuration.ConfigurationFactory;
-import io.airlift.configuration.ConfigurationModule;
+import io.airlift.bootstrap.Bootstrap;
+import io.airlift.bootstrap.LifeCycleManager;
 import io.airlift.discovery.client.testing.TestingDiscoveryModule;
 import io.airlift.http.client.ApacheHttpClient;
 import io.airlift.http.client.HttpClient;
@@ -42,24 +40,26 @@ public class TestRackModuleRaw
 {
     private HttpClient client;
     private TestingHttpServer server;
+    private LifeCycleManager lifeCycleManager;
 
     @BeforeMethod
     public void setup()
             throws Exception
     {
-        Injector injector = Guice.createInjector(
+        Bootstrap app = new Bootstrap(
                 new TestingHttpServerModule(),
                 new RackModule(),
                 new TestingNodeModule(),
-                new TestingDiscoveryModule(),
-                new ConfigurationModule(new ConfigurationFactory(
-                        ImmutableMap.<String, String>builder()
-                                .put("rackserver.rack-config-path", Resources.getResource("test/raw/config.ru").getFile())
-                                .build()
-                )));
+                new TestingDiscoveryModule());
 
+        Injector injector = app
+                .strictConfig()
+                .doNotInitializeLogging()
+                .setRequiredConfigurationProperty("rackserver.rack-config-path", Resources.getResource("test/raw/config.ru").getFile())
+                .initialize();
+
+        lifeCycleManager = injector.getInstance(LifeCycleManager.class);
         server = injector.getInstance(TestingHttpServer.class);
-        server.start();
         client = new ApacheHttpClient();
     }
 
@@ -67,8 +67,8 @@ public class TestRackModuleRaw
     public void tearDown()
             throws Exception
     {
-        if (server != null) {
-            server.stop();
+        if (lifeCycleManager != null) {
+            lifeCycleManager.stop();
         }
     }
 

rack/src/test/java/io/airlift/rack/TestRackModuleSinatra.java
@@ -18,10 +18,9 @@ package io.airlift.rack;
 import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Resources;
-import com.google.inject.Guice;
 import com.google.inject.Injector;
-import io.airlift.configuration.ConfigurationFactory;
-import io.airlift.configuration.ConfigurationModule;
+import io.airlift.bootstrap.Bootstrap;
+import io.airlift.bootstrap.LifeCycleManager;
 import io.airlift.discovery.client.testing.TestingDiscoveryModule;
 import io.airlift.http.client.ApacheHttpClient;
 import io.airlift.http.client.HttpClient;
@@ -50,24 +49,26 @@ public class TestRackModuleSinatra
 {
     private HttpClient client;
     private TestingHttpServer server;
+    private LifeCycleManager lifeCycleManager;
 
     @BeforeMethod
     public void setup()
             throws Exception
     {
-        Injector injector = Guice.createInjector(
+        Bootstrap app = new Bootstrap(
                 new TestingHttpServerModule(),
                 new RackModule(),
                 new TestingNodeModule(),
-                new TestingDiscoveryModule(),
-                new ConfigurationModule(new ConfigurationFactory(
-                        ImmutableMap.<String, String>builder()
-                                .put("rackserver.rack-config-path", Resources.getResource("test/sinatra/config.ru").getFile())
-                                .build()
-                )));
+                new TestingDiscoveryModule());
 
+        Injector injector = app
+                .strictConfig()
+                .doNotInitializeLogging()
+                .setRequiredConfigurationProperty("rackserver.rack-config-path", Resources.getResource("test/sinatra/config.ru").getFile())
+                .initialize();
+
+        lifeCycleManager = injector.getInstance(LifeCycleManager.class);
         server = injector.getInstance(TestingHttpServer.class);
-        server.start();
         client = new ApacheHttpClient();
     }
 
@@ -75,8 +76,8 @@ public class TestRackModuleSinatra
     public void tearDown()
             throws Exception
     {
-        if (server != null) {
-            server.stop();
+        if (lifeCycleManager != null) {
+            lifeCycleManager.stop();
         }
     }
 

proofpoint/platform

jmx-http-rpc/src/test/java/com/proofpoint/jmx/http/rpc/TestMBeanServerResource.java
@@ -18,12 +18,11 @@ package com.proofpoint.jmx.http.rpc;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.inject.Binder;
-import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
 import com.google.inject.Scopes;
-import com.proofpoint.configuration.ConfigurationFactory;
-import com.proofpoint.configuration.ConfigurationModule;
+import com.proofpoint.bootstrap.Bootstrap;
+import com.proofpoint.bootstrap.LifeCycleManager;
 import com.proofpoint.http.server.TheServlet;
 import com.proofpoint.http.server.testing.TestingHttpServer;
 import com.proofpoint.http.server.testing.TestingHttpServerModule;
@@ -45,7 +44,6 @@ import javax.management.remote.JMXConnector;
 import javax.management.remote.JMXConnectorFactory;
 import javax.management.remote.JMXServiceURL;
 import java.lang.management.ManagementFactory;
-import java.util.Collections;
 import java.util.UUID;
 
 import static org.testng.Assert.assertEquals;
@@ -55,6 +53,7 @@ import static org.weakref.jmx.ObjectNames.generatedNameOf;
 
 public class TestMBeanServerResource
 {
+    private LifeCycleManager lifeCycleManager;
     private TestingHttpServer server;
     private MBeanServerConnection mbeanServerConnection;
     private MBeanServer platformMBeanServer = ManagementFactory.getPlatformMBeanServer();
@@ -65,12 +64,11 @@ public class TestMBeanServerResource
     public void setup()
             throws Exception
     {
-        Injector injector = Guice.createInjector(
+        Bootstrap app = new Bootstrap(
                 new TestingNodeModule(),
                 new TestingHttpServerModule(),
                 new JsonModule(),
                 new JmxHttpRpcModule(TheServlet.class),
-                new ConfigurationModule(new ConfigurationFactory(Collections.<String, String>emptyMap())),
                 new Module()
                 {
                     @Override
@@ -81,8 +79,12 @@ public class TestMBeanServerResource
                     }
                 });
 
+        Injector injector = app
+                .doNotInitializeLogging()
+                .initialize();
+
+        lifeCycleManager = injector.getInstance(LifeCycleManager.class);
         server = injector.getInstance(TestingHttpServer.class);
-        server.start();
 
         testMBean = injector.getInstance(TestMBean.class);
         testMBeanName = new ObjectName(generatedNameOf(TestMBean.class));
@@ -93,15 +95,14 @@ public class TestMBeanServerResource
                 new JMXServiceURL("service:jmx:" + server.getBaseUrl()),
                 ImmutableMap.of(JMXConnector.CREDENTIALS, new String[] {"foo", "bar"}));
         mbeanServerConnection = connect.getMBeanServerConnection();
-
     }
 
     @AfterMethod
     public void teardown()
             throws Exception
     {
-        if (server != null) {
-            server.stop();
+        if (lifeCycleManager != null) {
+            lifeCycleManager.stop();
             platformMBeanServer.unregisterMBean(testMBeanName);
         }
     }

rack/src/test/java/com/proofpoint/rack/TestRackAnnounce.java
@@ -15,13 +15,11 @@
  */
 package com.proofpoint.rack;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
 import com.google.common.io.Resources;
-import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.proofpoint.configuration.ConfigurationFactory;
-import com.proofpoint.configuration.ConfigurationModule;
+import com.proofpoint.bootstrap.Bootstrap;
+import com.proofpoint.bootstrap.LifeCycleManager;
 import com.proofpoint.discovery.client.AnnouncementHttpServerInfo;
 import com.proofpoint.discovery.client.Announcer;
 import com.proofpoint.discovery.client.DiscoveryLookupClient;
@@ -29,7 +27,6 @@ import com.proofpoint.discovery.client.ServiceAnnouncement;
 import com.proofpoint.discovery.client.ServiceDescriptor;
 import com.proofpoint.discovery.client.testing.InMemoryDiscoveryClient;
 import com.proofpoint.discovery.client.testing.TestingDiscoveryModule;
-import com.proofpoint.http.server.testing.TestingHttpServer;
 import com.proofpoint.http.server.testing.TestingHttpServerModule;
 import com.proofpoint.node.testing.TestingNodeModule;
 import org.testng.annotations.AfterMethod;
@@ -42,14 +39,14 @@ import static org.testng.Assert.assertEquals;
 
 public class TestRackAnnounce
 {
-    private TestingHttpServer server;
+    private LifeCycleManager lifeCycleManager;
 
     @AfterMethod
     public void tearDown()
             throws Exception
     {
-        if (server != null) {
-            server.stop();
+        if (lifeCycleManager != null) {
+            lifeCycleManager.stop();
         }
     }
 
@@ -57,20 +54,19 @@ public class TestRackAnnounce
     public void testAnnouncement()
             throws Exception
     {
-        Injector injector = Guice.createInjector(
+        Bootstrap app = new Bootstrap(
                 new TestingHttpServerModule(),
                 new RackModule(),
                 new TestingNodeModule(),
-                new TestingDiscoveryModule(),
-                new ConfigurationModule(new ConfigurationFactory(
-                        ImmutableMap.<String, String>builder()
-                                .put("rackserver.rack-config-path", Resources.getResource("test/raw/config.ru").getFile())
-                                .put("rackserver.announcement", "racktest")
-                                .build()
-                )));
+                new TestingDiscoveryModule());
 
-        server = injector.getInstance(TestingHttpServer.class);
-        server.start();
+        Injector injector = app
+                .doNotInitializeLogging()
+                .setRequiredConfigurationProperty("rackserver.rack-config-path", Resources.getResource("test/raw/config.ru").getFile())
+                .setRequiredConfigurationProperty("rackserver.announcement", "racktest")
+                .initialize();
+
+        lifeCycleManager = injector.getInstance(LifeCycleManager.class);
 
         injector.getInstance(Announcer.class).start();
 

rack/src/test/java/com/proofpoint/rack/TestRackModuleRaw.java
@@ -15,12 +15,10 @@
  */
 package com.proofpoint.rack;
 
-import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Resources;
-import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.proofpoint.configuration.ConfigurationFactory;
-import com.proofpoint.configuration.ConfigurationModule;
+import com.proofpoint.bootstrap.Bootstrap;
+import com.proofpoint.bootstrap.LifeCycleManager;
 import com.proofpoint.discovery.client.testing.TestingDiscoveryModule;
 import com.proofpoint.http.client.ApacheHttpClient;
 import com.proofpoint.http.client.HttpClient;
@@ -42,24 +40,25 @@ public class TestRackModuleRaw
 {
     private HttpClient client;
     private TestingHttpServer server;
+    private LifeCycleManager lifeCycleManager;
 
     @BeforeMethod
     public void setup()
             throws Exception
     {
-        Injector injector = Guice.createInjector(
+        Bootstrap app = new Bootstrap(
                 new TestingHttpServerModule(),
                 new RackModule(),
                 new TestingNodeModule(),
-                new TestingDiscoveryModule(),
-                new ConfigurationModule(new ConfigurationFactory(
-                        ImmutableMap.<String, String>builder()
-                                .put("rackserver.rack-config-path", Resources.getResource("test/raw/config.ru").getFile())
-                                .build()
-                )));
+                new TestingDiscoveryModule());
 
+        Injector injector = app
+                .doNotInitializeLogging()
+                .setRequiredConfigurationProperty("rackserver.rack-config-path", Resources.getResource("test/raw/config.ru").getFile())
+                .initialize();
+
+        lifeCycleManager = injector.getInstance(LifeCycleManager.class);
         server = injector.getInstance(TestingHttpServer.class);
-        server.start();
         client = new ApacheHttpClient();
     }
 
@@ -67,8 +66,8 @@ public class TestRackModuleRaw
     public void tearDown()
             throws Exception
     {
-        if (server != null) {
-            server.stop();
+        if (lifeCycleManager != null) {
+            lifeCycleManager.stop();
         }
     }
 

rack/src/test/java/com/proofpoint/rack/TestRackModuleSinatra.java
@@ -18,10 +18,9 @@ package com.proofpoint.rack;
 import com.google.common.base.Charsets;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Resources;
-import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.proofpoint.configuration.ConfigurationFactory;
-import com.proofpoint.configuration.ConfigurationModule;
+import com.proofpoint.bootstrap.Bootstrap;
+import com.proofpoint.bootstrap.LifeCycleManager;
 import com.proofpoint.discovery.client.testing.TestingDiscoveryModule;
 import com.proofpoint.http.client.ApacheHttpClient;
 import com.proofpoint.http.client.HttpClient;
@@ -50,24 +49,25 @@ public class TestRackModuleSinatra
 {
     private HttpClient client;
     private TestingHttpServer server;
+    private LifeCycleManager lifeCycleManager;
 
     @BeforeMethod
     public void setup()
             throws Exception
     {
-        Injector injector = Guice.createInjector(
+        Bootstrap app = new Bootstrap(
                 new TestingHttpServerModule(),
                 new RackModule(),
                 new TestingNodeModule(),
-                new TestingDiscoveryModule(),
-                new ConfigurationModule(new ConfigurationFactory(
-                        ImmutableMap.<String, String>builder()
-                                .put("rackserver.rack-config-path", Resources.getResource("test/sinatra/config.ru").getFile())
-                                .build()
-                )));
+                new TestingDiscoveryModule());
 
+        Injector injector = app
+                .doNotInitializeLogging()
+                .setRequiredConfigurationProperty("rackserver.rack-config-path", Resources.getResource("test/sinatra/config.ru").getFile())
+                .initialize();
+
+        lifeCycleManager = injector.getInstance(LifeCycleManager.class);
         server = injector.getInstance(TestingHttpServer.class);
-        server.start();
         client = new ApacheHttpClient();
     }
 
@@ -75,8 +75,8 @@ public class TestRackModuleSinatra
     public void tearDown()
             throws Exception
     {
-        if (server != null) {
-            server.stop();
+        if (lifeCycleManager != null) {
+            lifeCycleManager.stop();
         }
     }
 