airlift/airlift

Create DefaultingJsonResponseHandler

http-client/src/main/java/io/airlift/http/client/DefaultingJsonResponseHandler.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright 2013 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.airlift.http.client;
+
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.io.CharStreams;
+import com.google.common.net.MediaType;
+import com.google.common.primitives.Ints;
+import io.airlift.json.JsonCodec;
+
+import java.io.InputStreamReader;
+import java.util.Set;
+
+public class DefaultingJsonResponseHandler<T> implements ResponseHandler<T, RuntimeException>
+{
+    private static final MediaType MEDIA_TYPE_JSON = MediaType.create("application", "json");
+
+    public static <T> DefaultingJsonResponseHandler<T> createDefaultingJsonResponseHandler(JsonCodec<T> jsonCodec, T defaultValue)
+    {
+        return new DefaultingJsonResponseHandler<>(jsonCodec, defaultValue);
+    }
+
+    public static <T> DefaultingJsonResponseHandler<T> createDefaultingJsonResponseHandler(JsonCodec<T> jsonCodec, T defaultValue, int firstSuccessfulResponseCode, int... otherSuccessfulResponseCodes)
+    {
+        return new DefaultingJsonResponseHandler<>(jsonCodec, defaultValue, firstSuccessfulResponseCode, otherSuccessfulResponseCodes);
+    }
+
+    private final JsonCodec<T> jsonCodec;
+    private final T defaultValue;
+    private final Set<Integer> successfulResponseCodes;
+
+    private DefaultingJsonResponseHandler(JsonCodec<T> jsonCodec, T defaultValue)
+    {
+        this(jsonCodec, defaultValue, 200, 201, 202, 203, 204, 205, 206);
+    }
+
+    private DefaultingJsonResponseHandler(JsonCodec<T> jsonCodec, T defaultValue, int firstSuccessfulResponseCode, int... otherSuccessfulResponseCodes)
+    {
+        this.jsonCodec = jsonCodec;
+        this.defaultValue = defaultValue;
+        this.successfulResponseCodes = ImmutableSet.<Integer>builder().add(firstSuccessfulResponseCode).addAll(Ints.asList(otherSuccessfulResponseCodes)).build();
+    }
+
+    @Override
+    public T handleException(Request request, Exception exception)
+    {
+        return defaultValue;
+    }
+
+    @Override
+    public T handle(Request request, Response response)
+    {
+        if (!successfulResponseCodes.contains(response.getStatusCode())) {
+            return defaultValue;
+        }
+        String contentType = response.getHeader("Content-Type");
+        if (!MediaType.parse(contentType).is(MEDIA_TYPE_JSON)) {
+            return defaultValue;
+        }
+        try {
+            String json = CharStreams.toString(new InputStreamReader(response.getInputStream(), Charsets.UTF_8));
+            return jsonCodec.fromJson(json);
+        }
+        catch (Exception e) {
+            return defaultValue;
+        }
+    }
+}

http-client/src/test/java/io/airlift/http/client/TestDefaultingJsonResponseHandler.java
@@ -1,0 +1,102 @@
+package io.airlift.http.client;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.airlift.json.JsonCodec;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static com.google.common.net.MediaType.JSON_UTF_8;
+import static com.google.common.net.MediaType.PLAIN_TEXT_UTF_8;
+import static io.airlift.http.client.DefaultingJsonResponseHandler.createDefaultingJsonResponseHandler;
+import static io.airlift.http.client.HttpStatus.INTERNAL_SERVER_ERROR;
+import static io.airlift.http.client.HttpStatus.OK;
+import static io.airlift.http.client.testing.TestingResponse.mockResponse;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertSame;
+
+public class TestDefaultingJsonResponseHandler
+{
+    private static final User DEFAULT_VALUE = new User("defaultUser", 998);
+    private JsonCodec<User> codec;
+    private DefaultingJsonResponseHandler<User> handler;
+
+    @BeforeMethod
+    public void setUp()
+            throws Exception
+    {
+        codec = JsonCodec.jsonCodec(User.class);
+        handler = createDefaultingJsonResponseHandler(codec, DEFAULT_VALUE);
+    }
+
+    @Test
+    public void testValidJson()
+            throws Exception
+    {
+        User user = new User("Joe", 25);
+        String json = codec.toJson(user);
+        User response = handler.handle(null, mockResponse(OK, JSON_UTF_8, json));
+
+        assertEquals(response.getName(), user.getName());
+        assertEquals(response.getAge(), user.getAge());
+    }
+
+    @Test
+    public void testInvalidJson()
+    {
+        String json = "{\"age\": \"foo\"}";
+        User response = handler.handle(null, mockResponse(OK, JSON_UTF_8, json));
+
+        assertSame(response, DEFAULT_VALUE);
+    }
+
+    @Test
+    public void testException()
+    {
+        User response = handler.handleException(null, null);
+
+        assertSame(response, DEFAULT_VALUE);
+    }
+
+    @Test
+    public void testNonJsonResponse()
+    {
+        User response = handler.handle(null, mockResponse(OK, PLAIN_TEXT_UTF_8, "hello"));
+
+        assertSame(response, DEFAULT_VALUE);
+    }
+
+    @Test
+    public void testJsonErrorResponse()
+    {
+        String json = "{\"error\": true}";
+        User response = handler.handle(null, mockResponse(INTERNAL_SERVER_ERROR, JSON_UTF_8, json));
+
+        assertSame(response, DEFAULT_VALUE);
+    }
+
+    public static class User
+    {
+        private final String name;
+        private final int age;
+
+        @JsonCreator
+        public User(@JsonProperty("name") String name, @JsonProperty("age") int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        @JsonProperty
+        public String getName()
+        {
+            return name;
+        }
+
+        @JsonProperty
+        public int getAge()
+        {
+            return age;
+        }
+    }
+}

proofpoint/platform

http-client/src/main/java/com/proofpoint/http/client/DefaultingJsonResponseHandler.java
@@ -1,0 +1,82 @@
+/*
+ * Copyright 2013 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.http.client;
+
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.io.CharStreams;
+import com.google.common.net.MediaType;
+import com.google.common.primitives.Ints;
+import com.proofpoint.json.JsonCodec;
+
+import java.io.InputStreamReader;
+import java.util.Set;
+
+public class DefaultingJsonResponseHandler<T> implements ResponseHandler<T, RuntimeException>
+{
+    private static final MediaType MEDIA_TYPE_JSON = MediaType.create("application", "json");
+
+    public static <T> DefaultingJsonResponseHandler<T> createDefaultingJsonResponseHandler(JsonCodec<T> jsonCodec, T defaultValue)
+    {
+        return new DefaultingJsonResponseHandler<>(jsonCodec, defaultValue);
+    }
+
+    public static <T> DefaultingJsonResponseHandler<T> createDefaultingJsonResponseHandler(JsonCodec<T> jsonCodec, T defaultValue, int firstSuccessfulResponseCode, int... otherSuccessfulResponseCodes)
+    {
+        return new DefaultingJsonResponseHandler<>(jsonCodec, defaultValue, firstSuccessfulResponseCode, otherSuccessfulResponseCodes);
+    }
+
+    private final JsonCodec<T> jsonCodec;
+    private final T defaultValue;
+    private final Set<Integer> successfulResponseCodes;
+
+    private DefaultingJsonResponseHandler(JsonCodec<T> jsonCodec, T defaultValue)
+    {
+        this(jsonCodec, defaultValue, 200, 201, 202, 203, 204, 205, 206);
+    }
+
+    private DefaultingJsonResponseHandler(JsonCodec<T> jsonCodec, T defaultValue, int firstSuccessfulResponseCode, int... otherSuccessfulResponseCodes)
+    {
+        this.jsonCodec = jsonCodec;
+        this.defaultValue = defaultValue;
+        this.successfulResponseCodes = ImmutableSet.<Integer>builder().add(firstSuccessfulResponseCode).addAll(Ints.asList(otherSuccessfulResponseCodes)).build();
+    }
+
+    @Override
+    public T handleException(Request request, Exception exception)
+    {
+        return defaultValue;
+    }
+
+    @Override
+    public T handle(Request request, Response response)
+    {
+        if (!successfulResponseCodes.contains(response.getStatusCode())) {
+            return defaultValue;
+        }
+        String contentType = response.getHeader("Content-Type");
+        if (!MediaType.parse(contentType).is(MEDIA_TYPE_JSON)) {
+            return defaultValue;
+        }
+        try {
+            String json = CharStreams.toString(new InputStreamReader(response.getInputStream(), Charsets.UTF_8));
+            return jsonCodec.fromJson(json);
+        }
+        catch (Exception e) {
+            return defaultValue;
+        }
+    }
+}

http-client/src/test/java/com/proofpoint/http/client/TestDefaultingJsonResponseHandler.java
@@ -1,0 +1,102 @@
+package com.proofpoint.http.client;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.proofpoint.json.JsonCodec;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static com.google.common.net.MediaType.JSON_UTF_8;
+import static com.google.common.net.MediaType.PLAIN_TEXT_UTF_8;
+import static com.proofpoint.http.client.DefaultingJsonResponseHandler.createDefaultingJsonResponseHandler;
+import static com.proofpoint.http.client.HttpStatus.INTERNAL_SERVER_ERROR;
+import static com.proofpoint.http.client.HttpStatus.OK;
+import static com.proofpoint.http.client.testing.TestingResponse.mockResponse;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertSame;
+
+public class TestDefaultingJsonResponseHandler
+{
+    private static final User DEFAULT_VALUE = new User("defaultUser", 998);
+    private JsonCodec<User> codec;
+    private DefaultingJsonResponseHandler<User> handler;
+
+    @BeforeMethod
+    public void setUp()
+            throws Exception
+    {
+        codec = JsonCodec.jsonCodec(User.class);
+        handler = createDefaultingJsonResponseHandler(codec, DEFAULT_VALUE);
+    }
+
+    @Test
+    public void testValidJson()
+            throws Exception
+    {
+        User user = new User("Joe", 25);
+        String json = codec.toJson(user);
+        User response = handler.handle(null, mockResponse(OK, JSON_UTF_8, json));
+
+        assertEquals(response.getName(), user.getName());
+        assertEquals(response.getAge(), user.getAge());
+    }
+
+    @Test
+    public void testInvalidJson()
+    {
+        String json = "{\"age\": \"foo\"}";
+        User response = handler.handle(null, mockResponse(OK, JSON_UTF_8, json));
+
+        assertSame(response, DEFAULT_VALUE);
+    }
+
+    @Test
+    public void testException()
+    {
+        User response = handler.handleException(null, null);
+
+        assertSame(response, DEFAULT_VALUE);
+    }
+
+    @Test
+    public void testNonJsonResponse()
+    {
+        User response = handler.handle(null, mockResponse(OK, PLAIN_TEXT_UTF_8, "hello"));
+
+        assertSame(response, DEFAULT_VALUE);
+    }
+
+    @Test
+    public void testJsonErrorResponse()
+    {
+        String json = "{\"error\": true}";
+        User response = handler.handle(null, mockResponse(INTERNAL_SERVER_ERROR, JSON_UTF_8, json));
+
+        assertSame(response, DEFAULT_VALUE);
+    }
+
+    public static class User
+    {
+        private final String name;
+        private final int age;
+
+        @JsonCreator
+        public User(@JsonProperty("name") String name, @JsonProperty("age") int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        @JsonProperty
+        public String getName()
+        {
+            return name;
+        }
+
+        @JsonProperty
+        public int getAge()
+        {
+            return age;
+        }
+    }
+}