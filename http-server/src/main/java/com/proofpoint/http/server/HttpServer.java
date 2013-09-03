ffad1f315a7679fdec09a2ebe26d6cee16ad94e7
John Myers <jgmyers@proofpoint.com>
2013-09-03 16:54:57 -0700

    Export http-server stats to Pulse
    Also remove redundant stats.

--- http-server/src/main/java/com/proofpoint/http/server/HttpServer.java
+++ http-server/src/main/java/com/proofpoint/http/server/HttpServer.java
@@ -81,6 +81,7 @@ public class HttpServer
             QueryStringFilter queryStringFilter,
             TraceTokenManager tokenManager,
             RequestStats stats,
+            DetailedRequestStats detailedRequestStats,
             EventClient eventClient)
             throws IOException
     {
@@ -219,7 +220,7 @@ public class HttpServer
         }
 
         RequestLogHandler statsRecorder = new RequestLogHandler();
-        statsRecorder.setRequestLog(new StatsRecordingHandler(stats));
+        statsRecorder.setRequestLog(new StatsRecordingHandler(stats, detailedRequestStats));
         handlers.addHandler(statsRecorder);
 
         // add handlers to Jetty
