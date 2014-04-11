28ce6bcc4a96e18974be968b7fcbc3b55d5b4fcb
John Myers <jgmyers@proofpoint.com>
2014-04-11 15:46:11 -0700

    Use sparse stats for report collections

--- stats/src/test/java/com/proofpoint/stats/TestTimeStat.java
+++ stats/src/test/java/com/proofpoint/stats/TestTimeStat.java
@@ -32,8 +32,7 @@ import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 
-public class
-        TestTimeStat
+public class TestTimeStat
 {
     private static final int VALUES = 1000;
     private TestingTicker ticker;
