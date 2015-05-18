airlift/airlift

Remove dependency on SLF4J for log output

log-manager/src/main/java/io/airlift/log/Level.java
@@ -1,0 +1,41 @@
+package io.airlift.log;
+
+enum Level
+{
+    OFF(java.util.logging.Level.OFF),
+    DEBUG(java.util.logging.Level.FINE),
+    INFO(java.util.logging.Level.INFO),
+    WARN(java.util.logging.Level.WARNING),
+    ERROR(java.util.logging.Level.SEVERE);
+
+    private final java.util.logging.Level julLevel;
+
+    Level(java.util.logging.Level julLevel)
+    {
+        this.julLevel = julLevel;
+    }
+
+    public java.util.logging.Level toJulLevel()
+    {
+        return julLevel;
+    }
+
+    static Level fromJulLevel(java.util.logging.Level level)
+    {
+        // Convert any implementation of Level based on the int value
+        int levelValue = level.intValue();
+        if (levelValue == java.util.logging.Level.OFF.intValue()) {
+            return OFF;
+        }
+        if (levelValue >= java.util.logging.Level.SEVERE.intValue()) {
+            return ERROR;
+        }
+        if (levelValue >= java.util.logging.Level.WARNING.intValue()) {
+            return WARN;
+        }
+        if (levelValue >= java.util.logging.Level.INFO.intValue()) {
+            return INFO;
+        }
+        return DEBUG;
+    }
+}

log-manager/src/main/java/io/airlift/log/OutputStreamHandler.java
@@ -1,0 +1,71 @@
+package io.airlift.log;
+
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.logging.ErrorManager.CLOSE_FAILURE;
+import static java.util.logging.ErrorManager.FLUSH_FAILURE;
+import static java.util.logging.ErrorManager.FORMAT_FAILURE;
+import static java.util.logging.ErrorManager.WRITE_FAILURE;
+
+final class OutputStreamHandler
+        extends Handler
+{
+    private final Writer writer;
+
+    public OutputStreamHandler(OutputStream out)
+    {
+        writer = new OutputStreamWriter(out, UTF_8);
+        setFormatter(new StaticFormatter());
+    }
+
+    @Override
+    public void publish(LogRecord record)
+    {
+        if (!isLoggable(record)) {
+            return;
+        }
+
+        String message;
+        try {
+            message = getFormatter().format(record);
+        }
+        catch (Exception e) {
+            reportError(null, e, FORMAT_FAILURE);
+            return;
+        }
+
+        try {
+            writer.write(message);
+        }
+        catch (Exception e) {
+            reportError(null, e, WRITE_FAILURE);
+        }
+    }
+
+    @Override
+    public void flush()
+    {
+        try {
+            writer.flush();
+        }
+        catch (Exception e) {
+            reportError(null, e, FLUSH_FAILURE);
+        }
+    }
+
+    @Override
+    public void close()
+    {
+        try {
+            writer.flush();
+        }
+        catch (Exception e) {
+            reportError(null, e, CLOSE_FAILURE);
+        }
+    }
+}

log-manager/src/main/java/io/airlift/log/StaticFormatter.java
@@ -1,0 +1,78 @@
+package io.airlift.log;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.SignStyle;
+import java.util.Locale;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+import static java.time.temporal.ChronoField.DAY_OF_MONTH;
+import static java.time.temporal.ChronoField.HOUR_OF_DAY;
+import static java.time.temporal.ChronoField.MILLI_OF_SECOND;
+import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
+import static java.time.temporal.ChronoField.MONTH_OF_YEAR;
+import static java.time.temporal.ChronoField.SECOND_OF_MINUTE;
+import static java.time.temporal.ChronoField.YEAR;
+
+class StaticFormatter
+        extends Formatter
+{
+    private static final ZoneId SYSTEM_ZONE = ZoneId.systemDefault().normalized();
+
+    private static final DateTimeFormatter TIMESTAMP_FORMATTER = new DateTimeFormatterBuilder()
+            .parseCaseInsensitive()
+            .appendValue(YEAR, 4, 10, SignStyle.EXCEEDS_PAD)
+            .appendLiteral('-')
+            .appendValue(MONTH_OF_YEAR, 2)
+            .appendLiteral('-')
+            .appendValue(DAY_OF_MONTH, 2)
+            .appendLiteral('T')
+            .appendValue(HOUR_OF_DAY, 2)
+            .appendLiteral(':')
+            .appendValue(MINUTE_OF_HOUR, 2)
+            .appendLiteral(':')
+            .appendValue(SECOND_OF_MINUTE, 2)
+            .appendLiteral('.')
+            .appendValue(MILLI_OF_SECOND, 3)
+            .appendOffset("+HHMM", "Z")
+            .toFormatter(Locale.US);
+
+    @Override
+    @SuppressWarnings("NonSynchronizedMethodOverridesSynchronizedMethod")
+    public String formatMessage(LogRecord record)
+    {
+        return format(record);
+    }
+
+    @Override
+    public String format(LogRecord record)
+    {
+        ZonedDateTime timestamp = ZonedDateTime.ofInstant(Instant.ofEpochMilli(record.getMillis()), SYSTEM_ZONE);
+
+        StringWriter stringWriter = new StringWriter()
+                .append(TIMESTAMP_FORMATTER.format(timestamp))
+                .append('\t')
+                .append(Level.fromJulLevel(record.getLevel()).name())
+                .append('\t')
+                .append(Thread.currentThread().getName())
+                .append('\t')
+                .append(record.getLoggerName())
+                .append('\t')
+                .append(record.getMessage());
+
+        if (record.getThrown() != null) {
+            stringWriter.append('\n');
+            record.getThrown().printStackTrace(new PrintWriter(stringWriter));
+            stringWriter.append('\n');
+        }
+
+        stringWriter.append('\n');
+        return stringWriter.toString();
+    }
+}

proofpoint/platform

log/src/main/java/com/proofpoint/log/Level.java
@@ -1,0 +1,41 @@
+package com.proofpoint.log;
+
+enum Level
+{
+    OFF(java.util.logging.Level.OFF),
+    DEBUG(java.util.logging.Level.FINE),
+    INFO(java.util.logging.Level.INFO),
+    WARN(java.util.logging.Level.WARNING),
+    ERROR(java.util.logging.Level.SEVERE);
+
+    private final java.util.logging.Level julLevel;
+
+    Level(java.util.logging.Level julLevel)
+    {
+        this.julLevel = julLevel;
+    }
+
+    public java.util.logging.Level toJulLevel()
+    {
+        return julLevel;
+    }
+
+    static Level fromJulLevel(java.util.logging.Level level)
+    {
+        // Convert any implementation of Level based on the int value
+        int levelValue = level.intValue();
+        if (levelValue == java.util.logging.Level.OFF.intValue()) {
+            return OFF;
+        }
+        if (levelValue >= java.util.logging.Level.SEVERE.intValue()) {
+            return ERROR;
+        }
+        if (levelValue >= java.util.logging.Level.WARNING.intValue()) {
+            return WARN;
+        }
+        if (levelValue >= java.util.logging.Level.INFO.intValue()) {
+            return INFO;
+        }
+        return DEBUG;
+    }
+}

log/src/main/java/com/proofpoint/log/OutputStreamHandler.java
@@ -1,0 +1,71 @@
+package com.proofpoint.log;
+
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.logging.ErrorManager.CLOSE_FAILURE;
+import static java.util.logging.ErrorManager.FLUSH_FAILURE;
+import static java.util.logging.ErrorManager.FORMAT_FAILURE;
+import static java.util.logging.ErrorManager.WRITE_FAILURE;
+
+final class OutputStreamHandler
+        extends Handler
+{
+    private final Writer writer;
+
+    public OutputStreamHandler(OutputStream out)
+    {
+        writer = new OutputStreamWriter(out, UTF_8);
+        setFormatter(new StaticFormatter());
+    }
+
+    @Override
+    public void publish(LogRecord record)
+    {
+        if (!isLoggable(record)) {
+            return;
+        }
+
+        String message;
+        try {
+            message = getFormatter().format(record);
+        }
+        catch (Exception e) {
+            reportError(null, e, FORMAT_FAILURE);
+            return;
+        }
+
+        try {
+            writer.write(message);
+        }
+        catch (Exception e) {
+            reportError(null, e, WRITE_FAILURE);
+        }
+    }
+
+    @Override
+    public void flush()
+    {
+        try {
+            writer.flush();
+        }
+        catch (Exception e) {
+            reportError(null, e, FLUSH_FAILURE);
+        }
+    }
+
+    @Override
+    public void close()
+    {
+        try {
+            writer.flush();
+        }
+        catch (Exception e) {
+            reportError(null, e, CLOSE_FAILURE);
+        }
+    }
+}

log/src/main/java/com/proofpoint/log/StaticFormatter.java
@@ -1,0 +1,78 @@
+package com.proofpoint.log;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.SignStyle;
+import java.util.Locale;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+import static java.time.temporal.ChronoField.DAY_OF_MONTH;
+import static java.time.temporal.ChronoField.HOUR_OF_DAY;
+import static java.time.temporal.ChronoField.MILLI_OF_SECOND;
+import static java.time.temporal.ChronoField.MINUTE_OF_HOUR;
+import static java.time.temporal.ChronoField.MONTH_OF_YEAR;
+import static java.time.temporal.ChronoField.SECOND_OF_MINUTE;
+import static java.time.temporal.ChronoField.YEAR;
+
+class StaticFormatter
+        extends Formatter
+{
+    private static final ZoneId SYSTEM_ZONE = ZoneId.systemDefault().normalized();
+
+    private static final DateTimeFormatter TIMESTAMP_FORMATTER = new DateTimeFormatterBuilder()
+            .parseCaseInsensitive()
+            .appendValue(YEAR, 4, 10, SignStyle.EXCEEDS_PAD)
+            .appendLiteral('-')
+            .appendValue(MONTH_OF_YEAR, 2)
+            .appendLiteral('-')
+            .appendValue(DAY_OF_MONTH, 2)
+            .appendLiteral('T')
+            .appendValue(HOUR_OF_DAY, 2)
+            .appendLiteral(':')
+            .appendValue(MINUTE_OF_HOUR, 2)
+            .appendLiteral(':')
+            .appendValue(SECOND_OF_MINUTE, 2)
+            .appendLiteral('.')
+            .appendValue(MILLI_OF_SECOND, 3)
+            .appendOffset("+HHMM", "Z")
+            .toFormatter(Locale.US);
+
+    @Override
+    @SuppressWarnings("NonSynchronizedMethodOverridesSynchronizedMethod")
+    public String formatMessage(LogRecord record)
+    {
+        return format(record);
+    }
+
+    @Override
+    public String format(LogRecord record)
+    {
+        ZonedDateTime timestamp = ZonedDateTime.ofInstant(Instant.ofEpochMilli(record.getMillis()), SYSTEM_ZONE);
+
+        StringWriter stringWriter = new StringWriter()
+                .append(TIMESTAMP_FORMATTER.format(timestamp))
+                .append('\t')
+                .append(Level.fromJulLevel(record.getLevel()).name())
+                .append('\t')
+                .append(Thread.currentThread().getName())
+                .append('\t')
+                .append(record.getLoggerName())
+                .append('\t')
+                .append(record.getMessage());
+
+        if (record.getThrown() != null) {
+            stringWriter.append('\n');
+            record.getThrown().printStackTrace(new PrintWriter(stringWriter));
+            stringWriter.append('\n');
+        }
+
+        stringWriter.append('\n');
+        return stringWriter.toString();
+    }
+}