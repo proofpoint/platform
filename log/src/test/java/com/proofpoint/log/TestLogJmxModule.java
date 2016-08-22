airlift/airlift

Upgrade jmxutils to fix LogJmxModule

log-manager/src/test/java/io/airlift/log/TestLogJmxModule.java
@@ -1,0 +1,18 @@
+package io.airlift.log;
+
+import com.google.inject.Guice;
+import com.google.inject.Stage;
+import org.testng.annotations.Test;
+import org.weakref.jmx.testing.TestingMBeanModule;
+
+public class TestLogJmxModule
+{
+    @Test
+    public void testModule()
+            throws Exception
+    {
+        Guice.createInjector(Stage.PRODUCTION,
+                new TestingMBeanModule(),
+                new LogJmxModule());
+    }
+}

proofpoint/platform

log/src/test/java/com/proofpoint/log/TestLogJmxModule.java
@@ -1,0 +1,19 @@
+package com.proofpoint.log;
+
+import com.google.inject.Guice;
+import com.google.inject.Stage;
+import org.testng.annotations.Test;
+import org.weakref.jmx.testing.TestingMBeanModule;
+
+public class TestLogJmxModule
+{
+    @Test
+    public void testModule()
+            throws Exception
+    {
+        Guice.createInjector(Stage.PRODUCTION,
+                new TestingMBeanModule(),
+                new LogJmxModule());
+    }
+}
+
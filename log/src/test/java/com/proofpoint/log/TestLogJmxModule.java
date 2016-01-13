9299a8e0a12808111fc78041e07ba3f97ae2f73d
David Phillips <david@acz.org>
2016-01-13 13:20:54 -0800

    Upgrade jmxutils to fix LogJmxModule

---
+++ log/src/test/java/com/proofpoint/log/TestLogJmxModule.java
@@ -0,0 +1,19 @@
+package com.proofpoint.log;
+
+import com.google.inject.Guice;
+import com.google.inject.Stage;
+import org.testng.annotations.Test;
+import org.weakref.jmx.testing.TestingMBeanModule;
+
+public class TestLogJmxModule
+{
+    @Test
+    public void testModule()
+            throws Exception
+    {
+        Guice.createInjector(Stage.PRODUCTION,
+                new TestingMBeanModule(),
+                new LogJmxModule());
+    }
+}
+
