electrum/airlift

Add HTTP/2 support in HTTP client

http-client/src/test/java/io/airlift/http/client/jetty/TestJettyHttpClientHttp2.java
@@ -1,0 +1,14 @@
+package io.airlift.http.client.jetty;
+
+import io.airlift.http.client.HttpClientConfig;
+
+public class TestJettyHttpClientHttp2
+        extends TestJettyHttpClient
+{
+    @Override
+    protected HttpClientConfig createClientConfig()
+    {
+        return super.createClientConfig()
+                .setHttp2Enabled(true);
+    }
+}

proofpoint/platform

http-client/src/test/java/com/proofpoint/http/client/jetty/TestJettyHttpClientHttp2.java
@@ -1,0 +1,14 @@
+package com.proofpoint.http.client.jetty;
+
+import com.proofpoint.http.client.HttpClientConfig;
+
+public class TestJettyHttpClientHttp2
+        extends TestJettyHttpClient
+{
+    @Override
+    protected HttpClientConfig createClientConfig()
+    {
+        return super.createClientConfig()
+                .setHttp2Enabled(true);
+    }
+}
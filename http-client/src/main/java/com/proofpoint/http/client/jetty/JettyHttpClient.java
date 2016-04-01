25ca8edefceaf9f81ec833ad670ef7a50d1e7db9
David Phillips <david@acz.org>
2016-04-01 09:35:31 -0700

    Add HTTP/2 support in HTTP client

--- http-client/src/main/java/com/proofpoint/http/client/jetty/JettyHttpClient.java
+++ http-client/src/main/java/com/proofpoint/http/client/jetty/JettyHttpClient.java
@@ -30,6 +30,7 @@ import com.proofpoint.tracetoken.TraceTokenScope;
 import com.proofpoint.units.Duration;
 import org.eclipse.jetty.client.DuplexConnectionPool;
 import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.HttpClientTransport;
 import org.eclipse.jetty.client.HttpDestination;
 import org.eclipse.jetty.client.HttpExchange;
 import org.eclipse.jetty.client.HttpRequest;
@@ -49,6 +50,8 @@ import org.eclipse.jetty.client.util.InputStreamContentProvider;
 import org.eclipse.jetty.client.util.InputStreamResponseListener;
 import org.eclipse.jetty.http.HttpFields;
 import org.eclipse.jetty.http.HttpHeader;
+import org.eclipse.jetty.http2.client.HTTP2Client;
+import org.eclipse.jetty.http2.client.http.HttpClientTransportOverHTTP2;
 import org.eclipse.jetty.util.ArrayQueue;
 import org.eclipse.jetty.util.HttpCookieStore;
 import org.eclipse.jetty.util.ssl.SslContextFactory;
@@ -119,6 +122,7 @@ public class JettyHttpClient
     private static final AtomicLong nameCounter = new AtomicLong();
     private static final String PLATFORM_STATS_KEY = "platform_stats";
     private static final long SWEEP_PERIOD_MILLIS = 5000;
+    private static final int CLIENT_TRANSPORT_SELECTORS = 2;
 
     private final JettyIoPool anonymousPool;
     private final HttpClient httpClient;
@@ -194,23 +198,35 @@ public class JettyHttpClient
             sslContextFactory.setKeyStorePassword(config.getKeyStorePassword());
         }
 
-        if (config.getMaxRequestsQueuedPerDestination() == 0) {
-            httpClient = new HttpClient(
-                    new HttpClientTransportOverHTTP(2)
-                    {
-                        @Override
-                        public HttpDestination newHttpDestination(Origin origin)
-                        {
-                            return new LimitQueuedToAvailableConnectionsHttpDestination(config.getMaxConnectionsPerServer(), getHttpClient(), origin);
-                        }
-                    },
-                    sslContextFactory);
-            httpClient.setMaxRequestsQueuedPerDestination(config.getMaxConnectionsPerServer());
+        HttpClientTransport transport;
+        if (config.isHttp2Enabled()) {
+            HTTP2Client client = new HTTP2Client();
+            client.setSelectors(CLIENT_TRANSPORT_SELECTORS);
+            transport = new HttpClientTransportOverHTTP2(client);
         }
         else {
-            httpClient = new HttpClient(new HttpClientTransportOverHTTP(2), sslContextFactory);
-            httpClient.setMaxRequestsQueuedPerDestination(config.getMaxRequestsQueuedPerDestination());
+            transport = new HttpClientTransportOverHTTP(CLIENT_TRANSPORT_SELECTORS);
         }
+
+//TODO
+//        if (config.getMaxRequestsQueuedPerDestination() == 0) {
+//            httpClient = new HttpClient(
+//                    new HttpClientTransportOverHTTP(2)
+//                    {
+//                        @Override
+//                        public HttpDestination newHttpDestination(Origin origin)
+//                        {
+//                            return new LimitQueuedToAvailableConnectionsHttpDestination(config.getMaxConnectionsPerServer(), getHttpClient(), origin);
+//                        }
+//                    },
+//                    sslContextFactory);
+//            httpClient.setMaxRequestsQueuedPerDestination(config.getMaxConnectionsPerServer());
+//        }
+//        else {
+            httpClient = new HttpClient(transport, sslContextFactory);
+            httpClient.setMaxRequestsQueuedPerDestination(config.getMaxRequestsQueuedPerDestination());
+//        }
+
         httpClient.setMaxConnectionsPerDestination(config.getMaxConnectionsPerServer());
 
         // disable cookies
@@ -378,6 +394,9 @@ public class JettyHttpClient
             Throwable cause = e.getCause();
             if (cause instanceof Exception) {
                 return responseHandler.handleException(request, (Exception) cause);
+            }
+            else if ((cause instanceof NoClassDefFoundError) && cause.getMessage().endsWith("ALPNClientConnection")) {
+                return responseHandler.handleException(request, new RuntimeException("HTTPS cannot be used when HTTP/2 is enabled", cause));
             }
             else {
                 return responseHandler.handleException(request, new RuntimeException(cause));
