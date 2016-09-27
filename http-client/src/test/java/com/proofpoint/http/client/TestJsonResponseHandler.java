1633e617db6cb3df4c83007b80c416b1499777bf
David Phillips <david@acz.org>
2016-09-26 18:27:58 -0700

    Move type name to end of JsonCodec error message

--- http-client/src/test/java/com/proofpoint/http/client/TestJsonResponseHandler.java
+++ http-client/src/test/java/com/proofpoint/http/client/TestJsonResponseHandler.java
@@ -54,7 +54,7 @@ public class TestJsonResponseHandler
         catch (IllegalArgumentException e) {
             assertEquals(e.getMessage(), "Unable to create " + User.class + " from JSON response:\n" + json);
             assertInstanceOf(e.getCause(), IllegalArgumentException.class);
-            assertEquals(e.getCause().getMessage(), "Invalid [simple type, class com.proofpoint.http.client.TestFullJsonResponseHandler$User] json bytes");
+            assertEquals(e.getCause().getMessage(), "Invalid json bytes for [simple type, class com.proofpoint.http.client.TestFullJsonResponseHandler$User]");
         }
     }
 
