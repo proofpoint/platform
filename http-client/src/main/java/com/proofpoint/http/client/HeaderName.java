electrum/airlift

Make HTTP client response headers case insensitive

http-client/src/main/java/io/airlift/http/client/HeaderName.java
@@ -1,0 +1,47 @@
+package io.airlift.http.client;
+
+import static java.util.Locale.ENGLISH;
+import static java.util.Objects.requireNonNull;
+
+public final class HeaderName
+{
+    private final String original;
+    private final String lowerCase;
+
+    public static HeaderName of(String value)
+    {
+        return new HeaderName(value);
+    }
+
+    private HeaderName(String value)
+    {
+        requireNonNull(value, "value is null");
+        this.original = value;
+        this.lowerCase = value.toLowerCase(ENGLISH);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) {
+            return true;
+        }
+        if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+        HeaderName other = (HeaderName) obj;
+        return lowerCase.equals(other.lowerCase);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return lowerCase.hashCode();
+    }
+
+    @Override
+    public String toString()
+    {
+        return original;
+    }
+}

http-client/src/main/java/io/airlift/http/client/jetty/JettyHttpClient.java
@@ -11,6 +11,7 @@ import com.google.common.net.HostAndPort;
 import com.google.common.primitives.Ints;
 import com.google.common.util.concurrent.AbstractFuture;
 import io.airlift.http.client.BodyGenerator;
+import io.airlift.http.client.HeaderName;
 import io.airlift.http.client.HttpClientConfig;
 import io.airlift.http.client.HttpRequestFilter;
 import io.airlift.http.client.Request;
@@ -648,11 +649,13 @@ public class JettyHttpClient
     {
         private final Response response;
         private final CountingInputStream inputStream;
+        private final ListMultimap<HeaderName, String> headers;
 
         public JettyResponse(Response response, InputStream inputStream)
         {
             this.response = response;
             this.inputStream = new CountingInputStream(inputStream);
+            this.headers = toHeadersMap(response.getHeaders());
         }
 
         @Override
@@ -668,23 +671,9 @@ public class JettyHttpClient
         }
 
         @Override
-        public String getHeader(String name)
+        public ListMultimap<HeaderName, String> getHeaders()
         {
-            return response.getHeaders().get(name);
-        }
-
-        @Override
-        public ListMultimap<String, String> getHeaders()
-        {
-            HttpFields headers = response.getHeaders();
-
-            ImmutableListMultimap.Builder<String, String> builder = ImmutableListMultimap.builder();
-            for (String name : headers.getFieldNamesCollection()) {
-                for (String value : headers.getValuesList(name)) {
-                    builder.put(name, value);
-                }
-            }
-            return builder.build();
+            return headers;
         }
 
         @Override
@@ -707,6 +696,17 @@ public class JettyHttpClient
                     .add("statusMessage", getStatusMessage())
                     .add("headers", getHeaders())
                     .toString();
+        }
+
+        private static ListMultimap<HeaderName, String> toHeadersMap(HttpFields headers)
+        {
+            ImmutableListMultimap.Builder<HeaderName, String> builder = ImmutableListMultimap.builder();
+            for (String name : headers.getFieldNamesCollection()) {
+                for (String value : headers.getValuesList(name)) {
+                    builder.put(HeaderName.of(name), value);
+                }
+            }
+            return builder.build();
         }
     }
 

http-client/src/main/java/io/airlift/http/client/testing/TestingResponse.java
@@ -6,13 +6,14 @@ import com.google.common.collect.ListMultimap;
 import com.google.common.io.CountingInputStream;
 import com.google.common.net.HttpHeaders;
 import com.google.common.net.MediaType;
+import io.airlift.http.client.HeaderName;
 import io.airlift.http.client.HttpStatus;
 import io.airlift.http.client.Response;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
+import java.util.Map;
 
 import static com.google.common.base.MoreObjects.toStringHelper;
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -21,7 +22,7 @@ public class TestingResponse
         implements Response
 {
     private final HttpStatus status;
-    private final ListMultimap<String, String> headers;
+    private final ListMultimap<HeaderName, String> headers;
     private final CountingInputStream countingInputStream;
 
     public TestingResponse(HttpStatus status, ListMultimap<String, String> headers, byte[] bytes)
@@ -32,7 +33,7 @@ public class TestingResponse
     public TestingResponse(HttpStatus status, ListMultimap<String, String> headers, InputStream input)
     {
         this.status = checkNotNull(status, "status is null");
-        this.headers = ImmutableListMultimap.copyOf(checkNotNull(headers, "headers is null"));
+        this.headers = ImmutableListMultimap.copyOf(toHeaderMap(checkNotNull(headers, "headers is null")));
         this.countingInputStream = new CountingInputStream(checkNotNull(input, "input is null"));
     }
 
@@ -49,14 +50,7 @@ public class TestingResponse
     }
 
     @Override
-    public String getHeader(String name)
-    {
-        List<String> list = getHeaders().get(name);
-        return list.isEmpty() ? null : list.get(0);
-    }
-
-    @Override
-    public ListMultimap<String, String> getHeaders()
+    public ListMultimap<HeaderName, String> getHeaders()
     {
         return headers;
     }
@@ -84,7 +78,6 @@ public class TestingResponse
                 .toString();
     }
 
-
     public static ListMultimap<String, String> contentType(MediaType type)
     {
         return ImmutableListMultimap.of(HttpHeaders.CONTENT_TYPE, type.toString());
@@ -93,5 +86,14 @@ public class TestingResponse
     public static Response mockResponse(HttpStatus status, MediaType type, String content)
     {
         return new TestingResponse(status, contentType(type), content.getBytes(Charsets.UTF_8));
+    }
+
+    private static ListMultimap<HeaderName, String> toHeaderMap(ListMultimap<String, String> headers)
+    {
+        ImmutableListMultimap.Builder<HeaderName, String> builder = ImmutableListMultimap.builder();
+        for (Map.Entry<String, String> entry : headers.entries()) {
+            builder.put(HeaderName.of(entry.getKey()), entry.getValue());
+        }
+        return builder.build();
     }
 }

http-client/src/test/java/io/airlift/http/client/AbstractHttpClientTest.java
@@ -1,8 +1,8 @@
 package io.airlift.http.client;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ListMultimap;
 import io.airlift.http.client.HttpClient.HttpResponseFuture;
+import io.airlift.http.client.StatusResponseHandler.StatusResponse;
 import io.airlift.http.client.jetty.JettyHttpClient;
 import io.airlift.log.Logging;
 import io.airlift.testing.Closeables;
@@ -68,6 +68,7 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.fail;
 
 @Test(singleThreaded = true)
@@ -360,6 +361,24 @@ public abstract class AbstractHttpClientTest
     }
 
     @Test
+    public void testResponseHeadersCaseInsensitive()
+            throws Exception
+    {
+        URI uri = baseURI.resolve("/road/to/nowhere");
+        Request request = prepareGet()
+                .setUri(uri)
+                .build();
+
+        Response response = executeRequest(request, new PassThroughResponseHandler());
+
+        assertNotNull(response.getHeader("date"));
+        assertNotNull(response.getHeader("DATE"));
+
+        assertEquals(response.getHeaders("date").size(), 1);
+        assertEquals(response.getHeaders("DATE").size(), 1);
+    }
+
+    @Test
     public void testQuotedSpace()
         throws Exception
     {
@@ -383,19 +402,19 @@ public abstract class AbstractHttpClientTest
                 .setUri(uri)
                 .build();
 
-        ListMultimap<String, String> headers1 = executeRequest(request, createStatusResponseHandler()).getHeaders();
+        StatusResponse response1 = executeRequest(request, createStatusResponseHandler());
         Thread.sleep(1000);
-        ListMultimap<String, String> headers2 = executeRequest(request, createStatusResponseHandler()).getHeaders();
+        StatusResponse response2 = executeRequest(request, createStatusResponseHandler());
         Thread.sleep(1000);
-        ListMultimap<String, String> headers3 = executeRequest(request, createStatusResponseHandler()).getHeaders();
+        StatusResponse response3 = executeRequest(request, createStatusResponseHandler());
 
-        assertEquals(headers1.get("remotePort").size(), 1);
-        assertEquals(headers2.get("remotePort").size(), 1);
-        assertEquals(headers3.get("remotePort").size(), 1);
+        assertNotNull(response1.getHeader("remotePort"));
+        assertNotNull(response2.getHeader("remotePort"));
+        assertNotNull(response3.getHeader("remotePort"));
 
-        int port1 = Integer.parseInt(headers1.get("remotePort").get(0));
-        int port2 = Integer.parseInt(headers2.get("remotePort").get(0));
-        int port3 = Integer.parseInt(headers3.get("remotePort").get(0));
+        int port1 = Integer.parseInt(response1.getHeader("remotePort"));
+        int port2 = Integer.parseInt(response2.getHeader("remotePort"));
+        int port3 = Integer.parseInt(response3.getHeader("remotePort"));
 
         assertEquals(port2, port1);
         assertEquals(port3, port1);
@@ -547,10 +566,10 @@ public abstract class AbstractHttpClientTest
                 .setUri(baseURI)
                 .build();
 
-        ListMultimap<String, String> headers = executeRequest(request, createStatusResponseHandler()).getHeaders();
+        StatusResponse response = executeRequest(request, createStatusResponseHandler());
 
-        assertEquals(headers.get("foo"), ImmutableList.of("bar"));
-        assertEquals(headers.get("dupe"), ImmutableList.of("first", "second"));
+        assertEquals(response.getHeaders("foo"), ImmutableList.of("bar"));
+        assertEquals(response.getHeaders("dupe"), ImmutableList.of("first", "second"));
     }
 
     @Test
@@ -826,6 +845,22 @@ public abstract class AbstractHttpClientTest
                 throws Exception
         {
             throw new UnsupportedOperationException();
+        }
+    }
+
+    private static class PassThroughResponseHandler
+            implements ResponseHandler<Response, RuntimeException>
+    {
+        @Override
+        public Response handleException(Request request, Exception exception)
+        {
+            throw propagate(exception);
+        }
+
+        @Override
+        public Response handle(Request request, Response response)
+        {
+            return response;
         }
     }
 

http-client/src/test/java/io/airlift/http/client/TestHeaderName.java
@@ -1,0 +1,25 @@
+package io.airlift.http.client;
+
+import org.testng.annotations.Test;
+
+import static io.airlift.testing.EquivalenceTester.equivalenceTester;
+import static org.testng.Assert.assertEquals;
+
+public class TestHeaderName
+{
+    @Test
+    public void testEquivalence()
+    {
+        equivalenceTester()
+                .addEquivalentGroup(HeaderName.of("foo"), HeaderName.of("FOO"), HeaderName.of("foo"))
+                .addEquivalentGroup(HeaderName.of("bar"), HeaderName.of("BAR"), HeaderName.of("bar"))
+                .check();
+    }
+
+    @Test
+    public void testToString()
+    {
+        assertEquals(HeaderName.of("FOO").toString(), "FOO");
+        assertEquals(HeaderName.of("foo").toString(), "foo");
+    }
+}

proofpoint/platform

http-client/src/main/java/com/proofpoint/http/client/HeaderName.java
@@ -1,0 +1,47 @@
+package com.proofpoint.http.client;
+
+import static java.util.Locale.ENGLISH;
+import static java.util.Objects.requireNonNull;
+
+public final class HeaderName
+{
+    private final String original;
+    private final String lowerCase;
+
+    public static HeaderName of(String value)
+    {
+        return new HeaderName(value);
+    }
+
+    private HeaderName(String value)
+    {
+        requireNonNull(value, "value is null");
+        this.original = value;
+        this.lowerCase = value.toLowerCase(ENGLISH);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj) {
+            return true;
+        }
+        if ((obj == null) || (getClass() != obj.getClass())) {
+            return false;
+        }
+        HeaderName other = (HeaderName) obj;
+        return lowerCase.equals(other.lowerCase);
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return lowerCase.hashCode();
+    }
+
+    @Override
+    public String toString()
+    {
+        return original;
+    }
+}

http-client/src/main/java/com/proofpoint/http/client/jetty/JettyHttpClient.java
@@ -16,6 +16,7 @@ import com.google.common.util.concurrent.AbstractFuture;
 import com.proofpoint.http.client.BodySource;
 import com.proofpoint.http.client.DynamicBodySource;
 import com.proofpoint.http.client.DynamicBodySource.Writer;
+import com.proofpoint.http.client.HeaderName;
 import com.proofpoint.http.client.HttpClientConfig;
 import com.proofpoint.http.client.HttpRequestFilter;
 import com.proofpoint.http.client.InputStreamBodySource;
@@ -680,11 +681,13 @@ public class JettyHttpClient
     {
         private final Response response;
         private final CountingInputStream inputStream;
+        private final ListMultimap<HeaderName, String> headers;
 
         JettyResponse(Response response, InputStream inputStream)
         {
             this.response = response;
             this.inputStream = new CountingInputStream(inputStream);
+            this.headers = toHeadersMap(response.getHeaders());
         }
 
         @Override
@@ -700,23 +703,9 @@ public class JettyHttpClient
         }
 
         @Override
-        public String getHeader(String name)
+        public ListMultimap<HeaderName, String> getHeaders()
         {
-            return response.getHeaders().get(name);
-        }
-
-        @Override
-        public ListMultimap<String, String> getHeaders()
-        {
-            HttpFields headers = response.getHeaders();
-
-            ImmutableListMultimap.Builder<String, String> builder = ImmutableListMultimap.builder();
-            for (String name : headers.getFieldNamesCollection()) {
-                for (String value : headers.getValuesList(name)) {
-                    builder.put(name, value);
-                }
-            }
-            return builder.build();
+            return headers;
         }
 
         @Override
@@ -739,6 +728,17 @@ public class JettyHttpClient
                     .add("statusMessage", getStatusMessage())
                     .add("headers", getHeaders())
                     .toString();
+        }
+
+        private static ListMultimap<HeaderName, String> toHeadersMap(HttpFields headers)
+        {
+            ImmutableListMultimap.Builder<HeaderName, String> builder = ImmutableListMultimap.builder();
+            for (String name : headers.getFieldNamesCollection()) {
+                for (String value : headers.getValuesList(name)) {
+                    builder.put(HeaderName.of(name), value);
+                }
+            }
+            return builder.build();
         }
     }
 

http-client/src/main/java/com/proofpoint/http/client/testing/TestingResponse.java
@@ -9,6 +9,7 @@ import com.google.common.collect.ListMultimap;
 import com.google.common.io.CountingInputStream;
 import com.google.common.net.HttpHeaders;
 import com.google.common.net.MediaType;
+import com.proofpoint.http.client.HeaderName;
 import com.proofpoint.http.client.HttpStatus;
 import com.proofpoint.http.client.Response;
 import com.proofpoint.json.ObjectMapperProvider;
@@ -19,7 +20,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Map;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -32,7 +33,7 @@ public class TestingResponse
         implements Response
 {
     private final HttpStatus status;
-    private final ListMultimap<String, String> headers;
+    private final ListMultimap<HeaderName, String> headers;
     private final CountingInputStream countingInputStream;
 
     /**
@@ -52,7 +53,7 @@ public class TestingResponse
     public TestingResponse(HttpStatus status, ListMultimap<String, String> headers, InputStream input)
     {
         this.status = requireNonNull(status, "status is null");
-        this.headers = ImmutableListMultimap.copyOf(requireNonNull(headers, "headers is null"));
+        this.headers = ImmutableListMultimap.copyOf(toHeaderMap(checkNotNull(headers, "headers is null")));
         this.countingInputStream = new CountingInputStream(requireNonNull(input, "input is null"));
     }
 
@@ -69,14 +70,7 @@ public class TestingResponse
     }
 
     @Override
-    public String getHeader(String name)
-    {
-        List<String> list = getHeaders().get(name);
-        return list.isEmpty() ? null : list.get(0);
-    }
-
-    @Override
-    public ListMultimap<String, String> getHeaders()
+    public ListMultimap<HeaderName, String> getHeaders()
     {
         return headers;
     }
@@ -288,5 +282,14 @@ public class TestingResponse
 
             return new TestingResponse(status, headers, firstNonNull(bytes, ZERO_LENGTH_BYTES));
         }
+    }
+
+    private static ListMultimap<HeaderName, String> toHeaderMap(ListMultimap<String, String> headers)
+    {
+        ImmutableListMultimap.Builder<HeaderName, String> builder = ImmutableListMultimap.builder();
+        for (Map.Entry<String, String> entry : headers.entries()) {
+            builder.put(HeaderName.of(entry.getKey()), entry.getValue());
+        }
+        return builder.build();
     }
 }

http-client/src/test/java/com/proofpoint/http/client/AbstractHttpClientTest.java
@@ -2,9 +2,9 @@ package com.proofpoint.http.client;
 
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ListMultimap;
 import com.proofpoint.http.client.DynamicBodySource.Writer;
 import com.proofpoint.http.client.HttpClient.HttpResponseFuture;
+import com.proofpoint.http.client.StatusResponseHandler.StatusResponse;
 import com.proofpoint.http.client.jetty.JettyHttpClient;
 import com.proofpoint.log.Logging;
 import com.proofpoint.testing.Assertions;
@@ -84,6 +84,7 @@ import static java.util.concurrent.Executors.newFixedThreadPool;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 
@@ -393,6 +394,24 @@ public abstract class AbstractHttpClientTest
     }
 
     @Test
+    public void testResponseHeadersCaseInsensitive()
+            throws Exception
+    {
+        URI uri = baseURI.resolve("/road/to/nowhere");
+        Request request = prepareGet()
+                .setUri(uri)
+                .build();
+
+        Response response = executeRequest(request, new PassThroughResponseHandler());
+
+        assertNotNull(response.getHeader("date"));
+        assertNotNull(response.getHeader("DATE"));
+
+        assertEquals(response.getHeaders("date").size(), 1);
+        assertEquals(response.getHeaders("DATE").size(), 1);
+    }
+
+    @Test
     public void testKeepAlive()
             throws Exception
     {
@@ -401,19 +420,19 @@ public abstract class AbstractHttpClientTest
                 .setUri(uri)
                 .build();
 
-        ListMultimap<String, String> headers1 = executeRequest(request, createStatusResponseHandler()).getHeaders();
+        StatusResponse response1 = executeRequest(request, createStatusResponseHandler());
         Thread.sleep(1000);
-        ListMultimap<String, String> headers2 = executeRequest(request, createStatusResponseHandler()).getHeaders();
+        StatusResponse response2 = executeRequest(request, createStatusResponseHandler());
         Thread.sleep(1000);
-        ListMultimap<String, String> headers3 = executeRequest(request, createStatusResponseHandler()).getHeaders();
+        StatusResponse response3 = executeRequest(request, createStatusResponseHandler());
 
-        assertEquals(headers1.get("remotePort").size(), 1);
-        assertEquals(headers2.get("remotePort").size(), 1);
-        assertEquals(headers3.get("remotePort").size(), 1);
+        assertNotNull(response1.getHeader("remotePort"));
+        assertNotNull(response2.getHeader("remotePort"));
+        assertNotNull(response3.getHeader("remotePort"));
 
-        int port1 = Integer.parseInt(headers1.get("remotePort").get(0));
-        int port2 = Integer.parseInt(headers2.get("remotePort").get(0));
-        int port3 = Integer.parseInt(headers3.get("remotePort").get(0));
+        int port1 = Integer.parseInt(response1.getHeader("remotePort"));
+        int port2 = Integer.parseInt(response2.getHeader("remotePort"));
+        int port3 = Integer.parseInt(response3.getHeader("remotePort"));
 
         assertEquals(port2, port1);
         assertEquals(port3, port1);
@@ -1052,10 +1071,10 @@ public abstract class AbstractHttpClientTest
                 .setUri(baseURI)
                 .build();
 
-        ListMultimap<String, String> headers = executeRequest(request, createStatusResponseHandler()).getHeaders();
+        StatusResponse response = executeRequest(request, createStatusResponseHandler());
 
-        assertEquals(headers.get("foo"), ImmutableList.of("bar"));
-        assertEquals(headers.get("dupe"), ImmutableList.of("first", "second"));
+        assertEquals(response.getHeaders("foo"), ImmutableList.of("bar"));
+        assertEquals(response.getHeaders("dupe"), ImmutableList.of("first", "second"));
     }
 
     @Test
@@ -1524,6 +1543,22 @@ public abstract class AbstractHttpClientTest
                 throws Exception
         {
             throw new UnsupportedOperationException();
+        }
+    }
+
+    private static class PassThroughResponseHandler
+            implements ResponseHandler<Response, RuntimeException>
+    {
+        @Override
+        public Response handleException(Request request, Exception exception)
+        {
+            throw propagate(exception);
+        }
+
+        @Override
+        public Response handle(Request request, Response response)
+        {
+            return response;
         }
     }
 

http-client/src/test/java/com/proofpoint/http/client/TestHeaderName.java
@@ -1,0 +1,25 @@
+package com.proofpoint.http.client;
+
+import org.testng.annotations.Test;
+
+import static com.proofpoint.testing.EquivalenceTester.equivalenceTester;
+import static org.testng.Assert.assertEquals;
+
+public class TestHeaderName
+{
+    @Test
+    public void testEquivalence()
+    {
+        equivalenceTester()
+                .addEquivalentGroup(HeaderName.of("foo"), HeaderName.of("FOO"), HeaderName.of("foo"))
+                .addEquivalentGroup(HeaderName.of("bar"), HeaderName.of("BAR"), HeaderName.of("bar"))
+                .check();
+    }
+
+    @Test
+    public void testToString()
+    {
+        assertEquals(HeaderName.of("FOO").toString(), "FOO");
+        assertEquals(HeaderName.of("foo").toString(), "foo");
+    }
+}