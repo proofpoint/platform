abd143114d2193a1842f2b1a74b35e7126b0f3d3
John Gardiner Myers <jgmyers@proofpoint.com>
2017-05-18 15:37:58 -0700

    Implement DecorrelatedJitteredBackoffPolicy

---
+++ http-client/src/main/java/com/proofpoint/http/client/balancing/DecorrelatedJitteredBackoffPolicy.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2017 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.http.client.balancing;
+
+import com.proofpoint.units.Duration;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+public class DecorrelatedJitteredBackoffPolicy
+    implements BackoffPolicy
+{
+    private final long min;
+    private final long max;
+
+    public DecorrelatedJitteredBackoffPolicy(Duration min, Duration max)
+    {
+        this.min = min.roundTo(TimeUnit.NANOSECONDS);
+        this.max = max.roundTo(TimeUnit.NANOSECONDS);
+        checkArgument(this.min <= this.max, "min is greater than max");
+    }
+
+    @Override
+    public BackoffPolicy nextAttempt()
+    {
+        return this;
+    }
+
+    @Override
+    public Duration backoff(Duration previousBackoff)
+    {
+        long prev = previousBackoff.roundTo(TimeUnit.NANOSECONDS);
+        long range = Math.abs(prev * 3 - min);
+        long randBackoff;
+        if (range == 0) {
+            randBackoff = min;
+        } else {
+            randBackoff = min + ThreadLocalRandom.current().nextLong(range);
+        }
+        long backoff = Math.min(max, randBackoff);
+
+        return new Duration(backoff, TimeUnit.NANOSECONDS);
+    }
+}
---
+++ http-client/src/test/java/com/proofpoint/http/client/balancing/TestDecorrelatedJitteredBackoffPolicy.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.http.client.balancing;
+
+import com.proofpoint.units.Duration;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.proofpoint.testing.Assertions.assertGreaterThanOrEqual;
+import static com.proofpoint.testing.Assertions.assertLessThanOrEqual;
+
+public class TestDecorrelatedJitteredBackoffPolicy
+{
+    @Test
+    public void testBackoff()
+    {
+        Duration min = new Duration(1, TimeUnit.MILLISECONDS);
+        Duration max = new Duration(5, TimeUnit.SECONDS);
+        BackoffPolicy policy = new DecorrelatedJitteredBackoffPolicy(min, max);
+        for (int i = 0; i < 1000; i++) {
+            Duration last = new Duration(0, TimeUnit.SECONDS);
+            Duration prev = min; // Special-case constraint for the first backoff
+            for (int j = 0; j < 1000; j++) {
+                Duration next = policy.backoff(last);
+                assertGreaterThanOrEqual(next, min);
+                assertLessThanOrEqual(next, new Duration(prev.roundTo(TimeUnit.NANOSECONDS) * 3, TimeUnit.NANOSECONDS));
+                assertLessThanOrEqual(next, max);
+                prev = next;
+                last = next;
+            }
+        }
+    }
+}
