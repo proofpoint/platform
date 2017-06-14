eb570533b7bcbea6b163a6e8517598e6933acbb1
David Phillips <david@acz.org>
2017-06-13 17:03:35 -0700

    Fix another case of UnknownFormatConversionException

--- configuration/src/test/java/com/proofpoint/configuration/ConfigurationFactoryTest.java
+++ configuration/src/test/java/com/proofpoint/configuration/ConfigurationFactoryTest.java
@@ -951,6 +951,22 @@ public class ConfigurationFactoryTest
         }
     }
 
+    @Test
+    public void testFailedCoercion()
+    {
+        Map<String, String> properties = new HashMap<>();
+        properties.put("int-value", "abc %s xyz");  // not an int
+        TestMonitor monitor = new TestMonitor();
+        try {
+            createInjector(properties, null, null, null, monitor, binder -> bindConfig(binder).to(BeanValidationClass.class));
+        }
+        catch (CreationException e) {
+            monitor.assertNumberOfErrors(1);
+            monitor.assertNumberOfWarnings(0);
+            monitor.assertMatchingErrorRecorded("Could not coerce value 'abc %s xyz' to int (property 'int-value')", "BeanValidationClass");
+        }
+    }
+
     private Injector createInjector(Map<String, String> properties, Map<String, String> applicationDefaults, Map<String, String> moduleDefaults, Map<String, ConfigurationDefaultingModule> moduleDefaultSource, TestMonitor monitor, Module module)
     {
         ConfigurationFactory configurationFactory = new ConfigurationFactory(
