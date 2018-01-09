ec9523f1f773a138e26114d1b7522fa13d000d0c
David Phillips <david@acz.org>
2018-01-09 15:28:56 -0800

    Make ScheduledExecutorScheduler a top-level class

--- http-client/src/main/java/com/proofpoint/http/client/jetty/JettyIoPool.java
+++ http-client/src/main/java/com/proofpoint/http/client/jetty/JettyIoPool.java
@@ -1,10 +1,8 @@
 package com.proofpoint.http.client.jetty;
 
-import com.proofpoint.concurrent.Threads;
 import com.proofpoint.reporting.Gauge;
 import org.eclipse.jetty.io.ByteBufferPool;
 import org.eclipse.jetty.io.MappedByteBufferPool;
-import org.eclipse.jetty.util.component.AbstractLifeCycle;
 import org.eclipse.jetty.util.component.LifeCycle;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.eclipse.jetty.util.thread.ScheduledExecutorScheduler;
@@ -12,21 +10,11 @@ import org.eclipse.jetty.util.thread.Scheduler;
 
 import java.io.Closeable;
 import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.TimeUnit;
 
 import static com.google.common.base.MoreObjects.toStringHelper;
-import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Throwables.throwIfUnchecked;
-import static com.proofpoint.concurrent.Threads.daemonThreadsNamed;
 import static java.lang.Math.max;
 import static java.lang.Thread.currentThread;
-import static java.util.Objects.requireNonNull;
 
 public final class JettyIoPool
         implements Closeable
@@ -126,56 +114,5 @@ public final class JettyIoPool
         return toStringHelper(this)
                 .add("name", name)
                 .toString();
-    }
-
-    // based on ScheduledExecutorScheduler
-    private static class ConcurrentScheduler
-            extends AbstractLifeCycle
-            implements Scheduler
-    {
-        private final int threadsPerScheduler;
-        private final ScheduledExecutorService[] schedulers;
-        private final ThreadFactory threadFactory;
-
-        ConcurrentScheduler(int schedulerCount, int threadsPerScheduler, String threadBaseName)
-        {
-            checkArgument(schedulerCount > 0, "schedulerCount must be at least one");
-            this.schedulers = new ScheduledThreadPoolExecutor[schedulerCount];
-            checkArgument(threadsPerScheduler > 0, "threadsPerScheduler must be at least one");
-            this.threadsPerScheduler = threadsPerScheduler;
-            requireNonNull(threadBaseName, "threadBaseName is null");
-            threadFactory = daemonThreadsNamed(threadBaseName + "-timeout-%s");
-        }
-
-        @Override
-        protected void doStart()
-        {
-            for (int i = 0; i < schedulers.length; i++) {
-                ScheduledThreadPoolExecutor scheduledExecutorService = new ScheduledThreadPoolExecutor(threadsPerScheduler, threadFactory);
-                scheduledExecutorService.setRemoveOnCancelPolicy(true);
-                schedulers[i] = scheduledExecutorService;
-            }
-        }
-
-        @Override
-        protected void doStop()
-        {
-            for (int i = 0; i < schedulers.length; i++) {
-                schedulers[i].shutdownNow();
-                schedulers[i] = null;
-            }
-        }
-
-        @Override
-        public Task schedule(Runnable task, long delay, TimeUnit unit)
-        {
-            ScheduledExecutorService scheduler = schedulers[ThreadLocalRandom.current().nextInt(schedulers.length)];
-            if (scheduler == null) {
-                return () -> false;
-            }
-
-            ScheduledFuture<?> result = scheduler.schedule(task, delay, unit);
-            return () -> result.cancel(false);
-        }
     }
 }
