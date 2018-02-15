f396929face2fc5b3fbba5c5e1d40982072425a7
John Gardiner Myers <jgmyers@proofpoint.com>
2018-02-15 10:26:34 -0800

    Allow "-" in node.pool

--- node/src/test/java/com/proofpoint/node/TestNodeConfig.java
+++ node/src/test/java/com/proofpoint/node/TestNodeConfig.java
@@ -90,6 +90,7 @@ public class TestNodeConfig
         assertValidates(new NodeConfig()
                 .setEnvironment("test")
                 .setNodeInternalHostname("ip-3-2.j-p.invalid")
+                .setPool("test_ab-1234")
                 .setNodeId(UUID.randomUUID().toString()));
 
         assertFailsValidation(new NodeConfig().setNodeId("abc/123"), "nodeId", "is malformed", Pattern.class);
