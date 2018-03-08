83446c092da92ffcafbec6d7c677d81eac66431c
David Phillips <david@acz.org>
2018-03-07 22:57:32 -0800

    Simplify addExceptionCallback in MoreFutures

--- concurrent/src/main/java/com/proofpoint/concurrent/MoreFutures.java
+++ concurrent/src/main/java/com/proofpoint/concurrent/MoreFutures.java
@@ -504,22 +504,9 @@ public final class MoreFutures
 
     public static <T> void addExceptionCallback(ListenableFuture<T> future, Runnable exceptionCallback)
     {
-        requireNonNull(future, "future is null");
         requireNonNull(exceptionCallback, "exceptionCallback is null");
 
-        Futures.addCallback(future, new FutureCallback<T>()
-        {
-            @Override
-            public void onSuccess(@Nullable T result)
-            {
-            }
-
-            @Override
-            public void onFailure(Throwable t)
-            {
-                exceptionCallback.run();
-            }
-        }, directExecutor());
+        addExceptionCallback(future, t -> exceptionCallback.run());
     }
 
     private static class UnmodifiableCompletableFuture<V>
