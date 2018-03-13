49972b90cfc8e064c0d85a280da00807a9c72ccd
John Gardiner Myers <jgmyers@proofpoint.com>
2018-03-13 14:31:10 -0700

    Add /liveness resource

---
+++ jaxrs/src/main/java/com/proofpoint/reporting/LivenessResource.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2016 Proofpoint, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.reporting;
+
+import com.proofpoint.jaxrs.AccessDoesNotRequireAuthentication;
+import com.proofpoint.reporting.HealthBeanAttribute.Type;
+
+import javax.inject.Inject;
+import javax.management.AttributeNotFoundException;
+import javax.management.MBeanException;
+import javax.management.ReflectionException;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import static java.util.Objects.requireNonNull;
+import static javax.ws.rs.core.MediaType.TEXT_PLAIN;
+
+@Path("/liveness")
+@AccessDoesNotRequireAuthentication
+public class LivenessResource
+{
+    private final HealthBeanRegistry healthBeanRegistry;
+
+    @Inject
+    public LivenessResource(HealthBeanRegistry healthBeanRegistry)
+    {
+        this.healthBeanRegistry = requireNonNull(healthBeanRegistry, "healthBeanRegistry is null");
+    }
+
+    @GET
+    @Produces(TEXT_PLAIN)
+    public Response get()
+    {
+        StringBuilder sb = new StringBuilder();
+        healthBeanRegistry.getHealthAttributes().values().stream()
+                .filter(healthBeanAttribute -> healthBeanAttribute.getType() == Type.RESTART)
+                .forEach(attribute -> {
+                    try {
+                        String value = attribute.getValue();
+                        if (value != null) {
+                            sb.append(value).append('\n');
+                        }
+                    }
+                    catch (AttributeNotFoundException | MBeanException | ReflectionException e) {
+                        sb.append(e.toString()).append('\n');
+                    }
+                });
+
+        String failures = sb.toString();
+        if (failures.isEmpty()) {
+            return Response.ok("OK").build();
+        }
+        return Response.serverError().entity(failures).build();
+    }
+}
