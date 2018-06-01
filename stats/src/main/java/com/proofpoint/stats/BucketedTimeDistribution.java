6b0c1d9fbb5885ada696c394ec17cf6a403a5322
John Gardiner Myers <jgmyers@proofpoint.com>
2018-06-01 16:44:41 -0700

    Support Prometheus summary type

--- stats/src/main/java/com/proofpoint/stats/BucketedTimeDistribution.java
+++ stats/src/main/java/com/proofpoint/stats/BucketedTimeDistribution.java
@@ -16,18 +16,24 @@
 package com.proofpoint.stats;
 
 import com.google.common.base.Function;
-import com.proofpoint.reporting.Bucketed;
+import com.proofpoint.reporting.Prometheus;
+import com.proofpoint.reporting.PrometheusSummary;
 import com.proofpoint.reporting.Reported;
 
 import javax.annotation.concurrent.GuardedBy;
 
+import static com.proofpoint.reporting.PrometheusType.COUNTER;
+import static com.proofpoint.reporting.PrometheusType.SUPPRESSED;
+
 public final class BucketedTimeDistribution
-    extends Bucketed<BucketedTimeDistribution.Distribution>
+    extends PrometheusSummary<BucketedTimeDistribution.Distribution>
 {
     public void add(final long value)
     {
         applyToCurrentBucket((Function<Distribution, Void>) input -> {
             synchronized (input) {
+                input.allTimeCount++;
+                input.allTimeTotal += value;
                 input.digest.add(value);
                 input.total += value;
             }
@@ -38,31 +44,54 @@ public final class BucketedTimeDistribution
     @Override
     protected Distribution createBucket(Distribution previousBucket)
     {
-        return new Distribution();
+        return new Distribution(previousBucket);
     }
 
     protected static class Distribution
     {
-        private final static double MAX_ERROR = 0.01;
-    
+        private static final double MAX_ERROR = 0.01;
+
         @GuardedBy("this")
-        private final QuantileDigest digest;
+        private long allTimeTotal = 0;
+
+        @GuardedBy("this")
+        private long allTimeCount = 0;
+
+        @GuardedBy("this")
+        private final QuantileDigest digest = new QuantileDigest(MAX_ERROR);
 
         @GuardedBy("this")
         private long total = 0;
     
-        public Distribution()
+        public Distribution(Distribution previousDistribution)
         {
-            digest = new QuantileDigest(MAX_ERROR);
+            if (previousDistribution != null) {
+                allTimeTotal = previousDistribution.allTimeTotal;
+                allTimeCount = previousDistribution.allTimeCount;
+            }
+        }
+
+        @Prometheus(name = "Sum", type = COUNTER)
+        public synchronized double getAllTimeTotal()
+        {
+            return convertToSeconds(allTimeTotal);
+        }
+
+        @Prometheus(name = "Count", type = COUNTER)
+        public synchronized long getAllTimeCount()
+        {
+            return allTimeCount;
         }
 
         @Reported
+        @Prometheus(type = SUPPRESSED)
         public synchronized double getCount()
         {
             return digest.getCount();
         }
 
         @Reported
+        @Prometheus(type = SUPPRESSED)
         public synchronized double getTotal() {
             return convertToSeconds(total);
         }
