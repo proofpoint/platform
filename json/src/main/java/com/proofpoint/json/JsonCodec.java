airlift/airlift

Add toJsonWithLengthLimit to JsonCodec
Also added JsonLengthLimitExceededException to signal this condition.

json/src/main/java/io/airlift/json/JsonCodec.java
@@ -21,11 +21,14 @@ import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
 import com.google.common.reflect.TypeParameter;
 import com.google.common.reflect.TypeToken;
+import io.airlift.json.LengthLimitedWriter.LengthLimitExceededException;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -151,6 +154,29 @@ public class JsonCodec<T>
     {
         try {
             return mapper.writeValueAsString(instance);
+        }
+        catch (IOException e) {
+            throw new IllegalArgumentException(String.format("%s could not be converted to json", instance.getClass().getName()), e);
+        }
+    }
+
+    /**
+     * Converts the specified instance to optional json string with a length limit. Returns Optional.empty() if length limit is exceeded.
+     *
+     * @param instance the instance to convert to json
+     * @param lengthLimit the maximum length of the serialized string in bytes
+     * @return json string
+     * @throws IllegalArgumentException if the specified instance can not be converted to json
+     */
+    public Optional<String> toJsonWithLengthLimit(T instance, int lengthLimit)
+    {
+        try (StringWriter stringWriter = new StringWriter();
+                LengthLimitedWriter lengthLimitedWriter = new LengthLimitedWriter(stringWriter, lengthLimit)) {
+            mapper.writeValue(lengthLimitedWriter, instance);
+            return Optional.of(stringWriter.getBuffer().toString());
+        }
+        catch (LengthLimitExceededException e) {
+            return Optional.empty();
         }
         catch (IOException e) {
             throw new IllegalArgumentException(String.format("%s could not be converted to json", instance.getClass().getName()), e);

json/src/main/java/io/airlift/json/LengthLimitedWriter.java
@@ -1,0 +1,51 @@
+package io.airlift.json;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import static java.util.Objects.requireNonNull;
+
+class LengthLimitedWriter
+        extends Writer
+{
+    private final Writer writer;
+    private final int maxLength;
+    private int count;
+
+    public LengthLimitedWriter(Writer writer, int maxLength)
+    {
+        this.writer = requireNonNull(writer, "writer is null");
+        this.maxLength = maxLength;
+    }
+
+    @Override
+    public void write(char[] buffer, int offset, int length)
+            throws IOException
+    {
+        count += length;
+        if (count > maxLength) {
+            throw new LengthLimitExceededException();
+        }
+        writer.write(buffer, offset, length);
+    }
+
+    @Override
+    public void flush()
+            throws IOException
+    {
+        writer.flush();
+    }
+
+    @Override
+    public void close()
+            throws IOException
+    {
+        writer.close();
+    }
+
+    // this needs to extend IOException so that Jackson doesn't wrap it
+    public static class LengthLimitExceededException
+            extends IOException
+    {
+    }
+}

proofpoint/platform

json/src/main/java/com/proofpoint/json/JsonCodec.java
@@ -21,11 +21,14 @@ import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
 import com.google.common.reflect.TypeParameter;
 import com.google.common.reflect.TypeToken;
+import com.proofpoint.json.LengthLimitedWriter.LengthLimitExceededException;
 
 import java.io.IOException;
+import java.io.StringWriter;
 import java.lang.reflect.Type;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static com.fasterxml.jackson.databind.SerializationFeature.INDENT_OUTPUT;
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -157,6 +160,29 @@ public class JsonCodec<T>
     {
         try {
             return mapper.writeValueAsString(instance);
+        }
+        catch (IOException e) {
+            throw new IllegalArgumentException(String.format("%s could not be converted to json", instance.getClass().getName()), e);
+        }
+    }
+
+    /**
+     * Converts the specified instance to optional json string with a length limit. Returns Optional.empty() if length limit is exceeded.
+     *
+     * @param instance the instance to convert to json
+     * @param lengthLimit the maximum length of the serialized string in bytes
+     * @return json string
+     * @throws IllegalArgumentException if the specified instance can not be converted to json
+     */
+    public Optional<String> toJsonWithLengthLimit(T instance, int lengthLimit)
+    {
+        try (StringWriter stringWriter = new StringWriter();
+             LengthLimitedWriter lengthLimitedWriter = new LengthLimitedWriter(stringWriter, lengthLimit)) {
+            mapper.writeValue(lengthLimitedWriter, instance);
+            return Optional.of(stringWriter.getBuffer().toString());
+        }
+        catch (LengthLimitExceededException e) {
+            return Optional.empty();
         }
         catch (IOException e) {
             throw new IllegalArgumentException(String.format("%s could not be converted to json", instance.getClass().getName()), e);

json/src/main/java/com/proofpoint/json/LengthLimitedWriter.java
@@ -1,0 +1,51 @@
+package com.proofpoint.json;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import static java.util.Objects.requireNonNull;
+
+class LengthLimitedWriter
+        extends Writer
+{
+    private final Writer writer;
+    private final int maxLength;
+    private int count;
+
+    public LengthLimitedWriter(Writer writer, int maxLength)
+    {
+        this.writer = requireNonNull(writer, "writer is null");
+        this.maxLength = maxLength;
+    }
+
+    @Override
+    public void write(char[] buffer, int offset, int length)
+            throws IOException
+    {
+        count += length;
+        if (count > maxLength) {
+            throw new LengthLimitExceededException();
+        }
+        writer.write(buffer, offset, length);
+    }
+
+    @Override
+    public void flush()
+            throws IOException
+    {
+        writer.flush();
+    }
+
+    @Override
+    public void close()
+            throws IOException
+    {
+        writer.close();
+    }
+
+    // this needs to extend IOException so that Jackson doesn't wrap it
+    public static class LengthLimitExceededException
+            extends IOException
+    {
+    }
+}