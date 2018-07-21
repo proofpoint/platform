airlift/airlift

Implement BasicAuthRequestFilter for HttpClient
Allows to pre-authenticate requests with the basic authentication

http-client/src/main/java/io/airlift/http/client/BasicAuthRequestFilter.java
@@ -1,0 +1,60 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.airlift.http.client;
+
+import java.util.Base64;
+import java.util.function.Predicate;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.net.HttpHeaders.AUTHORIZATION;
+import static io.airlift.http.client.Request.Builder.fromRequest;
+import static java.nio.charset.StandardCharsets.ISO_8859_1;
+import static java.util.Objects.requireNonNull;
+
+public class BasicAuthRequestFilter
+        implements HttpRequestFilter
+{
+    private final Predicate<Request> requestMatcher;
+    private final String authenticationHeader;
+
+    public BasicAuthRequestFilter(String user, String password)
+    {
+        this(request -> true, user, password);
+    }
+
+    public BasicAuthRequestFilter(Predicate<Request> requestMatcher, String user, String password)
+    {
+        this.requestMatcher = requireNonNull(requestMatcher, "requestMatcher is null");
+        this.authenticationHeader = createAuthenticationHeader(user, password);
+    }
+
+    private static String createAuthenticationHeader(String user, String password)
+    {
+        requireNonNull(user, "user is null");
+        checkArgument(!user.contains(":"), "Illegal character ':' found in username");
+        requireNonNull(password, "password is null");
+        return "Basic " + Base64.getEncoder().encodeToString((user + ":" + password).getBytes(ISO_8859_1));
+    }
+
+    @Override
+    public Request filterRequest(Request request)
+    {
+        if (!requestMatcher.test(request)) {
+            return request;
+        }
+        return fromRequest(request)
+                .addHeader(AUTHORIZATION, authenticationHeader)
+                .build();
+    }
+}

http-client/src/test/java/io/airlift/http/client/TestBasicAuthRequestFilter.java
@@ -1,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.airlift.http.client;
+
+import org.testng.annotations.Test;
+
+import java.net.URI;
+import java.util.function.Predicate;
+
+import static com.google.common.net.HttpHeaders.AUTHORIZATION;
+import static io.airlift.http.client.Request.Builder.prepareGet;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
+
+public class TestBasicAuthRequestFilter
+{
+    @Test
+    public void testBasicAuthentication()
+            throws Exception
+    {
+        Predicate<Request> predicate = request -> request.getUri().getPath().startsWith("/private");
+        HttpRequestFilter filter = new BasicAuthRequestFilter(predicate, "Aladdin", "open sesame");
+
+        Request publicResourceRequest = createTestRequest("/public");
+        assertNull(filter.filterRequest(publicResourceRequest).getHeader(AUTHORIZATION));
+
+        Request privateResourceRequest = createTestRequest("/private");
+        assertEquals(filter.filterRequest(privateResourceRequest).getHeader(AUTHORIZATION), "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==");
+    }
+
+    private static Request createTestRequest(String path)
+    {
+        return prepareGet()
+                .setUri(URI.create("http://example.com" + path))
+                .build();
+    }
+}

proofpoint/platform

http-client/src/main/java/com/proofpoint/http/client/BasicAuthRequestFilter.java
@@ -1,0 +1,60 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.http.client;
+
+import java.util.Base64;
+import java.util.function.Predicate;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.net.HttpHeaders.AUTHORIZATION;
+import static com.proofpoint.http.client.Request.Builder.fromRequest;
+import static java.nio.charset.StandardCharsets.ISO_8859_1;
+import static java.util.Objects.requireNonNull;
+
+public class BasicAuthRequestFilter
+        implements HttpRequestFilter
+{
+    private final Predicate<Request> requestMatcher;
+    private final String authenticationHeader;
+
+    public BasicAuthRequestFilter(String user, String password)
+    {
+        this(request -> true, user, password);
+    }
+
+    public BasicAuthRequestFilter(Predicate<Request> requestMatcher, String user, String password)
+    {
+        this.requestMatcher = requireNonNull(requestMatcher, "requestMatcher is null");
+        this.authenticationHeader = createAuthenticationHeader(user, password);
+    }
+
+    private static String createAuthenticationHeader(String user, String password)
+    {
+        requireNonNull(user, "user is null");
+        checkArgument(!user.contains(":"), "Illegal character ':' found in username");
+        requireNonNull(password, "password is null");
+        return "Basic " + Base64.getEncoder().encodeToString((user + ":" + password).getBytes(ISO_8859_1));
+    }
+
+    @Override
+    public Request filterRequest(Request request)
+    {
+        if (!requestMatcher.test(request)) {
+            return request;
+        }
+        return fromRequest(request)
+                .addHeader(AUTHORIZATION, authenticationHeader)
+                .build();
+    }
+}

http-client/src/test/java/com/proofpoint/http/client/TestBasicAuthRequestFilter.java
@@ -1,0 +1,48 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.http.client;
+
+import org.testng.annotations.Test;
+
+import java.net.URI;
+import java.util.function.Predicate;
+
+import static com.google.common.net.HttpHeaders.AUTHORIZATION;
+import static com.proofpoint.http.client.Request.Builder.prepareGet;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertNull;
+
+public class TestBasicAuthRequestFilter
+{
+    @Test
+    public void testBasicAuthentication()
+            throws Exception
+    {
+        Predicate<Request> predicate = request -> request.getUri().getPath().startsWith("/private");
+        HttpRequestFilter filter = new BasicAuthRequestFilter(predicate, "Aladdin", "open sesame");
+
+        Request publicResourceRequest = createTestRequest("/public");
+        assertNull(filter.filterRequest(publicResourceRequest).getHeader(AUTHORIZATION));
+
+        Request privateResourceRequest = createTestRequest("/private");
+        assertEquals(filter.filterRequest(privateResourceRequest).getHeader(AUTHORIZATION), "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==");
+    }
+
+    private static Request createTestRequest(String path)
+    {
+        return prepareGet()
+                .setUri(URI.create("http://example.com" + path))
+                .build();
+    }
+}