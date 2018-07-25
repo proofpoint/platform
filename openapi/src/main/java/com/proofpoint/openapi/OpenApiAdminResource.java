4834bfee2ee0a7cd7ad3fc13b386dc83cdde54ea
Garrick Dasbach <gdasbach@proofpoint.com>
2018-07-25 11:36:07 -0400

    #672: Code review revisions
    Apply CodeStyle and address review comments

--- openapi/src/main/java/com/proofpoint/openapi/OpenApiAdminResource.java
+++ openapi/src/main/java/com/proofpoint/openapi/OpenApiAdminResource.java
@@ -1,23 +1,21 @@
 package com.proofpoint.openapi;
 
 import com.proofpoint.jaxrs.AccessDoesNotRequireAuthentication;
-import com.proofpoint.jaxrs.JaxrsResource;
 import io.swagger.v3.jaxrs2.integration.resources.BaseOpenApiResource;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.integration.SwaggerConfiguration;
 import io.swagger.v3.oas.models.OpenAPI;
 
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
 import javax.servlet.ServletConfig;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.*;
-import java.util.Set;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import java.util.stream.Collectors;
-
-import static java.util.Objects.requireNonNull;
 
 @Path("/admin/openapi-admin.{type:json|yaml}")
 @AccessDoesNotRequireAuthentication
@@ -35,11 +33,13 @@ public class OpenApiAdminResource extends BaseOpenApiResource
             @Context Application app)
             throws Exception
     {
-        if(getOpenApiConfiguration() == null){
+        if (getOpenApiConfiguration() == null) {
             SwaggerConfiguration oasConfig = new SwaggerConfiguration()
                     .openAPI(new OpenAPI())
                     .prettyPrint(true);
-            oasConfig.setResourceClasses(app.getSingletons().stream().map(classame -> classame.getClass().getName()).collect(Collectors.toSet()));
+            oasConfig.setResourceClasses(app.getSingletons().stream()
+                    .map(classame -> classame.getClass().getName())
+                    .collect(Collectors.toSet()));
             oasConfig.setScannerClass(SWAGGER_JAXRS_ANNOTATION_SCANNER);
             setOpenApiConfiguration(oasConfig);
         }
