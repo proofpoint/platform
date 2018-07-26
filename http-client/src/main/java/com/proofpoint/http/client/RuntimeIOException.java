1ce3780a4faa3940057092549280cd24a5e256d6
John Gardiner Myers <jgmyers@proofpoint.com>
2018-07-26 14:24:42 -0300

    Deprecate RuntimeIOException

--- http-client/src/main/java/com/proofpoint/http/client/RuntimeIOException.java
+++ http-client/src/main/java/com/proofpoint/http/client/RuntimeIOException.java
@@ -1,9 +1,14 @@
 package com.proofpoint.http.client;
 
 import java.io.IOException;
+import java.io.UncheckedIOException;
 
+/**
+ * @deprecated Use {@link UncheckedIOException}.
+ */
+@Deprecated
 public class RuntimeIOException
-        extends RuntimeException
+        extends UncheckedIOException
 {
     public RuntimeIOException(IOException cause)
     {
