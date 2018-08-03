airlift/airlift

Capitalize JSON in JsonCodec error messages

http-client/src/test/java/io/airlift/http/client/TestFullJsonResponseHandler.java
@@ -60,7 +60,7 @@ public class TestFullJsonResponseHandler
         assertFalse(response.hasValue());
         assertEquals(response.getException().getMessage(), format("Unable to create %s from JSON response:\n[%s]", User.class, json));
         assertTrue(response.getException().getCause() instanceof IllegalArgumentException);
-        assertEquals(response.getException().getCause().getMessage(), "Invalid json bytes for [simple type, class io.airlift.http.client.TestFullJsonResponseHandler$User]");
+        assertEquals(response.getException().getCause().getMessage(), "Invalid JSON bytes for [simple type, class io.airlift.http.client.TestFullJsonResponseHandler$User]");
 
         assertEquals(response.getJsonBytes(), json.getBytes(UTF_8));
         assertEquals(response.getJson(), json);

http-client/src/test/java/io/airlift/http/client/TestJsonResponseHandler.java
@@ -42,7 +42,7 @@ public class TestJsonResponseHandler
         catch (IllegalArgumentException e) {
             assertEquals(e.getMessage(), format("Unable to create %s from JSON response:\n[%s]", User.class, json));
             assertTrue(e.getCause() instanceof IllegalArgumentException);
-            assertEquals(e.getCause().getMessage(), "Invalid json bytes for [simple type, class io.airlift.http.client.TestFullJsonResponseHandler$User]");
+            assertEquals(e.getCause().getMessage(), "Invalid JSON bytes for [simple type, class io.airlift.http.client.TestFullJsonResponseHandler$User]");
         }
     }
 

proofpoint/platform

http-client/src/test/java/com/proofpoint/http/client/TestFullJsonResponseHandler.java
@@ -75,7 +75,7 @@ public class TestFullJsonResponseHandler
         assertEquals(response.getException().getMessage(), format("Unable to create %s from JSON response:\n[%s]", User.class, json));
         assertInstanceOf(response.getException().getCause(), IllegalArgumentException.class);
 
-        assertEquals(response.getException().getCause().getMessage(), "Invalid json bytes for [simple type, class com.proofpoint.http.client.TestFullJsonResponseHandler$User]");
+        assertEquals(response.getException().getCause().getMessage(), "Invalid JSON bytes for [simple type, class com.proofpoint.http.client.TestFullJsonResponseHandler$User]");
         assertEquals(response.getJson(), json);
 
         assertEquals(response.getResponseBytes(), response.getJsonBytes());

http-client/src/test/java/com/proofpoint/http/client/TestJsonResponseHandler.java
@@ -55,7 +55,7 @@ public class TestJsonResponseHandler
         catch (IllegalArgumentException e) {
             assertEquals(e.getMessage(), format("Unable to create %s from JSON response:\n[%s]", User.class, json));
             assertInstanceOf(e.getCause(), IllegalArgumentException.class);
-            assertEquals(e.getCause().getMessage(), "Invalid json bytes for [simple type, class com.proofpoint.http.client.TestFullJsonResponseHandler$User]");
+            assertEquals(e.getCause().getMessage(), "Invalid JSON bytes for [simple type, class com.proofpoint.http.client.TestFullJsonResponseHandler$User]");
         }
     }
 