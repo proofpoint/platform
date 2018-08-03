airlift/airlift

Validate scheme for HTTP client Request objects

http-client/src/test/java/io/airlift/http/client/TestRequest.java
@@ -64,6 +64,27 @@ public class TestRequest
         new Request(URI.create("http://example.com:0/"), "GET", createHeaders1(), createBodyGenerator());
     }
 
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "uri does not have a host: http:///foo")
+    public void testInvalidUriMissingHost()
+            throws Exception
+    {
+        new Request(URI.create("http:///foo"), "GET", createHeaders1(), createBodyGenerator());
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "uri does not have a scheme: //foo")
+    public void testInvalidUriMissingScheme()
+            throws Exception
+    {
+        new Request(URI.create("//foo"), "GET", createHeaders1(), createBodyGenerator());
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "uri scheme must be http or https: gopher://example.com")
+    public void testInvalidUriScheme()
+            throws Exception
+    {
+        new Request(URI.create("gopher://example.com"), "GET", createHeaders1(), createBodyGenerator());
+    }
+
     private URI createUri1()
     {
         return URI.create("http://example.com");

proofpoint/platform

http-client/src/test/java/com/proofpoint/http/client/TestRequest.java
@@ -68,6 +68,20 @@ public class TestRequest
         new Request(URI.create("http://example.com:0/"), "GET", createHeaders1(), createBodySource());
     }
 
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "uri does not have a host: http:///foo")
+    public void testInvalidUriMissingHost()
+            throws Exception
+    {
+        new Request(URI.create("http:///foo"), "GET", createHeaders1(), createBodySource());
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class, expectedExceptionsMessageRegExp = "uri scheme must be http or https: gopher://example.com")
+    public void testInvalidUriScheme()
+            throws Exception
+    {
+        new Request(URI.create("gopher://example.com"), "GET", createHeaders1(), createBodySource());
+    }
+
     private URI createUri1()
     {
         return URI.create("http://example.com");