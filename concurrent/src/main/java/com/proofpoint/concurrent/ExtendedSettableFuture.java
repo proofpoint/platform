airlift/airlift

Add ExtendedSettableFuture
This is a copy of guava's SettableFuture, but allows a setAsync method.

concurrent/src/main/java/io/airlift/concurrent/ExtendedSettableFuture.java
@@ -1,0 +1,74 @@
+package io.airlift.concurrent;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.util.concurrent.AbstractFuture;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import javax.annotation.Nullable;
+
+import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
+
+public final class ExtendedSettableFuture<V>
+        extends AbstractFuture<V>
+{
+    public static <V> ExtendedSettableFuture<V> create()
+    {
+        return new ExtendedSettableFuture<>();
+    }
+
+    private ExtendedSettableFuture() {}
+
+    @Override
+    public boolean set(@Nullable V value)
+    {
+        return super.set(value);
+    }
+
+    @Override
+    public boolean setException(Throwable throwable)
+    {
+        return super.setException(throwable);
+    }
+
+    /**
+     * Sets this current future with the result of the delegate.
+     * <p>
+     * Values and exceptions are both propagated to this Future.
+     * If this Future is cancelled, than the delegate will also be cancelled
+     * with the same interrupt flag.
+     */
+    public void setAsync(ListenableFuture<? extends V> delegate)
+    {
+        Futures.addCallback(delegate, new FutureCallback<V>()
+        {
+            @Override
+            public void onSuccess(@Nullable V result)
+            {
+                set(result);
+            }
+
+            @Override
+            public void onFailure(Throwable t)
+            {
+                setException(t);
+            }
+        });
+
+        super.addListener(() -> {
+            if (super.isCancelled()) {
+                delegate.cancel(super.wasInterrupted());
+            }
+        }, directExecutor());
+    }
+
+    /**
+     * @return true if the Future was interrupted when cancelled.
+     */
+    @VisibleForTesting
+    boolean checkWasInterrupted()
+    {
+        return super.wasInterrupted();
+    }
+}

concurrent/src/test/java/io/airlift/concurrent/TestExtendedSettableFuture.java
@@ -1,0 +1,99 @@
+package io.airlift.concurrent;
+
+import org.testng.annotations.Test;
+
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertThrows;
+import static org.testng.Assert.assertTrue;
+
+public class TestExtendedSettableFuture
+{
+    @Test
+    public void testSet()
+            throws Exception
+    {
+        ExtendedSettableFuture<String> future = ExtendedSettableFuture.create();
+        future.set("abc");
+        assertTrue(future.isDone());
+        assertFalse(future.isCancelled());
+        assertFalse(future.checkWasInterrupted());
+        assertEquals(future.get(), "abc");
+    }
+
+    @Test
+    public void testSetException()
+            throws Exception
+    {
+        ExtendedSettableFuture<String> future = ExtendedSettableFuture.create();
+        future.setException(new Exception(""));
+        assertTrue(future.isDone());
+        assertFalse(future.isCancelled());
+        assertFalse(future.checkWasInterrupted());
+        assertThrows(ExecutionException.class, future::get);
+    }
+
+    @Test
+    public void testCancelWithoutInterrupt()
+            throws Exception
+    {
+        ExtendedSettableFuture<String> future = ExtendedSettableFuture.create();
+        future.cancel(false);
+        assertTrue(future.isDone());
+        assertTrue(future.isCancelled());
+        assertFalse(future.checkWasInterrupted());
+        assertThrows(CancellationException.class, future::get);
+    }
+
+    @Test
+    public void testCancelWithInterrupt()
+            throws Exception
+    {
+        ExtendedSettableFuture<String> future = ExtendedSettableFuture.create();
+        future.cancel(true);
+        assertTrue(future.isDone());
+        assertTrue(future.isCancelled());
+        assertTrue(future.checkWasInterrupted());
+        assertThrows(CancellationException.class, future::get);
+    }
+
+    @Test
+    public void testSetAsync()
+            throws Exception
+    {
+        // Test return value
+        ExtendedSettableFuture<String> fromFuture = ExtendedSettableFuture.create();
+        ExtendedSettableFuture<String> toFuture = ExtendedSettableFuture.create();
+        toFuture.setAsync(fromFuture);
+        fromFuture.set("abc");
+        assertEquals(toFuture.get(), "abc");
+
+        // Test exception
+        fromFuture = ExtendedSettableFuture.create();
+        toFuture = ExtendedSettableFuture.create();
+        toFuture.setAsync(fromFuture);
+        fromFuture.setException(new RuntimeException());
+        assertThrows(ExecutionException.class, toFuture::get);
+
+        // Test cancellation without interrupt
+        fromFuture = ExtendedSettableFuture.create();
+        toFuture = ExtendedSettableFuture.create();
+        toFuture.setAsync(fromFuture);
+        toFuture.cancel(false);
+        // Parent Future should receive the cancellation
+        assertTrue(fromFuture.isCancelled());
+        assertFalse(fromFuture.checkWasInterrupted());
+
+        // Test cancellation with interrupt
+        fromFuture = ExtendedSettableFuture.create();
+        toFuture = ExtendedSettableFuture.create();
+        toFuture.setAsync(fromFuture);
+        toFuture.cancel(true);
+        // Parent Future should receive the cancellation
+        assertTrue(fromFuture.isCancelled());
+        assertTrue(fromFuture.checkWasInterrupted());
+    }
+}

proofpoint/platform

concurrent/src/main/java/com/proofpoint/concurrent/ExtendedSettableFuture.java
@@ -1,0 +1,74 @@
+package com.proofpoint.concurrent;
+
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.util.concurrent.AbstractFuture;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+
+import javax.annotation.Nullable;
+
+import static com.google.common.util.concurrent.MoreExecutors.directExecutor;
+
+public final class ExtendedSettableFuture<V>
+        extends AbstractFuture<V>
+{
+    public static <V> ExtendedSettableFuture<V> create()
+    {
+        return new ExtendedSettableFuture<>();
+    }
+
+    private ExtendedSettableFuture() {}
+
+    @Override
+    public boolean set(@Nullable V value)
+    {
+        return super.set(value);
+    }
+
+    @Override
+    public boolean setException(Throwable throwable)
+    {
+        return super.setException(throwable);
+    }
+
+    /**
+     * Sets this current future with the result of the delegate.
+     * <p>
+     * Values and exceptions are both propagated to this Future.
+     * If this Future is cancelled, than the delegate will also be cancelled
+     * with the same interrupt flag.
+     */
+    public void setAsync(ListenableFuture<? extends V> delegate)
+    {
+        Futures.addCallback(delegate, new FutureCallback<V>()
+        {
+            @Override
+            public void onSuccess(@Nullable V result)
+            {
+                set(result);
+            }
+
+            @Override
+            public void onFailure(Throwable t)
+            {
+                setException(t);
+            }
+        });
+
+        super.addListener(() -> {
+            if (super.isCancelled()) {
+                delegate.cancel(super.wasInterrupted());
+            }
+        }, directExecutor());
+    }
+
+    /**
+     * @return true if the Future was interrupted when cancelled.
+     */
+    @VisibleForTesting
+    boolean checkWasInterrupted()
+    {
+        return super.wasInterrupted();
+    }
+}

concurrent/src/test/java/com/proofpoint/concurrent/TestExtendedSettableFuture.java
@@ -1,0 +1,99 @@
+package com.proofpoint.concurrent;
+
+import org.testng.annotations.Test;
+
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertThrows;
+import static org.testng.Assert.assertTrue;
+
+public class TestExtendedSettableFuture
+{
+    @Test
+    public void testSet()
+            throws Exception
+    {
+        ExtendedSettableFuture<String> future = ExtendedSettableFuture.create();
+        future.set("abc");
+        assertTrue(future.isDone());
+        assertFalse(future.isCancelled());
+        assertFalse(future.checkWasInterrupted());
+        assertEquals(future.get(), "abc");
+    }
+
+    @Test
+    public void testSetException()
+            throws Exception
+    {
+        ExtendedSettableFuture<String> future = ExtendedSettableFuture.create();
+        future.setException(new Exception(""));
+        assertTrue(future.isDone());
+        assertFalse(future.isCancelled());
+        assertFalse(future.checkWasInterrupted());
+        assertThrows(ExecutionException.class, future::get);
+    }
+
+    @Test
+    public void testCancelWithoutInterrupt()
+            throws Exception
+    {
+        ExtendedSettableFuture<String> future = ExtendedSettableFuture.create();
+        future.cancel(false);
+        assertTrue(future.isDone());
+        assertTrue(future.isCancelled());
+        assertFalse(future.checkWasInterrupted());
+        assertThrows(CancellationException.class, future::get);
+    }
+
+    @Test
+    public void testCancelWithInterrupt()
+            throws Exception
+    {
+        ExtendedSettableFuture<String> future = ExtendedSettableFuture.create();
+        future.cancel(true);
+        assertTrue(future.isDone());
+        assertTrue(future.isCancelled());
+        assertTrue(future.checkWasInterrupted());
+        assertThrows(CancellationException.class, future::get);
+    }
+
+    @Test
+    public void testSetAsync()
+            throws Exception
+    {
+        // Test return value
+        ExtendedSettableFuture<String> fromFuture = ExtendedSettableFuture.create();
+        ExtendedSettableFuture<String> toFuture = ExtendedSettableFuture.create();
+        toFuture.setAsync(fromFuture);
+        fromFuture.set("abc");
+        assertEquals(toFuture.get(), "abc");
+
+        // Test exception
+        fromFuture = ExtendedSettableFuture.create();
+        toFuture = ExtendedSettableFuture.create();
+        toFuture.setAsync(fromFuture);
+        fromFuture.setException(new RuntimeException());
+        assertThrows(ExecutionException.class, toFuture::get);
+
+        // Test cancellation without interrupt
+        fromFuture = ExtendedSettableFuture.create();
+        toFuture = ExtendedSettableFuture.create();
+        toFuture.setAsync(fromFuture);
+        toFuture.cancel(false);
+        // Parent Future should receive the cancellation
+        assertTrue(fromFuture.isCancelled());
+        assertFalse(fromFuture.checkWasInterrupted());
+
+        // Test cancellation with interrupt
+        fromFuture = ExtendedSettableFuture.create();
+        toFuture = ExtendedSettableFuture.create();
+        toFuture.setAsync(fromFuture);
+        toFuture.cancel(true);
+        // Parent Future should receive the cancellation
+        assertTrue(fromFuture.isCancelled());
+        assertTrue(fromFuture.checkWasInterrupted());
+    }
+}