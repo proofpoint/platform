airlift/airlift

Convert to getFutureValue in discovery client

discovery/src/main/java/io/airlift/discovery/client/Announcer.java
@@ -16,7 +16,6 @@
 package io.airlift.discovery.client;
 
 import com.google.common.base.Preconditions;
-import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.MapMaker;
 import com.google.common.util.concurrent.FutureCallback;
@@ -35,13 +34,12 @@ import java.net.ConnectException;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentMap;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicBoolean;
 
+import static io.airlift.concurrent.MoreFutures.getFutureValue;
 import static io.airlift.concurrent.Threads.daemonThreadsNamed;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
@@ -111,7 +109,7 @@ public final class Announcer
 
         // unannounce
         try {
-            getFutureResult(announcementClient.unannounce(), DiscoveryException.class);
+            getFutureValue(announcementClient.unannounce(), DiscoveryException.class);
         }
         catch (DiscoveryException e) {
             if (e.getCause() instanceof ConnectException) {
@@ -190,22 +188,5 @@ public final class Announcer
 
         long delayStart = System.nanoTime();
         executor.schedule(() -> announce(delayStart, expectedDelay), expectedDelay.toMillis(), MILLISECONDS);
-    }
-
-    // TODO: move this to a utility package
-    private static <T, X extends Throwable> T getFutureResult(Future<T> future, Class<X> type)
-            throws X
-    {
-        try {
-            return future.get();
-        }
-        catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw Throwables.propagate(e);
-        }
-        catch (ExecutionException e) {
-            Throwables.propagateIfPossible(e.getCause(), type);
-            throw Throwables.propagate(e.getCause());
-        }
     }
 }

discovery/src/main/java/io/airlift/discovery/client/testing/SimpleServiceSelector.java
@@ -15,7 +15,6 @@
  */
 package io.airlift.discovery.client.testing;
 
-import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
@@ -28,9 +27,8 @@ import io.airlift.discovery.client.ServiceSelectorConfig;
 import io.airlift.log.Logger;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
+import static io.airlift.concurrent.MoreFutures.getFutureValue;
 import static java.util.Objects.requireNonNull;
 
 public class SimpleServiceSelector implements ServiceSelector
@@ -69,7 +67,7 @@ public class SimpleServiceSelector implements ServiceSelector
     {
         try {
             ListenableFuture<ServiceDescriptors> future = lookupClient.getServices(type, pool);
-            ServiceDescriptors serviceDescriptors = getFutureResult(future, DiscoveryException.class);
+            ServiceDescriptors serviceDescriptors = getFutureValue(future, DiscoveryException.class);
             return serviceDescriptors.getServiceDescriptors();
         }
         catch (DiscoveryException e) {
@@ -82,22 +80,5 @@ public class SimpleServiceSelector implements ServiceSelector
     public ListenableFuture<List<ServiceDescriptor>> refresh()
     {
         return Futures.transform(lookupClient.getServices(type, pool), ServiceDescriptors::getServiceDescriptors);
-    }
-
-    // TODO: move this to a utility package
-    private static <T, X extends Throwable> T getFutureResult(Future<T> future, Class<X> type)
-            throws X
-    {
-        try {
-            return future.get();
-        }
-        catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw Throwables.propagate(e);
-        }
-        catch (ExecutionException e) {
-            Throwables.propagateIfPossible(e.getCause(), type);
-            throw Throwables.propagate(e.getCause());
-        }
     }
 }

proofpoint/platform

discovery/src/main/java/com/proofpoint/discovery/client/announce/Announcer.java
@@ -39,6 +39,7 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.google.common.base.Preconditions.checkState;
+import static com.proofpoint.concurrent.MoreFutures.getFutureValue;
 import static com.proofpoint.concurrent.Threads.daemonThreadsNamed;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
@@ -110,7 +111,7 @@ public class Announcer
 
         // unannounce
         try {
-            getFutureResult(announcementClient.unannounce(), DiscoveryException.class);
+            getFutureValue(announcementClient.unannounce(), DiscoveryException.class);
         }
         catch (DiscoveryException e) {
             if (e.getCause() instanceof ConnectException) {
@@ -183,22 +184,5 @@ public class Announcer
             return;
         }
         executor.schedule((Runnable) this::announce, delay.toMillis(), MILLISECONDS);
-    }
-
-    // TODO: move this to a utility package
-    private static <T, X extends Throwable> T getFutureResult(Future<T> future, Class<X> type)
-            throws X
-    {
-        try {
-            return future.get();
-        }
-        catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw Throwables.propagate(e);
-        }
-        catch (ExecutionException e) {
-            Throwables.propagateIfPossible(e.getCause(), type);
-            throw Throwables.propagate(e.getCause());
-        }
     }
 }

discovery/src/main/java/com/proofpoint/discovery/client/testing/SimpleServiceSelector.java
@@ -15,7 +15,6 @@
  */
 package com.proofpoint.discovery.client.testing;
 
-import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.proofpoint.discovery.client.DiscoveryException;
@@ -28,10 +27,9 @@ import com.proofpoint.log.Logger;
 import com.proofpoint.node.NodeInfo;
 
 import java.util.List;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
+import static com.proofpoint.concurrent.MoreFutures.getFutureValue;
 import static java.util.Objects.requireNonNull;
 
 public class SimpleServiceSelector implements ServiceSelector
@@ -70,29 +68,12 @@ public class SimpleServiceSelector implements ServiceSelector
     {
         try {
             ListenableFuture<ServiceDescriptors> future = lookupClient.getServices(type, pool);
-            ServiceDescriptors serviceDescriptors = getFutureResult(future, DiscoveryException.class);
+            ServiceDescriptors serviceDescriptors = getFutureValue(future, DiscoveryException.class);
             return serviceDescriptors.getServiceDescriptors();
         }
         catch (DiscoveryException e) {
             log.error(e);
             return ImmutableList.of();
-        }
-    }
-
-    // TODO: move this to a utility package
-    private static <T, X extends Throwable> T getFutureResult(Future<T> future, Class<X> type)
-            throws X
-    {
-        try {
-            return future.get();
-        }
-        catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw Throwables.propagate(e);
-        }
-        catch (ExecutionException e) {
-            Throwables.propagateIfPossible(e.getCause(), type);
-            throw Throwables.propagate(e.getCause());
         }
     }
 }