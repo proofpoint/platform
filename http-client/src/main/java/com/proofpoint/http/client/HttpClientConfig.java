airlift/airlift

Add static imports for unit enums

http-client/src/main/java/io/airlift/http/client/HttpClientConfig.java
@@ -21,7 +21,6 @@ import io.airlift.configuration.Config;
 import io.airlift.configuration.ConfigDescription;
 import io.airlift.configuration.LegacyConfig;
 import io.airlift.units.DataSize;
-import io.airlift.units.DataSize.Unit;
 import io.airlift.units.Duration;
 import io.airlift.units.MaxDataSize;
 import io.airlift.units.MinDataSize;
@@ -30,7 +29,9 @@ import io.airlift.units.MinDuration;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
-import java.util.concurrent.TimeUnit;
+import static io.airlift.units.DataSize.Unit.MEGABYTE;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 @Beta
 public class HttpClientConfig
@@ -41,14 +42,14 @@ public class HttpClientConfig
     public static final String JAVAX_NET_SSL_TRUST_STORE_PASSWORD = "javax.net.ssl.trustStorePassword";
 
     private boolean http2Enabled;
-    private Duration connectTimeout = new Duration(1, TimeUnit.SECONDS);
-    private Duration requestTimeout = new Duration(5, TimeUnit.MINUTES);
-    private Duration idleTimeout = new Duration(1, TimeUnit.MINUTES);
+    private Duration connectTimeout = new Duration(1, SECONDS);
+    private Duration requestTimeout = new Duration(5, MINUTES);
+    private Duration idleTimeout = new Duration(1, MINUTES);
     private Duration keepAliveInterval;
     private int maxConnections = 200;
     private int maxConnectionsPerServer = 20;
     private int maxRequestsQueuedPerDestination = 1024;
-    private DataSize maxContentLength = new DataSize(16, Unit.MEGABYTE);
+    private DataSize maxContentLength = new DataSize(16, MEGABYTE);
     private HostAndPort socksProxy;
     private String keyStorePath = System.getProperty(JAVAX_NET_SSL_KEY_STORE);
     private String keyStorePassword = System.getProperty(JAVAX_NET_SSL_KEY_STORE_PASSWORD);
@@ -58,8 +59,8 @@ public class HttpClientConfig
     private boolean authenticationEnabled;
     private String kerberosPrincipal;
     private String kerberosRemoteServiceName;
-    private DataSize http2InitialSessionReceiveWindowSize = new DataSize(16, Unit.MEGABYTE);
-    private DataSize http2InitialStreamReceiveWindowSize = new DataSize(16, Unit.MEGABYTE);
+    private DataSize http2InitialSessionReceiveWindowSize = new DataSize(16, MEGABYTE);
+    private DataSize http2InitialStreamReceiveWindowSize = new DataSize(16, MEGABYTE);
 
     public boolean isHttp2Enabled()
     {

http-client/src/test/java/io/airlift/http/client/TestHttpClientConfig.java
@@ -19,20 +19,21 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.net.HostAndPort;
 import io.airlift.configuration.testing.ConfigAssertions;
 import io.airlift.units.DataSize;
-import io.airlift.units.DataSize.Unit;
 import io.airlift.units.Duration;
 import org.testng.annotations.Test;
 
 import javax.validation.constraints.NotNull;
 
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import static io.airlift.http.client.HttpClientConfig.JAVAX_NET_SSL_KEY_STORE;
 import static io.airlift.http.client.HttpClientConfig.JAVAX_NET_SSL_KEY_STORE_PASSWORD;
 import static io.airlift.http.client.HttpClientConfig.JAVAX_NET_SSL_TRUST_STORE;
 import static io.airlift.http.client.HttpClientConfig.JAVAX_NET_SSL_TRUST_STORE_PASSWORD;
 import static io.airlift.testing.ValidationAssertions.assertFailsValidation;
+import static io.airlift.units.DataSize.Unit.MEGABYTE;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 @SuppressWarnings("deprecation")
 public class TestHttpClientConfig
@@ -42,14 +43,14 @@ public class TestHttpClientConfig
     {
         ConfigAssertions.assertRecordedDefaults(ConfigAssertions.recordDefaults(HttpClientConfig.class)
                 .setHttp2Enabled(false)
-                .setConnectTimeout(new Duration(1, TimeUnit.SECONDS))
-                .setRequestTimeout(new Duration(5, TimeUnit.MINUTES))
-                .setIdleTimeout(new Duration(1, TimeUnit.MINUTES))
+                .setConnectTimeout(new Duration(1, SECONDS))
+                .setRequestTimeout(new Duration(5, MINUTES))
+                .setIdleTimeout(new Duration(1, MINUTES))
                 .setKeepAliveInterval(null)
                 .setMaxConnections(200)
                 .setMaxConnectionsPerServer(20)
                 .setMaxRequestsQueuedPerDestination(1024)
-                .setMaxContentLength(new DataSize(16, Unit.MEGABYTE))
+                .setMaxContentLength(new DataSize(16, MEGABYTE))
                 .setSocksProxy(null)
                 .setKeyStorePath(System.getProperty(JAVAX_NET_SSL_KEY_STORE))
                 .setKeyStorePassword(System.getProperty(JAVAX_NET_SSL_KEY_STORE_PASSWORD))
@@ -59,8 +60,8 @@ public class TestHttpClientConfig
                 .setAuthenticationEnabled(false)
                 .setKerberosRemoteServiceName(null)
                 .setKerberosPrincipal(null)
-                .setHttp2InitialSessionReceiveWindowSize(new DataSize(16, DataSize.Unit.MEGABYTE))
-                .setHttp2InitialStreamReceiveWindowSize(new DataSize(16, DataSize.Unit.MEGABYTE)));
+                .setHttp2InitialSessionReceiveWindowSize(new DataSize(16, MEGABYTE))
+                .setHttp2InitialStreamReceiveWindowSize(new DataSize(16, MEGABYTE)));
     }
 
     @Test
@@ -91,14 +92,14 @@ public class TestHttpClientConfig
 
         HttpClientConfig expected = new HttpClientConfig()
                 .setHttp2Enabled(true)
-                .setConnectTimeout(new Duration(4, TimeUnit.SECONDS))
-                .setRequestTimeout(new Duration(15, TimeUnit.SECONDS))
-                .setIdleTimeout(new Duration(5, TimeUnit.SECONDS))
-                .setKeepAliveInterval(new Duration(6, TimeUnit.SECONDS))
+                .setConnectTimeout(new Duration(4, SECONDS))
+                .setRequestTimeout(new Duration(15, SECONDS))
+                .setIdleTimeout(new Duration(5, SECONDS))
+                .setKeepAliveInterval(new Duration(6, SECONDS))
                 .setMaxConnections(12)
                 .setMaxConnectionsPerServer(3)
                 .setMaxRequestsQueuedPerDestination(10)
-                .setMaxContentLength(new DataSize(1, Unit.MEGABYTE))
+                .setMaxContentLength(new DataSize(1, MEGABYTE))
                 .setSocksProxy(HostAndPort.fromParts("localhost", 1080))
                 .setKeyStorePath("key-store")
                 .setKeyStorePassword("key-store-password")
@@ -108,8 +109,8 @@ public class TestHttpClientConfig
                 .setAuthenticationEnabled(true)
                 .setKerberosRemoteServiceName("airlift")
                 .setKerberosPrincipal("airlift-client")
-                .setHttp2InitialSessionReceiveWindowSize(new DataSize(7, DataSize.Unit.MEGABYTE))
-                .setHttp2InitialStreamReceiveWindowSize(new DataSize(7, DataSize.Unit.MEGABYTE));
+                .setHttp2InitialSessionReceiveWindowSize(new DataSize(7, MEGABYTE))
+                .setHttp2InitialStreamReceiveWindowSize(new DataSize(7, MEGABYTE));
 
         ConfigAssertions.assertFullMapping(properties, expected);
     }

http-server/src/main/java/io/airlift/http/server/HttpServerConfig.java
@@ -18,8 +18,8 @@ package io.airlift.http.server;
 import com.google.common.base.Splitter;
 import com.google.common.collect.ImmutableList;
 import io.airlift.configuration.Config;
-import io.airlift.configuration.ConfigSecuritySensitive;
 import io.airlift.configuration.ConfigDescription;
+import io.airlift.configuration.ConfigSecuritySensitive;
 import io.airlift.configuration.DefunctConfig;
 import io.airlift.units.DataSize;
 import io.airlift.units.Duration;
@@ -30,9 +30,12 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
+import static io.airlift.units.DataSize.Unit.BYTE;
+import static io.airlift.units.DataSize.Unit.MEGABYTE;
 import static java.util.Objects.requireNonNull;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 @DefunctConfig({
         "jetty.http.enabled",
@@ -68,7 +71,7 @@ public class HttpServerConfig
     private String logPath = "var/log/http-request.log";
     private boolean logEnabled = true;
     private int logHistory = 15;
-    private DataSize logMaxFileSize = new DataSize(Long.MAX_VALUE, DataSize.Unit.BYTE);
+    private DataSize logMaxFileSize = new DataSize(Long.MAX_VALUE, BYTE);
 
     private Integer httpAcceptorThreads;
     private Integer httpSelectorThreads;
@@ -77,12 +80,12 @@ public class HttpServerConfig
 
     private int minThreads = 2;
     private int maxThreads = 200;
-    private Duration threadMaxIdleTime = new Duration(1, TimeUnit.MINUTES);
-    private Duration networkMaxIdleTime = new Duration(200, TimeUnit.SECONDS);
+    private Duration threadMaxIdleTime = new Duration(1, MINUTES);
+    private Duration networkMaxIdleTime = new Duration(200, SECONDS);
     private DataSize maxRequestHeaderSize;
     private int http2MaxConcurrentStreams = 16384;
-    private DataSize http2InitialSessionReceiveWindowSize = new DataSize(16, DataSize.Unit.MEGABYTE);
-    private DataSize http2InitialStreamReceiveWindowSize = new DataSize(16, DataSize.Unit.MEGABYTE);
+    private DataSize http2InitialSessionReceiveWindowSize = new DataSize(16, MEGABYTE);
+    private DataSize http2InitialStreamReceiveWindowSize = new DataSize(16, MEGABYTE);
 
     private String userAuthFile;
 

http-server/src/test/java/io/airlift/http/server/TestHttpServerConfig.java
@@ -22,7 +22,13 @@ import io.airlift.units.Duration;
 import org.testng.annotations.Test;
 
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
+
+import static io.airlift.units.DataSize.Unit.BYTE;
+import static io.airlift.units.DataSize.Unit.GIGABYTE;
+import static io.airlift.units.DataSize.Unit.KILOBYTE;
+import static io.airlift.units.DataSize.Unit.MEGABYTE;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class TestHttpServerConfig
 {
@@ -42,7 +48,7 @@ public class TestHttpServerConfig
                 .setKeystorePassword(null)
                 .setLogPath("var/log/http-request.log")
                 .setLogEnabled(true)
-                .setLogMaxFileSize(new DataSize(Long.MAX_VALUE, DataSize.Unit.BYTE))
+                .setLogMaxFileSize(new DataSize(Long.MAX_VALUE, BYTE))
                 .setLogHistory(15)
                 .setHttpAcceptorThreads(null)
                 .setHttpSelectorThreads(null)
@@ -50,8 +56,8 @@ public class TestHttpServerConfig
                 .setHttpsSelectorThreads(null)
                 .setMinThreads(2)
                 .setMaxThreads(200)
-                .setThreadMaxIdleTime(new Duration(1, TimeUnit.MINUTES))
-                .setNetworkMaxIdleTime(new Duration(200, TimeUnit.SECONDS))
+                .setThreadMaxIdleTime(new Duration(1, MINUTES))
+                .setNetworkMaxIdleTime(new Duration(200, SECONDS))
                 .setUserAuthFile(null)
                 .setAdminEnabled(true)
                 .setAdminPort(0)
@@ -60,8 +66,8 @@ public class TestHttpServerConfig
                 .setMaxRequestHeaderSize(null)
                 .setHttp2MaxConcurrentStreams(16384)
                 .setShowStackTrace(true)
-                .setHttp2InitialSessionReceiveWindowSize(new DataSize(16, DataSize.Unit.MEGABYTE))
-                .setHttp2InitialStreamReceiveWindowSize(new DataSize(16, DataSize.Unit.MEGABYTE))
+                .setHttp2InitialSessionReceiveWindowSize(new DataSize(16, MEGABYTE))
+                .setHttp2InitialStreamReceiveWindowSize(new DataSize(16, MEGABYTE))
         );
     }
 
@@ -116,7 +122,7 @@ public class TestHttpServerConfig
                 .setSecureRandomAlgorithm("NativePRNG")
                 .setLogPath("/log")
                 .setLogEnabled(false)
-                .setLogMaxFileSize(new DataSize(1, DataSize.Unit.GIGABYTE))
+                .setLogMaxFileSize(new DataSize(1, GIGABYTE))
                 .setLogHistory(1)
                 .setHttpAcceptorThreads(10)
                 .setHttpSelectorThreads(11)
@@ -124,9 +130,9 @@ public class TestHttpServerConfig
                 .setHttpsSelectorThreads(13)
                 .setMinThreads(100)
                 .setMaxThreads(500)
-                .setThreadMaxIdleTime(new Duration(10, TimeUnit.MINUTES))
-                .setNetworkMaxIdleTime(new Duration(20, TimeUnit.MINUTES))
-                .setMaxRequestHeaderSize(new DataSize(32, DataSize.Unit.KILOBYTE))
+                .setThreadMaxIdleTime(new Duration(10, MINUTES))
+                .setNetworkMaxIdleTime(new Duration(20, MINUTES))
+                .setMaxRequestHeaderSize(new DataSize(32, KILOBYTE))
                 .setUserAuthFile("/auth")
                 .setAdminEnabled(false)
                 .setAdminPort(3)
@@ -134,8 +140,8 @@ public class TestHttpServerConfig
                 .setAdminMaxThreads(4)
                 .setHttp2MaxConcurrentStreams(1234)
                 .setShowStackTrace(false)
-                .setHttp2InitialSessionReceiveWindowSize(new DataSize(4, DataSize.Unit.MEGABYTE))
-                .setHttp2InitialStreamReceiveWindowSize(new DataSize(4, DataSize.Unit.MEGABYTE));
+                .setHttp2InitialSessionReceiveWindowSize(new DataSize(4, MEGABYTE))
+                .setHttp2InitialStreamReceiveWindowSize(new DataSize(4, MEGABYTE));
 
         ConfigAssertions.assertFullMapping(properties, expected);
     }

proofpoint/platform

http-client/src/main/java/com/proofpoint/http/client/HttpClientConfig.java
@@ -20,13 +20,15 @@ import com.proofpoint.configuration.Config;
 import com.proofpoint.configuration.DefunctConfig;
 import com.proofpoint.configuration.LegacyConfig;
 import com.proofpoint.units.DataSize;
-import com.proofpoint.units.DataSize.Unit;
 import com.proofpoint.units.Duration;
 import com.proofpoint.units.MinDuration;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import java.util.concurrent.TimeUnit;
+
+import static com.proofpoint.units.DataSize.Unit.MEGABYTE;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 @DefunctConfig({"http-client.max-connections", "http-client.keep-alive-interval"})
 public class HttpClientConfig
@@ -36,12 +38,12 @@ public class HttpClientConfig
     public static final String JAVAX_NET_SSL_TRUST_STORE = "javax.net.ssl.trustStore";
     public static final String JAVAX_NET_SSL_TRUST_STORE_PASSWORD = "javax.net.ssl.trustStorePassword";
 
-    private Duration connectTimeout = new Duration(2, TimeUnit.SECONDS);
+    private Duration connectTimeout = new Duration(2, SECONDS);
     private Duration requestTimeout = null;
-    private Duration idleTimeout = new Duration(1, TimeUnit.MINUTES);
+    private Duration idleTimeout = new Duration(1, MINUTES);
     private int maxConnectionsPerServer = 20;
     private int maxRequestsQueuedPerDestination = 20;
-    private DataSize maxContentLength = new DataSize(16, Unit.MEGABYTE);
+    private DataSize maxContentLength = new DataSize(16, MEGABYTE);
     private HostAndPort socksProxy;
     private String keyStorePath = System.getProperty(JAVAX_NET_SSL_KEY_STORE);
     private String keyStorePassword = System.getProperty(JAVAX_NET_SSL_KEY_STORE_PASSWORD);

http-client/src/test/java/com/proofpoint/http/client/TestHttpClientConfig.java
@@ -18,14 +18,12 @@ package com.proofpoint.http.client;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.net.HostAndPort;
 import com.proofpoint.units.DataSize;
-import com.proofpoint.units.DataSize.Unit;
 import com.proofpoint.units.Duration;
 import org.testng.annotations.Test;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import static com.proofpoint.configuration.testing.ConfigAssertions.assertFullMapping;
 import static com.proofpoint.configuration.testing.ConfigAssertions.assertLegacyEquivalence;
@@ -34,6 +32,9 @@ import static com.proofpoint.configuration.testing.ConfigAssertions.recordDefaul
 import static com.proofpoint.http.client.HttpClientConfig.JAVAX_NET_SSL_KEY_STORE;
 import static com.proofpoint.http.client.HttpClientConfig.JAVAX_NET_SSL_KEY_STORE_PASSWORD;
 import static com.proofpoint.testing.ValidationAssertions.assertFailsValidation;
+import static com.proofpoint.units.DataSize.Unit.MEGABYTE;
+import static java.util.concurrent.TimeUnit.MINUTES;
+import static java.util.concurrent.TimeUnit.SECONDS;
 
 @SuppressWarnings("deprecation")
 public class TestHttpClientConfig
@@ -42,12 +43,12 @@ public class TestHttpClientConfig
     public void testDefaults()
     {
         assertRecordedDefaults(recordDefaults(HttpClientConfig.class)
-                .setConnectTimeout(new Duration(2, TimeUnit.SECONDS))
+                .setConnectTimeout(new Duration(2, SECONDS))
                 .setRequestTimeout(null)
-                .setIdleTimeout(new Duration(1, TimeUnit.MINUTES))
+                .setIdleTimeout(new Duration(1, MINUTES))
                 .setMaxConnectionsPerServer(20)
                 .setMaxRequestsQueuedPerDestination(20)
-                .setMaxContentLength(new DataSize(16, Unit.MEGABYTE))
+                .setMaxContentLength(new DataSize(16, MEGABYTE))
                 .setSocksProxy(null)
                 .setKeyStorePath(System.getProperty(JAVAX_NET_SSL_KEY_STORE))
                 .setKeyStorePassword(System.getProperty(JAVAX_NET_SSL_KEY_STORE_PASSWORD))
@@ -73,12 +74,12 @@ public class TestHttpClientConfig
                 .build();
 
         HttpClientConfig expected = new HttpClientConfig()
-                .setConnectTimeout(new Duration(4, TimeUnit.SECONDS))
-                .setRequestTimeout(new Duration(15, TimeUnit.SECONDS))
-                .setIdleTimeout(new Duration(5, TimeUnit.SECONDS))
+                .setConnectTimeout(new Duration(4, SECONDS))
+                .setRequestTimeout(new Duration(15, SECONDS))
+                .setIdleTimeout(new Duration(5, SECONDS))
                 .setMaxConnectionsPerServer(3)
                 .setMaxRequestsQueuedPerDestination(10)
-                .setMaxContentLength(new DataSize(1, Unit.MEGABYTE))
+                .setMaxContentLength(new DataSize(1, MEGABYTE))
                 .setSocksProxy(HostAndPort.fromParts("localhost", 1080))
                 .setKeyStorePath("key-store")
                 .setKeyStorePassword("key-store-password")

http-server/src/main/java/com/proofpoint/http/server/HttpServerConfig.java
@@ -20,12 +20,13 @@ import com.proofpoint.configuration.ConfigDescription;
 import com.proofpoint.configuration.ConfigSecuritySensitive;
 import com.proofpoint.configuration.DefunctConfig;
 import com.proofpoint.units.DataSize;
-import com.proofpoint.units.DataSize.Unit;
 import com.proofpoint.units.Duration;
 
 import javax.validation.constraints.Min;
-import java.util.concurrent.TimeUnit;
 
+import static com.proofpoint.units.DataSize.Unit.GIGABYTE;
+import static com.proofpoint.units.DataSize.Unit.MEGABYTE;
+import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 @DefunctConfig({
@@ -66,9 +67,9 @@ public class HttpServerConfig
     private String logPath = "var/log/http-request.log";
     private boolean logEnabled = true;
     private LogFormat logFormat = LogFormat.JSON;
-    private DataSize logMaxSegmentSize = new DataSize(100, Unit.MEGABYTE);
+    private DataSize logMaxSegmentSize = new DataSize(100, MEGABYTE);
     private int logMaxHistory = 30;
-    private DataSize logMaxTotalSize = new DataSize(1, Unit.GIGABYTE);
+    private DataSize logMaxTotalSize = new DataSize(1, GIGABYTE);
 
     private Integer httpAcceptorThreads;
     private Integer httpSelectorThreads;
@@ -77,7 +78,7 @@ public class HttpServerConfig
 
     private int minThreads = 2;
     private int maxThreads = 200;
-    private Duration threadMaxIdleTime = new Duration(1, TimeUnit.MINUTES);
+    private Duration threadMaxIdleTime = new Duration(1, MINUTES);
     private Duration networkMaxIdleTime = new Duration(200, SECONDS);
     private Duration stopTimeout = new Duration(30, SECONDS);
     private DataSize maxRequestHeaderSize;

http-server/src/test/java/com/proofpoint/http/server/TestHttpServerConfig.java
@@ -19,17 +19,18 @@ import com.google.common.collect.ImmutableMap;
 import com.proofpoint.configuration.testing.ConfigAssertions;
 import com.proofpoint.http.server.HttpServerConfig.LogFormat;
 import com.proofpoint.units.DataSize;
-import com.proofpoint.units.DataSize.Unit;
 import com.proofpoint.units.Duration;
 import org.testng.annotations.Test;
 
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import static com.proofpoint.configuration.testing.ConfigAssertions.assertFullMapping;
 import static com.proofpoint.configuration.testing.ConfigAssertions.assertLegacyEquivalence;
 import static com.proofpoint.configuration.testing.ConfigAssertions.assertRecordedDefaults;
+import static com.proofpoint.units.DataSize.Unit.GIGABYTE;
+import static com.proofpoint.units.DataSize.Unit.MEGABYTE;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.MINUTES;
 import static java.util.concurrent.TimeUnit.SECONDS;
 
 public class TestHttpServerConfig
@@ -48,16 +49,16 @@ public class TestHttpServerConfig
                 .setLogPath("var/log/http-request.log")
                 .setLogEnabled(true)
                 .setLogFormat(LogFormat.JSON)
-                .setLogMaxSegmentSize(new DataSize(100, Unit.MEGABYTE))
+                .setLogMaxSegmentSize(new DataSize(100, MEGABYTE))
                 .setLogMaxHistory(30)
-                .setLogMaxTotalSize(new DataSize(1, Unit.GIGABYTE))
+                .setLogMaxTotalSize(new DataSize(1, GIGABYTE))
                 .setHttpAcceptorThreads(null)
                 .setHttpSelectorThreads(null)
                 .setHttpsAcceptorThreads(null)
                 .setHttpsSelectorThreads(null)
                 .setMinThreads(2)
                 .setMaxThreads(200)
-                .setThreadMaxIdleTime(new Duration(1, TimeUnit.MINUTES))
+                .setThreadMaxIdleTime(new Duration(1, MINUTES))
                 .setNetworkMaxIdleTime(new Duration(200, SECONDS))
                 .setUserAuthFile(null)
                 .setAdminEnabled(true)
@@ -118,17 +119,17 @@ public class TestHttpServerConfig
                 .setLogPath("/log")
                 .setLogEnabled(false)
                 .setLogFormat(LogFormat.TSV)
-                .setLogMaxSegmentSize(new DataSize(1, Unit.GIGABYTE))
+                .setLogMaxSegmentSize(new DataSize(1, GIGABYTE))
                 .setLogMaxHistory(25)
-                .setLogMaxTotalSize(new DataSize(5, Unit.GIGABYTE))
+                .setLogMaxTotalSize(new DataSize(5, GIGABYTE))
                 .setHttpAcceptorThreads(10)
                 .setHttpSelectorThreads(11)
                 .setHttpsAcceptorThreads(12)
                 .setHttpsSelectorThreads(13)
                 .setMinThreads(100)
                 .setMaxThreads(500)
-                .setThreadMaxIdleTime(new Duration(10, TimeUnit.MINUTES))
-                .setNetworkMaxIdleTime(new Duration(20, TimeUnit.MINUTES))
+                .setThreadMaxIdleTime(new Duration(10, MINUTES))
+                .setNetworkMaxIdleTime(new Duration(20, MINUTES))
                 .setMaxRequestHeaderSize(new DataSize(32, DataSize.Unit.KILOBYTE))
                 .setUserAuthFile("/auth")
                 .setAdminEnabled(false)