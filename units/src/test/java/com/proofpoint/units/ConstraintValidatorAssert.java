airlift/units

Use AssertJ for assertions

src/test/java/io/airlift/units/ConstraintValidatorAssert.java
@@ -1,0 +1,79 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.airlift.units;
+
+import org.assertj.core.api.AbstractAssert;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import java.lang.annotation.Annotation;
+
+final class ConstraintValidatorAssert<A extends Annotation, T>
+        extends AbstractAssert<ConstraintValidatorAssert<A, T>, ConstraintValidator<A, T>>
+{
+    public ConstraintValidatorAssert(ConstraintValidator<A, T> actual)
+    {
+        super(actual, ConstraintValidatorAssert.class);
+    }
+
+    public static <A extends Annotation, T> ConstraintValidatorAssert<A, T> assertThat(ConstraintValidator<A, T> actual)
+    {
+        return new ConstraintValidatorAssert<>(actual);
+    }
+
+    public void isValidFor(T value)
+    {
+        isNotNull();
+        if (!actual.isValid(value, new MockContext())) {
+            failWithMessage("Expected <%s> to be valid for <%s>", actual, value);
+        }
+    }
+
+    public void isInvalidFor(T value)
+    {
+        isNotNull();
+        if (actual.isValid(value, new MockContext())) {
+            failWithMessage("Expected <%s> to be invalid for <%s>", actual, value);
+        }
+    }
+
+    private static class MockContext
+            implements ConstraintValidatorContext
+    {
+        @Override
+        public void disableDefaultConstraintViolation()
+        {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public String getDefaultConstraintMessageTemplate()
+        {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public ConstraintViolationBuilder buildConstraintViolationWithTemplate(String s)
+        {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public <T> T unwrap(Class<T> type)
+        {
+            throw new UnsupportedOperationException();
+        }
+    }
+}

src/test/java/io/airlift/units/TestDataSizeValidator.java
@@ -16,7 +16,6 @@ package io.airlift.units;
 import org.apache.bval.jsr.ApacheValidationProvider;
 import org.testng.annotations.Test;
 
-import javax.validation.ConstraintValidatorContext;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.ValidationException;
@@ -24,13 +23,11 @@ import javax.validation.Validator;
 
 import java.util.Set;
 
-import static com.google.common.base.Throwables.getRootCause;
-import static io.airlift.testing.Assertions.assertInstanceOf;
+import static io.airlift.units.ConstraintValidatorAssert.assertThat;
 import static io.airlift.units.DataSize.Unit.GIGABYTE;
 import static io.airlift.units.DataSize.Unit.KILOBYTE;
 import static io.airlift.units.DataSize.Unit.MEGABYTE;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 
@@ -44,14 +41,14 @@ public class TestDataSizeValidator
         MaxDataSizeValidator maxValidator = new MaxDataSizeValidator();
         maxValidator.initialize(new MockMaxDataSize(new DataSize(8, MEGABYTE)));
 
-        assertTrue(maxValidator.isValid(new DataSize(0, KILOBYTE), new MockContext()));
-        assertTrue(maxValidator.isValid(new DataSize(5, KILOBYTE), new MockContext()));
-        assertTrue(maxValidator.isValid(new DataSize(5005, KILOBYTE), new MockContext()));
-        assertTrue(maxValidator.isValid(new DataSize(5, MEGABYTE), new MockContext()));
-        assertTrue(maxValidator.isValid(new DataSize(8, MEGABYTE), new MockContext()));
-        assertTrue(maxValidator.isValid(new DataSize(8192, KILOBYTE), new MockContext()));
-        assertFalse(maxValidator.isValid(new DataSize(9, MEGABYTE), new MockContext()));
-        assertFalse(maxValidator.isValid(new DataSize(1, GIGABYTE), new MockContext()));
+        assertThat(maxValidator).isValidFor(new DataSize(0, KILOBYTE));
+        assertThat(maxValidator).isValidFor(new DataSize(5, KILOBYTE));
+        assertThat(maxValidator).isValidFor(new DataSize(5005, KILOBYTE));
+        assertThat(maxValidator).isValidFor(new DataSize(5, MEGABYTE));
+        assertThat(maxValidator).isValidFor(new DataSize(8, MEGABYTE));
+        assertThat(maxValidator).isValidFor(new DataSize(8192, KILOBYTE));
+        assertThat(maxValidator).isInvalidFor(new DataSize(9, MEGABYTE));
+        assertThat(maxValidator).isInvalidFor(new DataSize(1, GIGABYTE));
     }
 
     @Test
@@ -60,11 +57,11 @@ public class TestDataSizeValidator
         MinDataSizeValidator minValidator = new MinDataSizeValidator();
         minValidator.initialize(new MockMinDataSize(new DataSize(4, MEGABYTE)));
 
-        assertTrue(minValidator.isValid(new DataSize(4, MEGABYTE), new MockContext()));
-        assertTrue(minValidator.isValid(new DataSize(4096, KILOBYTE), new MockContext()));
-        assertTrue(minValidator.isValid(new DataSize(5, MEGABYTE), new MockContext()));
-        assertFalse(minValidator.isValid(new DataSize(0, GIGABYTE), new MockContext()));
-        assertFalse(minValidator.isValid(new DataSize(1, MEGABYTE), new MockContext()));
+        assertThat(minValidator).isValidFor(new DataSize(4, MEGABYTE));
+        assertThat(minValidator).isValidFor(new DataSize(4096, KILOBYTE));
+        assertThat(minValidator).isValidFor(new DataSize(5, MEGABYTE));
+        assertThat(minValidator).isInvalidFor(new DataSize(0, GIGABYTE));
+        assertThat(minValidator).isInvalidFor(new DataSize(1, MEGABYTE));
     }
 
     @Test
@@ -87,7 +84,7 @@ public class TestDataSizeValidator
             fail("expected a ValidationException caused by an IllegalArgumentException");
         }
         catch (ValidationException e) {
-            assertInstanceOf(getRootCause(e), IllegalArgumentException.class);
+            assertThat(e).hasRootCauseInstanceOf(IllegalArgumentException.class);
         }
     }
 
@@ -99,7 +96,7 @@ public class TestDataSizeValidator
             fail("expected a ValidationException caused by an IllegalArgumentException");
         }
         catch (ValidationException e) {
-            assertInstanceOf(getRootCause(e), IllegalArgumentException.class);
+            assertThat(e).hasRootCauseInstanceOf(IllegalArgumentException.class);
         }
     }
 
@@ -116,10 +113,10 @@ public class TestDataSizeValidator
     {
         ConstrainedDataSize object = new ConstrainedDataSize(new DataSize(11, MEGABYTE));
         Set<ConstraintViolation<ConstrainedDataSize>> violations = VALIDATOR.validate(object);
-        assertEquals(violations.size(), 2);
+        assertThat(violations).hasSize(2);
 
         for (ConstraintViolation<ConstrainedDataSize> violation : violations) {
-            assertInstanceOf(violation.getConstraintDescriptor().getAnnotation(), MaxDataSize.class);
+            assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(MaxDataSize.class);
         }
     }
 
@@ -128,38 +125,10 @@ public class TestDataSizeValidator
     {
         ConstrainedDataSize object = new ConstrainedDataSize(new DataSize(1, MEGABYTE));
         Set<ConstraintViolation<ConstrainedDataSize>> violations = VALIDATOR.validate(object);
-        assertEquals(violations.size(), 2);
+        assertThat(violations).hasSize(2);
 
         for (ConstraintViolation<ConstrainedDataSize> violation : violations) {
-            assertInstanceOf(violation.getConstraintDescriptor().getAnnotation(), MinDataSize.class);
-        }
-    }
-
-    private static class MockContext
-            implements ConstraintValidatorContext
-    {
-        @Override
-        public void disableDefaultConstraintViolation()
-        {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public String getDefaultConstraintMessageTemplate()
-        {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public ConstraintViolationBuilder buildConstraintViolationWithTemplate(String s)
-        {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public <T> T unwrap(Class<T> type)
-        {
-            throw new UnsupportedOperationException();
+            assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(MinDataSize.class);
         }
     }
 

src/test/java/io/airlift/units/TestDurationValidator.java
@@ -18,7 +18,6 @@ package io.airlift.units;
 import org.apache.bval.jsr.ApacheValidationProvider;
 import org.testng.annotations.Test;
 
-import javax.validation.ConstraintValidatorContext;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.ValidationException;
@@ -27,11 +26,8 @@ import javax.validation.Validator;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-import static com.google.common.base.Throwables.getRootCause;
-import static io.airlift.testing.Assertions.assertInstanceOf;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
+import static io.airlift.units.ConstraintValidatorAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.testng.Assert.fail;
 
 public class TestDurationValidator
@@ -44,9 +40,9 @@ public class TestDurationValidator
         MaxDurationValidator maxValidator = new MaxDurationValidator();
         maxValidator.initialize(new MockMaxDuration(new Duration(5, TimeUnit.SECONDS)));
 
-        assertTrue(maxValidator.isValid(new Duration(0, TimeUnit.SECONDS), new MockContext()));
-        assertTrue(maxValidator.isValid(new Duration(5, TimeUnit.SECONDS), new MockContext()));
-        assertFalse(maxValidator.isValid(new Duration(6, TimeUnit.SECONDS), new MockContext()));
+        assertThat(maxValidator).isValidFor(new Duration(0, TimeUnit.SECONDS));
+        assertThat(maxValidator).isValidFor(new Duration(5, TimeUnit.SECONDS));
+        assertThat(maxValidator).isInvalidFor(new Duration(6, TimeUnit.SECONDS));
     }
 
     @Test
@@ -55,9 +51,9 @@ public class TestDurationValidator
         MinDurationValidator minValidator = new MinDurationValidator();
         minValidator.initialize(new MockMinDuration(new Duration(5, TimeUnit.SECONDS)));
 
-        assertTrue(minValidator.isValid(new Duration(5, TimeUnit.SECONDS), new MockContext()));
-        assertTrue(minValidator.isValid(new Duration(6, TimeUnit.SECONDS), new MockContext()));
-        assertFalse(minValidator.isValid(new Duration(0, TimeUnit.SECONDS), new MockContext()));
+        assertThat(minValidator).isValidFor(new Duration(5, TimeUnit.SECONDS));
+        assertThat(minValidator).isValidFor(new Duration(6, TimeUnit.SECONDS));
+        assertThat(minValidator).isInvalidFor(new Duration(0, TimeUnit.SECONDS));
     }
 
     @Test
@@ -80,7 +76,7 @@ public class TestDurationValidator
             fail("expected a ValidationException caused by an IllegalArgumentException");
         }
         catch (ValidationException e) {
-            assertInstanceOf(getRootCause(e), IllegalArgumentException.class);
+            assertThat(e).hasRootCauseInstanceOf(IllegalArgumentException.class);
         }
     }
 
@@ -92,7 +88,7 @@ public class TestDurationValidator
             fail("expected a ValidationException caused by an IllegalArgumentException");
         }
         catch (ValidationException e) {
-            assertInstanceOf(getRootCause(e), IllegalArgumentException.class);
+            assertThat(e).hasRootCauseInstanceOf(IllegalArgumentException.class);
         }
     }
 
@@ -101,7 +97,7 @@ public class TestDurationValidator
     {
         ConstrainedDuration object = new ConstrainedDuration(new Duration(7, TimeUnit.SECONDS));
         Set<ConstraintViolation<ConstrainedDuration>> violations = validator.validate(object);
-        assertTrue(violations.isEmpty());
+        assertThat(violations).isEmpty();
     }
 
     @Test
@@ -109,10 +105,10 @@ public class TestDurationValidator
     {
         ConstrainedDuration object = new ConstrainedDuration(new Duration(11, TimeUnit.SECONDS));
         Set<ConstraintViolation<ConstrainedDuration>> violations = validator.validate(object);
-        assertEquals(violations.size(), 2);
+        assertThat(violations).hasSize(2);
 
         for (ConstraintViolation<ConstrainedDuration> violation : violations) {
-            assertInstanceOf(violation.getConstraintDescriptor().getAnnotation(), MaxDuration.class);
+            assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(MaxDuration.class);
         }
     }
 
@@ -121,38 +117,10 @@ public class TestDurationValidator
     {
         ConstrainedDuration object = new ConstrainedDuration(new Duration(1, TimeUnit.SECONDS));
         Set<ConstraintViolation<ConstrainedDuration>> violations = validator.validate(object);
-        assertEquals(violations.size(), 2);
+        assertThat(violations).hasSize(2);
 
         for (ConstraintViolation<ConstrainedDuration> violation : violations) {
-            assertInstanceOf(violation.getConstraintDescriptor().getAnnotation(), MinDuration.class);
-        }
-    }
-
-    private static class MockContext
-            implements ConstraintValidatorContext
-    {
-        @Override
-        public void disableDefaultConstraintViolation()
-        {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public String getDefaultConstraintMessageTemplate()
-        {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public ConstraintValidatorContext.ConstraintViolationBuilder buildConstraintViolationWithTemplate(String s)
-        {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public <T> T unwrap(Class<T> type)
-        {
-            throw new UnsupportedOperationException();
+            assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(MinDuration.class);
         }
     }
 

proofpoint/platform

units/src/test/java/com/proofpoint/units/ConstraintValidatorAssert.java
@@ -1,0 +1,79 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.proofpoint.units;
+
+import org.assertj.core.api.AbstractAssert;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import java.lang.annotation.Annotation;
+
+final class ConstraintValidatorAssert<A extends Annotation, T>
+        extends AbstractAssert<ConstraintValidatorAssert<A, T>, ConstraintValidator<A, T>>
+{
+    public ConstraintValidatorAssert(ConstraintValidator<A, T> actual)
+    {
+        super(actual, ConstraintValidatorAssert.class);
+    }
+
+    public static <A extends Annotation, T> ConstraintValidatorAssert<A, T> assertThat(ConstraintValidator<A, T> actual)
+    {
+        return new ConstraintValidatorAssert<>(actual);
+    }
+
+    public void isValidFor(T value)
+    {
+        isNotNull();
+        if (!actual.isValid(value, new MockContext())) {
+            failWithMessage("Expected <%s> to be valid for <%s>", actual, value);
+        }
+    }
+
+    public void isInvalidFor(T value)
+    {
+        isNotNull();
+        if (actual.isValid(value, new MockContext())) {
+            failWithMessage("Expected <%s> to be invalid for <%s>", actual, value);
+        }
+    }
+
+    private static class MockContext
+            implements ConstraintValidatorContext
+    {
+        @Override
+        public void disableDefaultConstraintViolation()
+        {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public String getDefaultConstraintMessageTemplate()
+        {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public ConstraintViolationBuilder buildConstraintViolationWithTemplate(String s)
+        {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public <T> T unwrap(Class<T> type)
+        {
+            throw new UnsupportedOperationException();
+        }
+    }
+}

units/src/test/java/com/proofpoint/units/TestDataSizeValidator.java
@@ -16,20 +16,17 @@ package com.proofpoint.units;
 import org.apache.bval.jsr.ApacheValidationProvider;
 import org.testng.annotations.Test;
 
-import javax.validation.ConstraintValidatorContext;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.ValidationException;
 import javax.validation.Validator;
 import java.util.Set;
 
-import static com.google.common.base.Throwables.getRootCause;
-import static com.proofpoint.testing.Assertions.assertInstanceOf;
+import static com.proofpoint.units.ConstraintValidatorAssert.assertThat;
 import static com.proofpoint.units.DataSize.Unit.GIGABYTE;
 import static com.proofpoint.units.DataSize.Unit.KILOBYTE;
 import static com.proofpoint.units.DataSize.Unit.MEGABYTE;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.testng.Assert.assertTrue;
 import static org.testng.Assert.fail;
 
@@ -43,14 +40,14 @@ public class TestDataSizeValidator
         MaxDataSizeValidator maxValidator = new MaxDataSizeValidator();
         maxValidator.initialize(new MockMaxDataSize(new DataSize(8, MEGABYTE)));
 
-        assertTrue(maxValidator.isValid(new DataSize(0, KILOBYTE), new MockContext()));
-        assertTrue(maxValidator.isValid(new DataSize(5, KILOBYTE), new MockContext()));
-        assertTrue(maxValidator.isValid(new DataSize(5005, KILOBYTE), new MockContext()));
-        assertTrue(maxValidator.isValid(new DataSize(5, MEGABYTE), new MockContext()));
-        assertTrue(maxValidator.isValid(new DataSize(8, MEGABYTE), new MockContext()));
-        assertTrue(maxValidator.isValid(new DataSize(8192, KILOBYTE), new MockContext()));
-        assertFalse(maxValidator.isValid(new DataSize(9, MEGABYTE), new MockContext()));
-        assertFalse(maxValidator.isValid(new DataSize(1, GIGABYTE), new MockContext()));
+        assertThat(maxValidator).isValidFor(new DataSize(0, KILOBYTE));
+        assertThat(maxValidator).isValidFor(new DataSize(5, KILOBYTE));
+        assertThat(maxValidator).isValidFor(new DataSize(5005, KILOBYTE));
+        assertThat(maxValidator).isValidFor(new DataSize(5, MEGABYTE));
+        assertThat(maxValidator).isValidFor(new DataSize(8, MEGABYTE));
+        assertThat(maxValidator).isValidFor(new DataSize(8192, KILOBYTE));
+        assertThat(maxValidator).isInvalidFor(new DataSize(9, MEGABYTE));
+        assertThat(maxValidator).isInvalidFor(new DataSize(1, GIGABYTE));
     }
 
     @Test
@@ -59,11 +56,11 @@ public class TestDataSizeValidator
         MinDataSizeValidator minValidator = new MinDataSizeValidator();
         minValidator.initialize(new MockMinDataSize(new DataSize(4, MEGABYTE)));
 
-        assertTrue(minValidator.isValid(new DataSize(4, MEGABYTE), new MockContext()));
-        assertTrue(minValidator.isValid(new DataSize(4096, KILOBYTE), new MockContext()));
-        assertTrue(minValidator.isValid(new DataSize(5, MEGABYTE), new MockContext()));
-        assertFalse(minValidator.isValid(new DataSize(0, GIGABYTE), new MockContext()));
-        assertFalse(minValidator.isValid(new DataSize(1, MEGABYTE), new MockContext()));
+        assertThat(minValidator).isValidFor(new DataSize(4, MEGABYTE));
+        assertThat(minValidator).isValidFor(new DataSize(4096, KILOBYTE));
+        assertThat(minValidator).isValidFor(new DataSize(5, MEGABYTE));
+        assertThat(minValidator).isInvalidFor(new DataSize(0, GIGABYTE));
+        assertThat(minValidator).isInvalidFor(new DataSize(1, MEGABYTE));
     }
 
     @Test
@@ -86,7 +83,7 @@ public class TestDataSizeValidator
             fail("expected a ValidationException caused by an IllegalArgumentException");
         }
         catch (ValidationException e) {
-            assertInstanceOf(getRootCause(e), IllegalArgumentException.class);
+            assertThat(e).hasRootCauseInstanceOf(IllegalArgumentException.class);
         }
     }
 
@@ -98,7 +95,7 @@ public class TestDataSizeValidator
             fail("expected a ValidationException caused by an IllegalArgumentException");
         }
         catch (ValidationException e) {
-            assertInstanceOf(getRootCause(e), IllegalArgumentException.class);
+            assertThat(e).hasRootCauseInstanceOf(IllegalArgumentException.class);
         }
     }
 
@@ -115,10 +112,10 @@ public class TestDataSizeValidator
     {
         ConstrainedDataSize object = new ConstrainedDataSize(new DataSize(11, MEGABYTE));
         Set<ConstraintViolation<ConstrainedDataSize>> violations = VALIDATOR.validate(object);
-        assertEquals(violations.size(), 2);
+        assertThat(violations).hasSize(2);
 
         for (ConstraintViolation<ConstrainedDataSize> violation : violations) {
-            assertInstanceOf(violation.getConstraintDescriptor().getAnnotation(), MaxDataSize.class);
+            assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(MaxDataSize.class);
         }
     }
 
@@ -127,38 +124,10 @@ public class TestDataSizeValidator
     {
         ConstrainedDataSize object = new ConstrainedDataSize(new DataSize(1, MEGABYTE));
         Set<ConstraintViolation<ConstrainedDataSize>> violations = VALIDATOR.validate(object);
-        assertEquals(violations.size(), 2);
+        assertThat(violations).hasSize(2);
 
         for (ConstraintViolation<ConstrainedDataSize> violation : violations) {
-            assertInstanceOf(violation.getConstraintDescriptor().getAnnotation(), MinDataSize.class);
-        }
-    }
-
-    private static class MockContext
-            implements ConstraintValidatorContext
-    {
-        @Override
-        public void disableDefaultConstraintViolation()
-        {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public String getDefaultConstraintMessageTemplate()
-        {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public ConstraintViolationBuilder buildConstraintViolationWithTemplate(String s)
-        {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public <T> T unwrap(Class<T> type)
-        {
-            throw new UnsupportedOperationException();
+            assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(MinDataSize.class);
         }
     }
 

units/src/test/java/com/proofpoint/units/TestDurationValidator.java
@@ -18,7 +18,6 @@ package com.proofpoint.units;
 import org.apache.bval.jsr.ApacheValidationProvider;
 import org.testng.annotations.Test;
 
-import javax.validation.ConstraintValidatorContext;
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.ValidationException;
@@ -26,11 +25,8 @@ import javax.validation.Validator;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-import static com.google.common.base.Throwables.getRootCause;
-import static com.proofpoint.testing.Assertions.assertInstanceOf;
-import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertFalse;
-import static org.testng.Assert.assertTrue;
+import static com.proofpoint.units.ConstraintValidatorAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.testng.Assert.fail;
 
 public class TestDurationValidator
@@ -43,9 +39,9 @@ public class TestDurationValidator
         MaxDurationValidator maxValidator = new MaxDurationValidator();
         maxValidator.initialize(new MockMaxDuration(new Duration(5, TimeUnit.SECONDS)));
 
-        assertTrue(maxValidator.isValid(new Duration(0, TimeUnit.SECONDS), new MockContext()));
-        assertTrue(maxValidator.isValid(new Duration(5, TimeUnit.SECONDS), new MockContext()));
-        assertFalse(maxValidator.isValid(new Duration(6, TimeUnit.SECONDS), new MockContext()));
+        assertThat(maxValidator).isValidFor(new Duration(0, TimeUnit.SECONDS));
+        assertThat(maxValidator).isValidFor(new Duration(5, TimeUnit.SECONDS));
+        assertThat(maxValidator).isInvalidFor(new Duration(6, TimeUnit.SECONDS));
     }
 
     @Test
@@ -54,9 +50,9 @@ public class TestDurationValidator
         MinDurationValidator minValidator = new MinDurationValidator();
         minValidator.initialize(new MockMinDuration(new Duration(5, TimeUnit.SECONDS)));
 
-        assertTrue(minValidator.isValid(new Duration(5, TimeUnit.SECONDS), new MockContext()));
-        assertTrue(minValidator.isValid(new Duration(6, TimeUnit.SECONDS), new MockContext()));
-        assertFalse(minValidator.isValid(new Duration(0, TimeUnit.SECONDS), new MockContext()));
+        assertThat(minValidator).isValidFor(new Duration(5, TimeUnit.SECONDS));
+        assertThat(minValidator).isValidFor(new Duration(6, TimeUnit.SECONDS));
+        assertThat(minValidator).isInvalidFor(new Duration(0, TimeUnit.SECONDS));
     }
 
     @Test
@@ -79,7 +75,7 @@ public class TestDurationValidator
             fail("expected a ValidationException caused by an IllegalArgumentException");
         }
         catch (ValidationException e) {
-            assertInstanceOf(getRootCause(e), IllegalArgumentException.class);
+            assertThat(e).hasRootCauseInstanceOf(IllegalArgumentException.class);
         }
     }
 
@@ -91,7 +87,7 @@ public class TestDurationValidator
             fail("expected a ValidationException caused by an IllegalArgumentException");
         }
         catch (ValidationException e) {
-            assertInstanceOf(getRootCause(e), IllegalArgumentException.class);
+            assertThat(e).hasRootCauseInstanceOf(IllegalArgumentException.class);
         }
     }
 
@@ -100,7 +96,7 @@ public class TestDurationValidator
     {
         ConstrainedDuration object = new ConstrainedDuration(new Duration(7, TimeUnit.SECONDS));
         Set<ConstraintViolation<ConstrainedDuration>> violations = validator.validate(object);
-        assertTrue(violations.isEmpty());
+        assertThat(violations).isEmpty();
     }
 
     @Test
@@ -108,10 +104,10 @@ public class TestDurationValidator
     {
         ConstrainedDuration object = new ConstrainedDuration(new Duration(11, TimeUnit.SECONDS));
         Set<ConstraintViolation<ConstrainedDuration>> violations = validator.validate(object);
-        assertEquals(violations.size(), 2);
+        assertThat(violations).hasSize(2);
 
         for (ConstraintViolation<ConstrainedDuration> violation : violations) {
-            assertInstanceOf((violation.getConstraintDescriptor()).getAnnotation(), MaxDuration.class);
+            assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(MaxDuration.class);
         }
     }
 
@@ -120,38 +116,10 @@ public class TestDurationValidator
     {
         ConstrainedDuration object = new ConstrainedDuration(new Duration(1, TimeUnit.SECONDS));
         Set<ConstraintViolation<ConstrainedDuration>> violations = validator.validate(object);
-        assertEquals(violations.size(), 2);
+        assertThat(violations).hasSize(2);
 
         for (ConstraintViolation<ConstrainedDuration> violation : violations) {
-            assertInstanceOf((violation.getConstraintDescriptor()).getAnnotation(), MinDuration.class);
-        }
-    }
-
-    private static class MockContext
-            implements ConstraintValidatorContext
-    {
-        @Override
-        public void disableDefaultConstraintViolation()
-        {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public String getDefaultConstraintMessageTemplate()
-        {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public ConstraintValidatorContext.ConstraintViolationBuilder buildConstraintViolationWithTemplate(String s)
-        {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public <T> T unwrap(Class<T> type)
-        {
-            throw new UnsupportedOperationException();
+            assertThat(violation.getConstraintDescriptor().getAnnotation()).isInstanceOf(MinDuration.class);
         }
     }
 