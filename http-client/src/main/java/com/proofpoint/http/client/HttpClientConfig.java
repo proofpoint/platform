airlift/airlift

Upgrade Jetty to 9.4.6.v20170531

http-client/src/main/java/io/airlift/http/client/HttpClientConfig.java
@@ -23,6 +23,8 @@ import io.airlift.configuration.LegacyConfig;
 import io.airlift.units.DataSize;
 import io.airlift.units.DataSize.Unit;
 import io.airlift.units.Duration;
+import io.airlift.units.MaxDataSize;
+import io.airlift.units.MinDataSize;
 import io.airlift.units.MinDuration;
 
 import javax.validation.constraints.Min;
@@ -56,6 +58,8 @@ public class HttpClientConfig
     private boolean authenticationEnabled;
     private String kerberosPrincipal;
     private String kerberosRemoteServiceName;
+    private DataSize http2InitialSessionReceiveWindowSize = new DataSize(16, Unit.MEGABYTE);
+    private DataSize http2InitialStreamReceiveWindowSize = new DataSize(16, Unit.MEGABYTE);
 
     public boolean isHttp2Enabled()
     {
@@ -287,6 +291,38 @@ public class HttpClientConfig
     public HttpClientConfig setKerberosRemoteServiceName(String serviceName)
     {
         this.kerberosRemoteServiceName = serviceName;
+        return this;
+    }
+
+    @NotNull
+    @MinDataSize("1kB")
+    @MaxDataSize("1GB")
+    public DataSize getHttp2InitialSessionReceiveWindowSize()
+    {
+        return http2InitialSessionReceiveWindowSize;
+    }
+
+    @Config("http-client.http2.session-receive-window-size")
+    @ConfigDescription("Initial size of session's flow control receive window for HTTP/2")
+    public HttpClientConfig setHttp2InitialSessionReceiveWindowSize(DataSize http2InitialSessionReceiveWindowSize)
+    {
+        this.http2InitialSessionReceiveWindowSize = http2InitialSessionReceiveWindowSize;
+        return this;
+    }
+
+    @NotNull
+    @MinDataSize("1kB")
+    @MaxDataSize("1GB")
+    public DataSize getHttp2InitialStreamReceiveWindowSize()
+    {
+        return http2InitialStreamReceiveWindowSize;
+    }
+
+    @Config("http-client.http2.stream-receive-window-size")
+    @ConfigDescription("Initial size of stream's flow control receive window for HTTP/2")
+    public HttpClientConfig setHttp2InitialStreamReceiveWindowSize(DataSize http2InitialStreamReceiveWindowSize)
+    {
+        this.http2InitialStreamReceiveWindowSize = http2InitialStreamReceiveWindowSize;
         return this;
     }
 }

http-client/src/main/java/io/airlift/http/client/jetty/JettyHttpClient.java
@@ -73,6 +73,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Queue;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -91,10 +92,12 @@ import static com.google.common.base.MoreObjects.toStringHelper;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.base.Throwables.throwIfUnchecked;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static io.airlift.units.DataSize.Unit.KILOBYTE;
 import static io.airlift.units.DataSize.Unit.MEGABYTE;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
+import static java.lang.Math.toIntExact;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
@@ -135,12 +138,12 @@ public class JettyHttpClient
 
     public JettyHttpClient()
     {
-        this(new HttpClientConfig(), ImmutableList.<HttpRequestFilter>of());
+        this(new HttpClientConfig(), ImmutableList.of());
     }
 
     public JettyHttpClient(HttpClientConfig config)
     {
-        this(config, ImmutableList.<HttpRequestFilter>of());
+        this(config, ImmutableList.of());
     }
 
     public JettyHttpClient(HttpClientConfig config, Iterable<? extends HttpRequestFilter> requestFilters)
@@ -185,6 +188,8 @@ public class JettyHttpClient
         HttpClientTransport transport;
         if (config.isHttp2Enabled()) {
             HTTP2Client client = new HTTP2Client();
+            client.setInitialSessionRecvWindow(toIntExact(config.getHttp2InitialSessionReceiveWindowSize().toBytes()));
+            client.setInitialStreamRecvWindow(toIntExact(config.getHttp2InitialStreamReceiveWindowSize().toBytes()));
             client.setSelectors(CLIENT_TRANSPORT_SELECTORS);
             transport = new HttpClientTransportOverHTTP2(client);
         }
@@ -623,27 +628,31 @@ public class JettyHttpClient
     private static List<JettyRequestListener> getRequestListenersForDestination(Destination destination)
     {
         return getRequestForDestination(destination).stream()
-                .map(request -> (JettyRequestListener) request.getAttributes().get(PRESTO_STATS_KEY))
-                .filter(listener -> listener != null)
+                .map(request -> request.getAttributes().get(PRESTO_STATS_KEY))
+                .map(JettyRequestListener.class::cast)
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
     }
 
     private static List<org.eclipse.jetty.client.api.Request> getRequestForDestination(Destination destination)
     {
-        PoolingHttpDestination<?> poolingHttpDestination = (PoolingHttpDestination<?>) destination;
+        PoolingHttpDestination poolingHttpDestination = (PoolingHttpDestination) destination;
         Queue<HttpExchange> httpExchanges = poolingHttpDestination.getHttpExchanges();
 
         List<org.eclipse.jetty.client.api.Request> requests = httpExchanges.stream()
                 .map(HttpExchange::getRequest)
                 .collect(Collectors.toList());
 
-        poolingHttpDestination.getConnectionPool().getActiveConnections().stream()
+        ((DuplexConnectionPool) poolingHttpDestination.getConnectionPool()).getActiveConnections().stream()
                 .filter(HttpConnectionOverHTTP.class::isInstance)
-                .map(connection -> ((HttpConnectionOverHTTP) connection).getHttpChannel().getHttpExchange())
-                .filter(exchange -> exchange != null)
+                .map(HttpConnectionOverHTTP.class::cast)
+                .map(connection -> connection.getHttpChannel().getHttpExchange())
+                .filter(Objects::nonNull)
                 .forEach(exchange -> requests.add(exchange.getRequest()));
 
-        return requests.stream().filter(request -> request != null).collect(Collectors.toList());
+        return requests.stream()
+                .filter(Objects::nonNull)
+                .collect(toImmutableList());
     }
 
     private static String dumpRequest(long now, JettyRequestListener listener)
@@ -1377,9 +1386,10 @@ public class JettyHttpClient
                 Distribution distribution = new Distribution();
                 httpClient.getDestinations().stream()
                         .filter(PoolingHttpDestination.class::isInstance)
-                        .map(destination -> (PoolingHttpDestination<?>) destination)
+                        .map(PoolingHttpDestination.class::cast)
                         .map(PoolingHttpDestination::getConnectionPool)
-                        .filter(pool -> pool != null)
+                        .filter(Objects::nonNull)
+                        .map(DuplexConnectionPool.class::cast)
                         .forEach(pool -> processor.process(distribution, pool));
                 return distribution;
             });
@@ -1391,7 +1401,7 @@ public class JettyHttpClient
     {
         interface Processor
         {
-            void process(Distribution distribution, PoolingHttpDestination<?> destination);
+            void process(Distribution distribution, PoolingHttpDestination destination);
         }
 
         public DestinationDistribution(HttpClient httpClient, Processor processor)
@@ -1400,7 +1410,7 @@ public class JettyHttpClient
                 Distribution distribution = new Distribution();
                 httpClient.getDestinations().stream()
                         .filter(PoolingHttpDestination.class::isInstance)
-                        .map(destination -> (PoolingHttpDestination<?>) destination)
+                        .map(PoolingHttpDestination.class::cast)
                         .forEach(destination -> processor.process(distribution, destination));
                 return distribution;
             });
@@ -1422,7 +1432,7 @@ public class JettyHttpClient
                 Distribution distribution = new Distribution();
                 httpClient.getDestinations().stream()
                         .filter(PoolingHttpDestination.class::isInstance)
-                        .map(destination -> (PoolingHttpDestination<?>) destination)
+                        .map(PoolingHttpDestination.class::cast)
                         .map(JettyHttpClient::getRequestListenersForDestination)
                         .flatMap(List::stream)
                         .forEach(listener -> processor.process(distribution, listener, now));

http-server/src/main/java/io/airlift/http/server/HttpServerConfig.java
@@ -22,8 +22,11 @@ import io.airlift.configuration.ConfigDescription;
 import io.airlift.configuration.DefunctConfig;
 import io.airlift.units.DataSize;
 import io.airlift.units.Duration;
+import io.airlift.units.MaxDataSize;
+import io.airlift.units.MinDataSize;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -77,6 +80,8 @@ public class HttpServerConfig
     private Duration networkMaxIdleTime = new Duration(200, TimeUnit.SECONDS);
     private DataSize maxRequestHeaderSize;
     private int http2MaxConcurrentStreams = 16384;
+    private DataSize http2InitialSessionReceiveWindowSize = new DataSize(16, DataSize.Unit.MEGABYTE);
+    private DataSize http2InitialStreamReceiveWindowSize = new DataSize(16, DataSize.Unit.MEGABYTE);
 
     private String userAuthFile;
 
@@ -460,6 +465,38 @@ public class HttpServerConfig
     public HttpServerConfig setShowStackTrace(boolean showStackTrace)
     {
         this.showStackTrace = showStackTrace;
+        return this;
+    }
+
+    @NotNull
+    @MinDataSize("1kB")
+    @MaxDataSize("1GB")
+    public DataSize getHttp2InitialSessionReceiveWindowSize()
+    {
+        return http2InitialSessionReceiveWindowSize;
+    }
+
+    @Config("http-server.http2.session-receive-window-size")
+    @ConfigDescription("Initial size of session's flow control receive window for HTTP/2")
+    public HttpServerConfig setHttp2InitialSessionReceiveWindowSize(DataSize http2InitialSessionReceiveWindowSize)
+    {
+        this.http2InitialSessionReceiveWindowSize = http2InitialSessionReceiveWindowSize;
+        return this;
+    }
+
+    @NotNull
+    @MinDataSize("1kB")
+    @MaxDataSize("1GB")
+    public DataSize getHttp2InitialStreamReceiveWindowSize()
+    {
+        return http2InitialStreamReceiveWindowSize;
+    }
+
+    @Config("http-server.http2.stream-receive-window-size")
+    @ConfigDescription("Initial size of stream's flow control receive window for HTTP/2")
+    public HttpServerConfig setHttp2InitialStreamReceiveWindowSize(DataSize http2InitialStreamReceiveWindowSize)
+    {
+        this.http2InitialStreamReceiveWindowSize = http2InitialStreamReceiveWindowSize;
         return this;
     }
 }

proofpoint/platform

http-client/src/main/java/com/proofpoint/http/client/HttpClientConfig.java
@@ -21,7 +21,10 @@ import com.proofpoint.configuration.ConfigDescription;
 import com.proofpoint.configuration.DefunctConfig;
 import com.proofpoint.configuration.LegacyConfig;
 import com.proofpoint.units.DataSize;
+import com.proofpoint.units.DataSize.Unit;
 import com.proofpoint.units.Duration;
+import com.proofpoint.units.MaxDataSize;
+import com.proofpoint.units.MinDataSize;
 import com.proofpoint.units.MinDuration;
 
 import javax.validation.constraints.Min;
@@ -51,6 +54,8 @@ public class HttpClientConfig
     private String keyStorePassword = System.getProperty(JAVAX_NET_SSL_KEY_STORE_PASSWORD);
     private String trustStorePath;
     private String trustStorePassword;
+    private DataSize http2InitialSessionReceiveWindowSize = new DataSize(16, Unit.MEGABYTE);
+    private DataSize http2InitialStreamReceiveWindowSize = new DataSize(16, Unit.MEGABYTE);
 
     public boolean isHttp2Enabled()
     {
@@ -203,6 +208,38 @@ public class HttpClientConfig
     public HttpClientConfig setTrustStorePassword(String trustStorePassword)
     {
         this.trustStorePassword = trustStorePassword;
+        return this;
+    }
+
+    @NotNull
+    @MinDataSize("1kB")
+    @MaxDataSize("1GB")
+    public DataSize getHttp2InitialSessionReceiveWindowSize()
+    {
+        return http2InitialSessionReceiveWindowSize;
+    }
+
+    @Config("http-client.http2.session-receive-window-size")
+    @ConfigDescription("Initial size of session's flow control receive window for HTTP/2")
+    public HttpClientConfig setHttp2InitialSessionReceiveWindowSize(DataSize http2InitialSessionReceiveWindowSize)
+    {
+        this.http2InitialSessionReceiveWindowSize = http2InitialSessionReceiveWindowSize;
+        return this;
+    }
+
+    @NotNull
+    @MinDataSize("1kB")
+    @MaxDataSize("1GB")
+    public DataSize getHttp2InitialStreamReceiveWindowSize()
+    {
+        return http2InitialStreamReceiveWindowSize;
+    }
+
+    @Config("http-client.http2.stream-receive-window-size")
+    @ConfigDescription("Initial size of stream's flow control receive window for HTTP/2")
+    public HttpClientConfig setHttp2InitialStreamReceiveWindowSize(DataSize http2InitialStreamReceiveWindowSize)
+    {
+        this.http2InitialStreamReceiveWindowSize = http2InitialStreamReceiveWindowSize;
         return this;
     }
 }

http-client/src/main/java/com/proofpoint/http/client/jetty/JettyHttpClient.java
@@ -74,6 +74,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Queue;
 import java.util.concurrent.CancellationException;
@@ -89,12 +90,14 @@ import static com.google.common.base.MoreObjects.toStringHelper;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.base.Throwables.throwIfUnchecked;
+import static com.google.common.collect.ImmutableList.toImmutableList;
 import static com.proofpoint.tracetoken.TraceTokenManager.getCurrentTraceToken;
 import static com.proofpoint.tracetoken.TraceTokenManager.registerTraceToken;
 import static com.proofpoint.units.DataSize.Unit.KILOBYTE;
 import static com.proofpoint.units.DataSize.Unit.MEGABYTE;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
+import static java.lang.Math.toIntExact;
 import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
@@ -203,6 +206,8 @@ public class JettyHttpClient
         HttpClientTransport transport;
         if (config.isHttp2Enabled()) {
             HTTP2Client client = new HTTP2Client();
+            client.setInitialSessionRecvWindow(toIntExact(config.getHttp2InitialSessionReceiveWindowSize().toBytes()));
+            client.setInitialStreamRecvWindow(toIntExact(config.getHttp2InitialStreamReceiveWindowSize().toBytes()));
             client.setSelectors(CLIENT_TRANSPORT_SELECTORS);
             transport = new HttpClientTransportOverHTTP2(client);
         }
@@ -628,27 +633,31 @@ public class JettyHttpClient
     private static List<JettyRequestListener> getRequestListenersForDestination(Destination destination)
     {
         return getRequestForDestination(destination).stream()
-                .map(request -> (JettyRequestListener) request.getAttributes().get(PLATFORM_STATS_KEY))
-                .filter(listener -> listener != null)
+                .map(request -> request.getAttributes().get(PLATFORM_STATS_KEY))
+                .map(JettyRequestListener.class::cast)
+                .filter(Objects::nonNull)
                 .collect(Collectors.toList());
     }
 
     private static List<org.eclipse.jetty.client.api.Request> getRequestForDestination(Destination destination)
     {
-        PoolingHttpDestination<?> poolingHttpDestination = (PoolingHttpDestination<?>) destination;
+        PoolingHttpDestination poolingHttpDestination = (PoolingHttpDestination) destination;
         Queue<HttpExchange> httpExchanges = poolingHttpDestination.getHttpExchanges();
 
         List<org.eclipse.jetty.client.api.Request> requests = httpExchanges.stream()
                 .map(HttpExchange::getRequest)
                 .collect(Collectors.toList());
 
-        poolingHttpDestination.getConnectionPool().getActiveConnections().stream()
+        ((DuplexConnectionPool) poolingHttpDestination.getConnectionPool()).getActiveConnections().stream()
                 .filter(HttpConnectionOverHTTP.class::isInstance)
-                .map(connection -> ((HttpConnectionOverHTTP) connection).getHttpChannel().getHttpExchange())
-                .filter(exchange -> exchange != null)
+                .map(HttpConnectionOverHTTP.class::cast)
+                .map(connection -> connection.getHttpChannel().getHttpExchange())
+                .filter(Objects::nonNull)
                 .forEach(exchange -> requests.add(exchange.getRequest()));
 
-        return requests.stream().filter(request -> request != null).collect(Collectors.toList());
+        return requests.stream()
+                .filter(Objects::nonNull)
+                .collect(toImmutableList());
     }
 
     private static String dumpRequest(long now, JettyRequestListener listener)
@@ -1526,9 +1535,10 @@ public class JettyHttpClient
                 Distribution distribution = new Distribution();
                 httpClient.getDestinations().stream()
                         .filter(PoolingHttpDestination.class::isInstance)
-                        .map(destination -> (PoolingHttpDestination<?>) destination)
+                        .map(PoolingHttpDestination.class::cast)
                         .map(PoolingHttpDestination::getConnectionPool)
-                        .filter(pool -> pool != null)
+                        .filter(Objects::nonNull)
+                        .map(DuplexConnectionPool.class::cast)
                         .forEach(pool -> processor.process(distribution, pool));
                 return distribution;
             });
@@ -1540,7 +1550,7 @@ public class JettyHttpClient
     {
         interface Processor
         {
-            void process(Distribution distribution, PoolingHttpDestination<?> destination);
+            void process(Distribution distribution, PoolingHttpDestination destination);
         }
 
         DestinationDistribution(HttpClient httpClient, Processor processor)
@@ -1549,7 +1559,7 @@ public class JettyHttpClient
                 Distribution distribution = new Distribution();
                 httpClient.getDestinations().stream()
                         .filter(PoolingHttpDestination.class::isInstance)
-                        .map(destination -> (PoolingHttpDestination<?>) destination)
+                        .map(PoolingHttpDestination.class::cast)
                         .forEach(destination -> processor.process(distribution, destination));
                 return distribution;
             });
@@ -1571,7 +1581,7 @@ public class JettyHttpClient
                 Distribution distribution = new Distribution();
                 httpClient.getDestinations().stream()
                         .filter(PoolingHttpDestination.class::isInstance)
-                        .map(destination -> (PoolingHttpDestination<?>) destination)
+                        .map(PoolingHttpDestination.class::cast)
                         .map(JettyHttpClient::getRequestListenersForDestination)
                         .flatMap(List::stream)
                         .forEach(listener -> processor.process(distribution, listener, now));

http-server/src/main/java/com/proofpoint/http/server/HttpServerConfig.java
@@ -21,8 +21,11 @@ import com.proofpoint.configuration.ConfigSecuritySensitive;
 import com.proofpoint.configuration.DefunctConfig;
 import com.proofpoint.units.DataSize;
 import com.proofpoint.units.Duration;
+import com.proofpoint.units.MaxDataSize;
+import com.proofpoint.units.MinDataSize;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 import static com.proofpoint.units.DataSize.Unit.GIGABYTE;
 import static com.proofpoint.units.DataSize.Unit.MEGABYTE;
@@ -83,6 +86,8 @@ public class HttpServerConfig
     private Duration stopTimeout = new Duration(30, SECONDS);
     private DataSize maxRequestHeaderSize;
     private int http2MaxConcurrentStreams = 16384;
+    private DataSize http2InitialSessionReceiveWindowSize = new DataSize(16, DataSize.Unit.MEGABYTE);
+    private DataSize http2InitialStreamReceiveWindowSize = new DataSize(16, DataSize.Unit.MEGABYTE);
 
     private String userAuthFile;
 
@@ -462,6 +467,38 @@ public class HttpServerConfig
     public HttpServerConfig setShowStackTrace(boolean showStackTrace)
     {
         this.showStackTrace = showStackTrace;
+        return this;
+    }
+
+    @NotNull
+    @MinDataSize("1kB")
+    @MaxDataSize("1GB")
+    public DataSize getHttp2InitialSessionReceiveWindowSize()
+    {
+        return http2InitialSessionReceiveWindowSize;
+    }
+
+    @Config("http-server.http2.session-receive-window-size")
+    @ConfigDescription("Initial size of session's flow control receive window for HTTP/2")
+    public HttpServerConfig setHttp2InitialSessionReceiveWindowSize(DataSize http2InitialSessionReceiveWindowSize)
+    {
+        this.http2InitialSessionReceiveWindowSize = http2InitialSessionReceiveWindowSize;
+        return this;
+    }
+
+    @NotNull
+    @MinDataSize("1kB")
+    @MaxDataSize("1GB")
+    public DataSize getHttp2InitialStreamReceiveWindowSize()
+    {
+        return http2InitialStreamReceiveWindowSize;
+    }
+
+    @Config("http-server.http2.stream-receive-window-size")
+    @ConfigDescription("Initial size of stream's flow control receive window for HTTP/2")
+    public HttpServerConfig setHttp2InitialStreamReceiveWindowSize(DataSize http2InitialStreamReceiveWindowSize)
+    {
+        this.http2InitialStreamReceiveWindowSize = http2InitialStreamReceiveWindowSize;
         return this;
     }
 