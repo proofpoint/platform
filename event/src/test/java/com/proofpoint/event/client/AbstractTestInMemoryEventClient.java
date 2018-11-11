airlift/airlift

Static import assertion methods

event/src/test/java/io/airlift/event/client/AbstractTestInMemoryEventClient.java
@@ -18,10 +18,11 @@ package io.airlift.event.client;
 import com.google.common.collect.ImmutableList;
 import io.airlift.event.client.EventClient.EventGenerator;
 import io.airlift.event.client.EventClient.EventPoster;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+
+import static org.testng.Assert.assertEquals;
 
 @Test(singleThreaded = true)
 public abstract class AbstractTestInMemoryEventClient
@@ -38,7 +39,7 @@ public abstract class AbstractTestInMemoryEventClient
     {
         eventClient.post(event1);
 
-        Assert.assertEquals(inMemoryEventClient.getEvents(), ImmutableList.of(event1));
+        assertEquals(inMemoryEventClient.getEvents(), ImmutableList.of(event1));
     }
 
     @Test
@@ -48,7 +49,7 @@ public abstract class AbstractTestInMemoryEventClient
         eventClient.post(event2);
         eventClient.post(event3);
 
-        Assert.assertEquals(inMemoryEventClient.getEvents(),
+        assertEquals(inMemoryEventClient.getEvents(),
                 ImmutableList.of(event1, event2, event3));
     }
 
@@ -57,7 +58,7 @@ public abstract class AbstractTestInMemoryEventClient
     {
         eventClient.post(event1, event2, event3);
 
-        Assert.assertEquals(inMemoryEventClient.getEvents(),
+        assertEquals(inMemoryEventClient.getEvents(),
                 ImmutableList.of(event1, event2, event3));
     }
 
@@ -66,7 +67,7 @@ public abstract class AbstractTestInMemoryEventClient
     {
         eventClient.post(ImmutableList.of(event1, event2, event3));
 
-        Assert.assertEquals(inMemoryEventClient.getEvents(),
+        assertEquals(inMemoryEventClient.getEvents(),
                 ImmutableList.of(event1, event2, event3));
     }
 
@@ -85,7 +86,7 @@ public abstract class AbstractTestInMemoryEventClient
             }
         });
 
-        Assert.assertEquals(inMemoryEventClient.getEvents(),
+        assertEquals(inMemoryEventClient.getEvents(),
                 ImmutableList.of(event1, event2, event3));
     }
 }

jaxrs/src/test/java/io/airlift/jaxrs/TestJsonMapper.java
@@ -20,7 +20,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.net.HttpHeaders;
 import io.airlift.jaxrs.testing.GuavaMultivaluedMap;
 import io.airlift.json.JsonCodec;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import javax.ws.rs.WebApplicationException;
@@ -33,6 +32,9 @@ import java.io.InputStream;
 import java.util.zip.ZipException;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+import static org.testng.Assert.fail;
 
 public class TestJsonMapper
 {
@@ -58,13 +60,13 @@ public class TestJsonMapper
         jsonMapper.writeTo(value, String.class, null, null, null, headers, outputStream);
 
         String json = new String(outputStream.toByteArray(), UTF_8);
-        Assert.assertTrue(!json.contains("<"));
-        Assert.assertTrue(!json.contains(">"));
-        Assert.assertTrue(!json.contains("'"));
-        Assert.assertTrue(!json.contains("&"));
-        Assert.assertEquals(jsonCodec.fromJson(json), value);
+        assertTrue(!json.contains("<"));
+        assertTrue(!json.contains(">"));
+        assertTrue(!json.contains("'"));
+        assertTrue(!json.contains("&"));
+        assertEquals(jsonCodec.fromJson(json), value);
 
-        Assert.assertEquals(headers.getFirst(HttpHeaders.X_CONTENT_TYPE_OPTIONS), "nosniff");
+        assertEquals(headers.getFirst(HttpHeaders.X_CONTENT_TYPE_OPTIONS), "nosniff");
     }
 
     @Test
@@ -96,10 +98,10 @@ public class TestJsonMapper
                     throw new EOFException("forced EOF Exception");
                 }
             });
-            Assert.fail("Should have thrown a JsonMapperParsingException");
+            fail("Should have thrown a JsonMapperParsingException");
         }
         catch (JsonMapperParsingException e) {
-            Assert.assertTrue((e.getMessage()).startsWith("Invalid json for Java type"));
+            assertTrue((e.getMessage()).startsWith("Invalid json for Java type"));
         }
     }
 
@@ -132,10 +134,10 @@ public class TestJsonMapper
                     throw new TestingJsonProcessingException("forced JsonProcessingException");
                 }
             });
-            Assert.fail("Should have thrown a JsonMapperParsingException");
+            fail("Should have thrown a JsonMapperParsingException");
         }
         catch (JsonMapperParsingException e) {
-            Assert.assertTrue((e.getMessage()).startsWith("Invalid json for Java type"));
+            assertTrue((e.getMessage()).startsWith("Invalid json for Java type"));
         }
     }
 
@@ -168,10 +170,10 @@ public class TestJsonMapper
                     throw new ZipException("forced ZipException");
                 }
             });
-            Assert.fail("Should have thrown an IOException");
+            fail("Should have thrown an IOException");
         }
         catch (WebApplicationException e) {
-            Assert.fail("Should not have received a WebApplicationException", e);
+            fail("Should not have received a WebApplicationException", e);
         }
     }
 

proofpoint/platform

event/src/test/java/com/proofpoint/event/client/AbstractTestInMemoryEventClient.java
@@ -16,10 +16,10 @@
 package com.proofpoint.event.client;
 
 import com.google.common.collect.ImmutableList;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static com.proofpoint.event.client.DummyEventClass.dummyEventClass;
+import static org.testng.Assert.assertEquals;
 
 public abstract class AbstractTestInMemoryEventClient
 {
@@ -33,7 +33,7 @@ public abstract class AbstractTestInMemoryEventClient
     {
         eventClient.post(event1);
 
-        Assert.assertEquals(eventClient.getEvents(), ImmutableList.of(event1));
+        assertEquals(eventClient.getEvents(), ImmutableList.of(event1));
     }
 
     @Test
@@ -43,7 +43,7 @@ public abstract class AbstractTestInMemoryEventClient
         eventClient.post(event2);
         eventClient.post(event3);
 
-        Assert.assertEquals(eventClient.getEvents(),
+        assertEquals(eventClient.getEvents(),
                 ImmutableList.of(event1, event2, event3));
     }
 
@@ -52,7 +52,7 @@ public abstract class AbstractTestInMemoryEventClient
     {
         eventClient.post(event1, event2, event3);
 
-        Assert.assertEquals(eventClient.getEvents(),
+        assertEquals(eventClient.getEvents(),
                 ImmutableList.of(event1, event2, event3));
     }
 
@@ -61,7 +61,7 @@ public abstract class AbstractTestInMemoryEventClient
     {
         eventClient.post(ImmutableList.of(event1, event2, event3));
 
-        Assert.assertEquals(eventClient.getEvents(),
+        assertEquals(eventClient.getEvents(),
                 ImmutableList.of(event1, event2, event3));
     }
 }

jaxrs/src/test/java/com/proofpoint/jaxrs/TestJsonMapper.java
@@ -18,12 +18,13 @@ package com.proofpoint.jaxrs;
 import com.google.common.net.HttpHeaders;
 import com.proofpoint.json.JsonCodec;
 import com.proofpoint.json.ObjectMapperProvider;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 
 import javax.ws.rs.core.MultivaluedMap;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class TestJsonMapper
     extends AbstractMapperTest<JsonMapper>
@@ -39,12 +40,12 @@ public class TestJsonMapper
     {
         JsonCodec<String> jsonCodec = JsonCodec.jsonCodec(String.class);
         String json = new String(encoded, UTF_8);
-        Assert.assertTrue(!json.contains("<"));
-        Assert.assertTrue(!json.contains(">"));
-        Assert.assertTrue(!json.contains("'"));
-        Assert.assertTrue(!json.contains("&"));
-        Assert.assertEquals(jsonCodec.fromJson(json), expected);
+        assertTrue(!json.contains("<"));
+        assertTrue(!json.contains(">"));
+        assertTrue(!json.contains("'"));
+        assertTrue(!json.contains("&"));
+        assertEquals(jsonCodec.fromJson(json), expected);
 
-        Assert.assertEquals(headers.getFirst(HttpHeaders.X_CONTENT_TYPE_OPTIONS), "nosniff");
+        assertEquals(headers.getFirst(HttpHeaders.X_CONTENT_TYPE_OPTIONS), "nosniff");
     }
 }