airlift/airlift

Make the inner classes of JettyHttpClient top-level

http-client/src/main/java/io/airlift/http/client/jetty/CachedDistribution.java
@@ -1,0 +1,132 @@
+package io.airlift.http.client.jetty;
+
+import io.airlift.stats.Distribution;
+import org.weakref.jmx.Managed;
+
+import javax.annotation.concurrent.GuardedBy;
+import javax.annotation.concurrent.ThreadSafe;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
+/*
+ * This class is needed because jmxutils only fetches a nested instance object once and holds on to it forever.
+ * todo remove this when https://github.com/martint/jmxutils/issues/26 is implemented
+ */
+@ThreadSafe
+class CachedDistribution
+{
+    private final Supplier<Distribution> distributionSupplier;
+
+    @GuardedBy("this")
+    private Distribution distribution;
+    @GuardedBy("this")
+    private long lastUpdate = System.nanoTime();
+
+    public CachedDistribution(Supplier<Distribution> distributionSupplier)
+    {
+        this.distributionSupplier = distributionSupplier;
+    }
+
+    public synchronized Distribution getDistribution()
+    {
+        // refresh stats only once a second
+        if (NANOSECONDS.toMillis(System.nanoTime() - lastUpdate) > 1000) {
+            this.distribution = distributionSupplier.get();
+            this.lastUpdate = System.nanoTime();
+        }
+        return distribution;
+    }
+
+    @Managed
+    public double getMaxError()
+    {
+        return getDistribution().getMaxError();
+    }
+
+    @Managed
+    public double getCount()
+    {
+        return getDistribution().getCount();
+    }
+
+    @Managed
+    public double getTotal()
+    {
+        return getDistribution().getTotal();
+    }
+
+    @Managed
+    public long getP01()
+    {
+        return getDistribution().getP01();
+    }
+
+    @Managed
+    public long getP05()
+    {
+        return getDistribution().getP05();
+    }
+
+    @Managed
+    public long getP10()
+    {
+        return getDistribution().getP10();
+    }
+
+    @Managed
+    public long getP25()
+    {
+        return getDistribution().getP25();
+    }
+
+    @Managed
+    public long getP50()
+    {
+        return getDistribution().getP50();
+    }
+
+    @Managed
+    public long getP75()
+    {
+        return getDistribution().getP75();
+    }
+
+    @Managed
+    public long getP90()
+    {
+        return getDistribution().getP90();
+    }
+
+    @Managed
+    public long getP95()
+    {
+        return getDistribution().getP95();
+    }
+
+    @Managed
+    public long getP99()
+    {
+        return getDistribution().getP99();
+    }
+
+    @Managed
+    public long getMin()
+    {
+        return getDistribution().getMin();
+    }
+
+    @Managed
+    public long getMax()
+    {
+        return getDistribution().getMax();
+    }
+
+    @Managed
+    public Map<Double, Long> getPercentiles()
+    {
+        return getDistribution().getPercentiles();
+    }
+}

http-client/src/main/java/io/airlift/http/client/jetty/ConnectionPoolDistribution.java
@@ -1,0 +1,32 @@
+package io.airlift.http.client.jetty;
+
+import io.airlift.stats.Distribution;
+import org.eclipse.jetty.client.DuplexConnectionPool;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.PoolingHttpDestination;
+
+import java.util.Objects;
+
+class ConnectionPoolDistribution
+        extends CachedDistribution
+{
+    interface Processor
+    {
+        void process(Distribution distribution, DuplexConnectionPool pool);
+    }
+
+    public ConnectionPoolDistribution(HttpClient httpClient, Processor processor)
+    {
+        super(() -> {
+            Distribution distribution = new Distribution();
+            httpClient.getDestinations().stream()
+                    .filter(PoolingHttpDestination.class::isInstance)
+                    .map(PoolingHttpDestination.class::cast)
+                    .map(PoolingHttpDestination::getConnectionPool)
+                    .filter(Objects::nonNull)
+                    .map(DuplexConnectionPool.class::cast)
+                    .forEach(pool -> processor.process(distribution, pool));
+            return distribution;
+        });
+    }
+}

http-client/src/main/java/io/airlift/http/client/jetty/DestinationDistribution.java
@@ -1,0 +1,26 @@
+package io.airlift.http.client.jetty;
+
+import io.airlift.stats.Distribution;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.PoolingHttpDestination;
+
+class DestinationDistribution
+        extends CachedDistribution
+{
+    interface Processor
+    {
+        void process(Distribution distribution, PoolingHttpDestination destination);
+    }
+
+    public DestinationDistribution(HttpClient httpClient, Processor processor)
+    {
+        super(() -> {
+            Distribution distribution = new Distribution();
+            httpClient.getDestinations().stream()
+                    .filter(PoolingHttpDestination.class::isInstance)
+                    .map(PoolingHttpDestination.class::cast)
+                    .forEach(destination -> processor.process(distribution, destination));
+            return distribution;
+        });
+    }
+}

http-client/src/main/java/io/airlift/http/client/jetty/JettyRequestListener.java
@@ -1,0 +1,107 @@
+package io.airlift.http.client.jetty;
+
+import java.net.URI;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
+class JettyRequestListener
+{
+    enum State
+    {
+        CREATED, SENDING_REQUEST, AWAITING_RESPONSE, READING_RESPONSE, FINISHED
+    }
+
+    private final AtomicReference<State> state = new AtomicReference<>(State.CREATED);
+
+    private final URI uri;
+    private final long created = System.nanoTime();
+    private final AtomicLong requestStarted = new AtomicLong();
+    private final AtomicLong requestFinished = new AtomicLong();
+    private final AtomicLong responseStarted = new AtomicLong();
+    private final AtomicLong responseFinished = new AtomicLong();
+
+    public JettyRequestListener(URI uri)
+    {
+        this.uri = uri;
+    }
+
+    public URI getUri()
+    {
+        return uri;
+    }
+
+    public State getState()
+    {
+        return state.get();
+    }
+
+    public long getCreated()
+    {
+        return created;
+    }
+
+    public long getRequestStarted()
+    {
+        return requestStarted.get();
+    }
+
+    public long getRequestFinished()
+    {
+        return requestFinished.get();
+    }
+
+    public long getResponseStarted()
+    {
+        return responseStarted.get();
+    }
+
+    public long getResponseFinished()
+    {
+        return responseFinished.get();
+    }
+
+    public void onRequestBegin()
+    {
+        changeState(State.SENDING_REQUEST);
+
+        long now = System.nanoTime();
+        requestStarted.compareAndSet(0, now);
+    }
+
+    public void onRequestEnd()
+    {
+        changeState(State.AWAITING_RESPONSE);
+
+        long now = System.nanoTime();
+        requestStarted.compareAndSet(0, now);
+        requestFinished.compareAndSet(0, now);
+    }
+
+    public void onResponseBegin()
+    {
+        changeState(State.READING_RESPONSE);
+
+        long now = System.nanoTime();
+        requestStarted.compareAndSet(0, now);
+        requestFinished.compareAndSet(0, now);
+        responseStarted.compareAndSet(0, now);
+    }
+
+    public void onFinish()
+    {
+        changeState(State.FINISHED);
+
+        long now = System.nanoTime();
+        requestStarted.compareAndSet(0, now);
+        requestFinished.compareAndSet(0, now);
+        responseStarted.compareAndSet(0, now);
+        responseFinished.compareAndSet(0, now);
+    }
+
+    private synchronized void changeState(State newState)
+    {
+        if (state.get().ordinal() < newState.ordinal()) {
+            state.set(newState);
+        }
+    }
+}

http-client/src/main/java/io/airlift/http/client/jetty/JettyResponse.java
@@ -1,0 +1,78 @@
+package io.airlift.http.client.jetty;
+
+import com.google.common.collect.ImmutableListMultimap;
+import com.google.common.collect.ListMultimap;
+import com.google.common.io.CountingInputStream;
+import io.airlift.http.client.HeaderName;
+import org.eclipse.jetty.client.api.Response;
+import org.eclipse.jetty.http.HttpFields;
+
+import java.io.InputStream;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+
+class JettyResponse
+        implements io.airlift.http.client.Response
+{
+    private final Response response;
+    private final CountingInputStream inputStream;
+    private final ListMultimap<HeaderName, String> headers;
+
+    public JettyResponse(Response response, InputStream inputStream)
+    {
+        this.response = response;
+        this.inputStream = new CountingInputStream(inputStream);
+        this.headers = toHeadersMap(response.getHeaders());
+    }
+
+    @Override
+    public int getStatusCode()
+    {
+        return response.getStatus();
+    }
+
+    @Override
+    public String getStatusMessage()
+    {
+        return response.getReason();
+    }
+
+    @Override
+    public ListMultimap<HeaderName, String> getHeaders()
+    {
+        return headers;
+    }
+
+    @Override
+    public long getBytesRead()
+    {
+        return inputStream.getCount();
+    }
+
+    @Override
+    public InputStream getInputStream()
+    {
+        return inputStream;
+    }
+
+    @Override
+    public String toString()
+    {
+        return toStringHelper(this)
+                .add("statusCode", getStatusCode())
+                .add("statusMessage", getStatusMessage())
+                .add("headers", getHeaders())
+                .toString();
+    }
+
+    private static ListMultimap<HeaderName, String> toHeadersMap(HttpFields headers)
+    {
+        ImmutableListMultimap.Builder<HeaderName, String> builder = ImmutableListMultimap.builder();
+        for (String name : headers.getFieldNamesCollection()) {
+            for (String value : headers.getValuesList(name)) {
+                builder.put(HeaderName.of(name), value);
+            }
+        }
+        return builder.build();
+    }
+}

http-client/src/main/java/io/airlift/http/client/jetty/RequestDistribution.java
@@ -1,0 +1,31 @@
+package io.airlift.http.client.jetty;
+
+import io.airlift.stats.Distribution;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.PoolingHttpDestination;
+
+import java.util.List;
+
+class RequestDistribution
+        extends CachedDistribution
+{
+    interface Processor
+    {
+        void process(Distribution distribution, JettyRequestListener listener, long now);
+    }
+
+    public RequestDistribution(HttpClient httpClient, Processor processor)
+    {
+        super(() -> {
+            long now = System.nanoTime();
+            Distribution distribution = new Distribution();
+            httpClient.getDestinations().stream()
+                    .filter(PoolingHttpDestination.class::isInstance)
+                    .map(PoolingHttpDestination.class::cast)
+                    .map(JettyHttpClient::getRequestListenersForDestination)
+                    .flatMap(List::stream)
+                    .forEach(listener -> processor.process(distribution, listener, now));
+            return distribution;
+        });
+    }
+}

proofpoint/platform

http-client/src/main/java/com/proofpoint/http/client/jetty/CachedDistribution.java
@@ -1,0 +1,131 @@
+package com.proofpoint.http.client.jetty;
+
+import com.proofpoint.stats.Distribution;
+import org.weakref.jmx.Managed;
+
+import javax.annotation.concurrent.GuardedBy;
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
+
+/*
+ * This class is needed because jmxutils only fetches a nested instance object once and holds on to it forever.
+ * todo remove this when https://github.com/martint/jmxutils/issues/26 is implemented
+ */
+@ThreadSafe
+public class CachedDistribution
+{
+    private final Supplier<Distribution> distributionSupplier;
+
+    @GuardedBy("this")
+    private Distribution distribution;
+    @GuardedBy("this")
+    private long lastUpdate = System.nanoTime();
+
+    public CachedDistribution(Supplier<Distribution> distributionSupplier)
+    {
+        this.distributionSupplier = distributionSupplier;
+    }
+
+    public synchronized Distribution getDistribution()
+    {
+        // refresh stats only once a second
+        if (NANOSECONDS.toMillis(System.nanoTime() - lastUpdate) > 1000) {
+            this.distribution = distributionSupplier.get();
+            this.lastUpdate = System.nanoTime();
+        }
+        return distribution;
+    }
+
+    @Managed
+    public double getMaxError()
+    {
+        return getDistribution().getMaxError();
+    }
+
+    @Managed
+    public double getCount()
+    {
+        return getDistribution().getCount();
+    }
+
+    @Managed
+    public double getTotal()
+    {
+        return getDistribution().getTotal();
+    }
+
+    @Managed
+    public long getP01()
+    {
+        return getDistribution().getP01();
+    }
+
+    @Managed
+    public long getP05()
+    {
+        return getDistribution().getP05();
+    }
+
+    @Managed
+    public long getP10()
+    {
+        return getDistribution().getP10();
+    }
+
+    @Managed
+    public long getP25()
+    {
+        return getDistribution().getP25();
+    }
+
+    @Managed
+    public long getP50()
+    {
+        return getDistribution().getP50();
+    }
+
+    @Managed
+    public long getP75()
+    {
+        return getDistribution().getP75();
+    }
+
+    @Managed
+    public long getP90()
+    {
+        return getDistribution().getP90();
+    }
+
+    @Managed
+    public long getP95()
+    {
+        return getDistribution().getP95();
+    }
+
+    @Managed
+    public long getP99()
+    {
+        return getDistribution().getP99();
+    }
+
+    @Managed
+    public long getMin()
+    {
+        return getDistribution().getMin();
+    }
+
+    @Managed
+    public long getMax()
+    {
+        return getDistribution().getMax();
+    }
+
+    @Managed
+    public Map<Double, Long> getPercentiles()
+    {
+        return getDistribution().getPercentiles();
+    }
+}

http-client/src/main/java/com/proofpoint/http/client/jetty/ConnectionPoolDistribution.java
@@ -1,0 +1,32 @@
+package com.proofpoint.http.client.jetty;
+
+import com.proofpoint.stats.Distribution;
+import org.eclipse.jetty.client.DuplexConnectionPool;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.PoolingHttpDestination;
+
+import java.util.Objects;
+
+class ConnectionPoolDistribution
+        extends CachedDistribution
+{
+    interface Processor
+    {
+        void process(Distribution distribution, DuplexConnectionPool pool);
+    }
+
+    ConnectionPoolDistribution(HttpClient httpClient, Processor processor)
+    {
+        super(() -> {
+            Distribution distribution = new Distribution();
+            httpClient.getDestinations().stream()
+                    .filter(PoolingHttpDestination.class::isInstance)
+                    .map(PoolingHttpDestination.class::cast)
+                    .map(PoolingHttpDestination::getConnectionPool)
+                    .filter(Objects::nonNull)
+                    .map(DuplexConnectionPool.class::cast)
+                    .forEach(pool -> processor.process(distribution, pool));
+            return distribution;
+        });
+    }
+}

http-client/src/main/java/com/proofpoint/http/client/jetty/DestinationDistribution.java
@@ -1,0 +1,26 @@
+package com.proofpoint.http.client.jetty;
+
+import com.proofpoint.stats.Distribution;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.PoolingHttpDestination;
+
+class DestinationDistribution
+        extends CachedDistribution
+{
+    interface Processor
+    {
+        void process(Distribution distribution, PoolingHttpDestination destination);
+    }
+
+    DestinationDistribution(HttpClient httpClient, Processor processor)
+    {
+        super(() -> {
+            Distribution distribution = new Distribution();
+            httpClient.getDestinations().stream()
+                    .filter(PoolingHttpDestination.class::isInstance)
+                    .map(PoolingHttpDestination.class::cast)
+                    .forEach(destination -> processor.process(distribution, destination));
+            return distribution;
+        });
+    }
+}

http-client/src/main/java/com/proofpoint/http/client/jetty/JettyRequestListener.java
@@ -1,0 +1,107 @@
+package com.proofpoint.http.client.jetty;
+
+import java.net.URI;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicReference;
+
+class JettyRequestListener
+{
+    enum State
+    {
+        CREATED, SENDING_REQUEST, AWAITING_RESPONSE, READING_RESPONSE, FINISHED
+    }
+
+    private final AtomicReference<State> state = new AtomicReference<>(State.CREATED);
+
+    private final URI uri;
+    private final long created = System.nanoTime();
+    private final AtomicLong requestStarted = new AtomicLong();
+    private final AtomicLong requestFinished = new AtomicLong();
+    private final AtomicLong responseStarted = new AtomicLong();
+    private final AtomicLong responseFinished = new AtomicLong();
+
+    JettyRequestListener(URI uri)
+    {
+        this.uri = uri;
+    }
+
+    public URI getUri()
+    {
+        return uri;
+    }
+
+    public State getState()
+    {
+        return state.get();
+    }
+
+    public long getCreated()
+    {
+        return created;
+    }
+
+    public long getRequestStarted()
+    {
+        return requestStarted.get();
+    }
+
+    public long getRequestFinished()
+    {
+        return requestFinished.get();
+    }
+
+    public long getResponseStarted()
+    {
+        return responseStarted.get();
+    }
+
+    public long getResponseFinished()
+    {
+        return responseFinished.get();
+    }
+
+    public void onRequestBegin()
+    {
+        changeState(State.SENDING_REQUEST);
+
+        long now = System.nanoTime();
+        requestStarted.compareAndSet(0, now);
+    }
+
+    public void onRequestEnd()
+    {
+        changeState(State.AWAITING_RESPONSE);
+
+        long now = System.nanoTime();
+        requestStarted.compareAndSet(0, now);
+        requestFinished.compareAndSet(0, now);
+    }
+
+    void onResponseBegin()
+    {
+        changeState(State.READING_RESPONSE);
+
+        long now = System.nanoTime();
+        requestStarted.compareAndSet(0, now);
+        requestFinished.compareAndSet(0, now);
+        responseStarted.compareAndSet(0, now);
+    }
+
+    void onFinish()
+    {
+        changeState(State.FINISHED);
+
+        long now = System.nanoTime();
+        requestStarted.compareAndSet(0, now);
+        requestFinished.compareAndSet(0, now);
+        responseStarted.compareAndSet(0, now);
+        responseFinished.compareAndSet(0, now);
+    }
+
+    private synchronized void changeState(State newState)
+    {
+        if (state.get().ordinal() < newState.ordinal()) {
+            state.set(newState);
+        }
+    }
+}

http-client/src/main/java/com/proofpoint/http/client/jetty/JettyResponse.java
@@ -1,0 +1,78 @@
+package com.proofpoint.http.client.jetty;
+
+import com.google.common.collect.ImmutableListMultimap;
+import com.google.common.collect.ListMultimap;
+import com.google.common.io.CountingInputStream;
+import com.proofpoint.http.client.HeaderName;
+import org.eclipse.jetty.client.api.Response;
+import org.eclipse.jetty.http.HttpFields;
+
+import java.io.InputStream;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+
+class JettyResponse
+        implements com.proofpoint.http.client.Response
+{
+    private final Response response;
+    private final CountingInputStream inputStream;
+    private final ListMultimap<HeaderName, String> headers;
+
+    JettyResponse(Response response, InputStream inputStream)
+    {
+        this.response = response;
+        this.inputStream = new CountingInputStream(inputStream);
+        this.headers = toHeadersMap(response.getHeaders());
+    }
+
+    @Override
+    public int getStatusCode()
+    {
+        return response.getStatus();
+    }
+
+    @Override
+    public String getStatusMessage()
+    {
+        return response.getReason();
+    }
+
+    @Override
+    public ListMultimap<HeaderName, String> getHeaders()
+    {
+        return headers;
+    }
+
+    @Override
+    public long getBytesRead()
+    {
+        return inputStream.getCount();
+    }
+
+    @Override
+    public InputStream getInputStream()
+    {
+        return inputStream;
+    }
+
+    @Override
+    public String toString()
+    {
+        return toStringHelper(this)
+                .add("statusCode", getStatusCode())
+                .add("statusMessage", getStatusMessage())
+                .add("headers", getHeaders())
+                .toString();
+    }
+
+    private static ListMultimap<HeaderName, String> toHeadersMap(HttpFields headers)
+    {
+        ImmutableListMultimap.Builder<HeaderName, String> builder = ImmutableListMultimap.builder();
+        for (String name : headers.getFieldNamesCollection()) {
+            for (String value : headers.getValuesList(name)) {
+                builder.put(HeaderName.of(name), value);
+            }
+        }
+        return builder.build();
+    }
+}

http-client/src/main/java/com/proofpoint/http/client/jetty/RequestDistribution.java
@@ -1,0 +1,31 @@
+package com.proofpoint.http.client.jetty;
+
+import com.proofpoint.stats.Distribution;
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.PoolingHttpDestination;
+
+import java.util.List;
+
+class RequestDistribution
+        extends CachedDistribution
+{
+    interface Processor
+    {
+        void process(Distribution distribution, JettyRequestListener listener, long now);
+    }
+
+    RequestDistribution(HttpClient httpClient, Processor processor)
+    {
+        super(() -> {
+            long now = System.nanoTime();
+            Distribution distribution = new Distribution();
+            httpClient.getDestinations().stream()
+                    .filter(PoolingHttpDestination.class::isInstance)
+                    .map(PoolingHttpDestination.class::cast)
+                    .map(JettyHttpClient::getRequestListenersForDestination)
+                    .flatMap(List::stream)
+                    .forEach(listener -> processor.process(distribution, listener, now));
+            return distribution;
+        });
+    }
+}