airlift/airlift

Allow preserving Authorization when following redirects

http-client/src/main/java/io/airlift/http/client/jetty/AuthorizationPreservingHttpClient.java
@@ -1,0 +1,50 @@
+package io.airlift.http.client.jetty;
+
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.HttpClientTransport;
+import org.eclipse.jetty.client.HttpRequest;
+import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.http.HttpField;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+
+import java.net.URI;
+
+import static org.eclipse.jetty.http.HttpHeader.AUTHORIZATION;
+
+class AuthorizationPreservingHttpClient
+        extends HttpClient
+{
+    private static final String PRESERVE_AUTHORIZATION_KEY = "airlift_preserve_authorization";
+
+    public AuthorizationPreservingHttpClient(HttpClientTransport transport, SslContextFactory sslContextFactory)
+    {
+        super(transport, sslContextFactory);
+    }
+
+    @Override
+    protected Request copyRequest(HttpRequest oldRequest, URI newUri)
+    {
+        Request newRequest = super.copyRequest(oldRequest, newUri);
+
+        if (isPreserveAuthorization(oldRequest)) {
+            setPreserveAuthorization(newRequest, true);
+            for (HttpField field : oldRequest.getHeaders()) {
+                if (field.getHeader() == AUTHORIZATION) {
+                    newRequest.header(field.getName(), field.getValue());
+                }
+            }
+        }
+
+        return newRequest;
+    }
+
+    public static void setPreserveAuthorization(Request request, boolean preserveAuthorization)
+    {
+        request.attribute(PRESERVE_AUTHORIZATION_KEY, preserveAuthorization);
+    }
+
+    private static boolean isPreserveAuthorization(Request request)
+    {
+        return (boolean) request.getAttributes().get(PRESERVE_AUTHORIZATION_KEY);
+    }
+}

http-client/src/main/java/io/airlift/http/client/jetty/JettyHttpClient.java
@@ -76,6 +76,7 @@ import static com.google.common.base.MoreObjects.toStringHelper;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Throwables.throwIfUnchecked;
 import static com.google.common.collect.ImmutableList.toImmutableList;
+import static io.airlift.http.client.jetty.AuthorizationPreservingHttpClient.setPreserveAuthorization;
 import static java.lang.Math.max;
 import static java.lang.Math.toIntExact;
 import static java.lang.String.format;
@@ -193,7 +194,7 @@ public class JettyHttpClient
             transport = new HttpClientTransportOverHTTP(config.getSelectorCount());
         }
 
-        httpClient = new HttpClient(transport, sslContextFactory);
+        httpClient = new AuthorizationPreservingHttpClient(transport, sslContextFactory);
 
         // Kerberos authentication
         if (config.getAuthenticationEnabled()) {
@@ -617,6 +618,8 @@ public class JettyHttpClient
         }
 
         jettyRequest.followRedirects(finalRequest.isFollowRedirects());
+
+        setPreserveAuthorization(jettyRequest, finalRequest.isPreserveAuthorizationOnRedirect());
 
         // timeouts
         jettyRequest.timeout(requestTimeoutMillis, MILLISECONDS);

proofpoint/platform

http-client/src/main/java/com/proofpoint/http/client/jetty/AuthorizationPreservingHttpClient.java
@@ -1,0 +1,50 @@
+package com.proofpoint.http.client.jetty;
+
+import org.eclipse.jetty.client.HttpClient;
+import org.eclipse.jetty.client.HttpClientTransport;
+import org.eclipse.jetty.client.HttpRequest;
+import org.eclipse.jetty.client.api.Request;
+import org.eclipse.jetty.http.HttpField;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
+
+import java.net.URI;
+
+import static org.eclipse.jetty.http.HttpHeader.AUTHORIZATION;
+
+class AuthorizationPreservingHttpClient
+        extends HttpClient
+{
+    private static final String PRESERVE_AUTHORIZATION_KEY = "airlift_preserve_authorization";
+
+    public AuthorizationPreservingHttpClient(HttpClientTransport transport, SslContextFactory sslContextFactory)
+    {
+        super(transport, sslContextFactory);
+    }
+
+    @Override
+    protected Request copyRequest(HttpRequest oldRequest, URI newUri)
+    {
+        Request newRequest = super.copyRequest(oldRequest, newUri);
+
+        if (isPreserveAuthorization(oldRequest)) {
+            setPreserveAuthorization(newRequest, true);
+            for (HttpField field : oldRequest.getHeaders()) {
+                if (field.getHeader() == AUTHORIZATION) {
+                    newRequest.header(field.getName(), field.getValue());
+                }
+            }
+        }
+
+        return newRequest;
+    }
+
+    public static void setPreserveAuthorization(Request request, boolean preserveAuthorization)
+    {
+        request.attribute(PRESERVE_AUTHORIZATION_KEY, preserveAuthorization);
+    }
+
+    private static boolean isPreserveAuthorization(Request request)
+    {
+        return (boolean) request.getAttributes().get(PRESERVE_AUTHORIZATION_KEY);
+    }
+}

http-client/src/main/java/com/proofpoint/http/client/jetty/JettyHttpClient.java
@@ -29,7 +29,6 @@ import org.eclipse.jetty.client.http.HttpClientTransportOverHTTP;
 import org.eclipse.jetty.client.http.HttpConnectionOverHTTP;
 import org.eclipse.jetty.client.util.BytesContentProvider;
 import org.eclipse.jetty.client.util.InputStreamResponseListener;
-import org.eclipse.jetty.http.HttpHeader;
 import org.eclipse.jetty.http2.client.HTTP2Client;
 import org.eclipse.jetty.http2.client.http.HttpClientTransportOverHTTP2;
 import org.eclipse.jetty.io.MappedByteBufferPool;
@@ -62,6 +61,7 @@ import static com.google.common.base.MoreObjects.toStringHelper;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Throwables.throwIfUnchecked;
 import static com.google.common.collect.ImmutableList.toImmutableList;
+import static com.proofpoint.http.client.jetty.AuthorizationPreservingHttpClient.setPreserveAuthorization;
 import static java.lang.Math.max;
 import static java.lang.Math.toIntExact;
 import static java.util.Objects.requireNonNull;
@@ -181,7 +181,7 @@ public class JettyHttpClient
             transport = new HttpClientTransportOverHTTP(config.getSelectorCount());
         }
 
-        httpClient = new HttpClient(transport, sslContextFactory);
+        httpClient = new AuthorizationPreservingHttpClient(transport, sslContextFactory);
 
         httpClient.setMaxRequestsQueuedPerDestination(config.getMaxRequestsQueuedPerDestination());
         httpClient.setMaxConnectionsPerDestination(config.getMaxConnectionsPerServer());
@@ -517,6 +517,8 @@ public class JettyHttpClient
             }
         }
         jettyRequest.followRedirects(finalRequest.isFollowRedirects());
+
+        setPreserveAuthorization(jettyRequest, finalRequest.isPreserveAuthorizationOnRedirect());
 
         // timeouts
         if (requestTimeoutMillis != null) {