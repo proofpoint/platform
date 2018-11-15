airlift/airlift

Add client executor as a managed bean to have it in dumps

http-client/src/main/java/io/airlift/http/client/jetty/JettyHttpClient.java
@@ -201,7 +201,10 @@ public class JettyHttpClient
         }
 
         httpClient.setByteBufferPool(new MappedByteBufferPool());
-        httpClient.setExecutor(createExecutor(name, config.getMinThreads(), config.getMaxThreads()));
+        QueuedThreadPool queuedThreadPool = createExecutor(name, config.getMinThreads(), config.getMaxThreads());
+        httpClient.setExecutor(queuedThreadPool);
+        // add queuedThreadPool as a managed bean to get its state in the client dumps
+        httpClient.addBean(queuedThreadPool, true);
         httpClient.setScheduler(createScheduler(name, config.getTimeoutConcurrency(), config.getTimeoutThreads()));
 
         httpClient.setSocketAddressResolver(new JettyAsyncSocketAddressResolver(

proofpoint/platform

http-client/src/main/java/com/proofpoint/http/client/jetty/JettyHttpClient.java
@@ -209,6 +209,8 @@ public class JettyHttpClient
         QueuedThreadPool executor = createExecutor(name, config.getMinThreads(), config.getMaxThreads());
         ioPoolStats = new IoPoolStats(executor);
         httpClient.setExecutor(executor);
+        // add executor as a managed bean to get its state in the client dumps
+        httpClient.addBean(executor, true);
         httpClient.setScheduler(createScheduler(name, config.getTimeoutConcurrency(), config.getTimeoutThreads()));
 
         httpClient.setSocketAddressResolver(new JettyAsyncSocketAddressResolver(