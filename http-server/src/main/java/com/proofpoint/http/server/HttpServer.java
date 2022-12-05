85217d5287081158b413ed1c7d8b32909a9d4264
Thomas Shore <tshore@proofpoint.com>
2022-12-05 17:07:17 +0000

    Added spaces back in to start of arrays

--- http-server/src/main/java/com/proofpoint/http/server/HttpServer.java
+++ http-server/src/main/java/com/proofpoint/http/server/HttpServer.java
@@ -90,7 +90,7 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 public class HttpServer
 {
     private static final String[] ENABLED_PROTOCOLS = System.getProperty("java.version").matches("11(\\.0\\.[12])?") ?
-            new String[]{"TLSv1.2"} : new String[]{"TLSv1.2", "TLSv1.3"};
+            new String[] {"TLSv1.2"} : new String[] {"TLSv1.2", "TLSv1.3"};
     private static final String[] ENABLED_CIPHERS = {
             "TLS_AES_128_GCM_SHA256",
             "TLS_AES_256_GCM_SHA384",
