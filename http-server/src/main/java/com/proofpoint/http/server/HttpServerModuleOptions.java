0a8eeb6ffc9d4a5a2f27f46403148a76607d2f6a
John Gardiner Myers <jgmyers@proofpoint.com>
2025-02-17 19:58:45 -0800

    Add virtual threads support in the server

--- http-server/src/main/java/com/proofpoint/http/server/HttpServerModuleOptions.java
+++ http-server/src/main/java/com/proofpoint/http/server/HttpServerModuleOptions.java
@@ -3,6 +3,7 @@ package com.proofpoint.http.server;
 public class HttpServerModuleOptions
 {
     private boolean allowAmbiguousUris = false;
+    private boolean enableVirtualThreads = false;
 
     public boolean isAllowAmbiguousUris()
     {
@@ -12,5 +13,15 @@ public class HttpServerModuleOptions
     public void setAllowAmbiguousUris()
     {
         this.allowAmbiguousUris = true;
+    }
+
+    public boolean isEnableVirtualThreads()
+    {
+        return enableVirtualThreads;
+    }
+
+    public void setEnableVirtualThreads()
+    {
+        this.enableVirtualThreads = true;
     }
 }
